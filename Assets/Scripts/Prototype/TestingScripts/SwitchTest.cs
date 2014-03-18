0878b98bffd9185bf4802c34175afbb1b9887cce
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-18 22:46:49 +0000

    Added my test scene, and a testing script for that scene.

---
+++ Assets/Scripts/Prototype/TestingScripts/SwitchTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchTest : MonoBehaviour {
+
+	public CameraController camera1;
+
+	public Transform p1;
+	public Transform p2;
+
+	bool player1Active = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown ("space"))
+		{
+			if (player1Active)
+			{
+				camera1.switchTo (p2);
+				player1Active = false;
+			}
+			else
+			{
+				camera1.switchTo (p1);
+				player1Active = true;
+			}
+		}
+
+		if (Input.GetKeyDown ("x"))
+		{
+			camera1.toggleAiming ( );
+		}
+	}
+}
