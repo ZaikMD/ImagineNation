b6da8684429ae54bf196925125f65b255618cb60
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-23 18:58:33 +0000

    Added moveable block script

---
+++ Assets/Scripts/Prototype/MoveableBlock.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveableBlock : MonoBehaviour
+{
+
+
+	public Size m_BlockSize; //This get's set by designers
+
+	void start()
+	{
+		
+	}
+	void Update()
+	{
+
+	}
+	Size returnSize()
+	{
+		return m_BlockSize;
+	}
+	
+	public void makeChild(GameObject obj)
+	{
+		transform.parent = obj.transform.parent;
+	}
+	
+	public void removeParent()
+	{
+		//Remove the parent-child relation between the block and the player
+		makeChild(null);             //Call makechild and pass in null to terminate the parent
+	}
+}
