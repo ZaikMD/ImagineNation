9cc92768d930e4f1516d24fffc6b0d3f279fd50b
elia0034 <elia0034@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-23 19:38:05 +0000

    Added Prototype files with some commented out code in order to remove errors Added Checkpoint.cs Added CheckpointManager.cs Added Trampoline.cs Added TrampolineButton.cs Added PlayerAIStateMachine.cs

---
+++ Assets/Scripts/Prototype/Checkpoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour 
+{
+
+	CheckpointManager m_CheckpointManager;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_CheckpointManager = CheckpointManager.m_Instance;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Character")
+		{
+			//m_CheckpointManager.setCurrentCheckPoint (this, other.gameObject.GetComponent<PlayerScript>());
+		}
+	}
+}
---
+++ Assets/Scripts/Prototype/TrampolineButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrampolineButton : MonoBehaviour 
+{
+	public Trampoline m_MyTrampoline;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Character")
+		{
+			if (other.gameObject.GetComponent<CharacterController>().velocity.y < 0)
+			{
+
+				if (Input.GetKeyDown(KeyCode.B))
+				{
+
+					m_MyTrampoline.m_DoubleJump = true;
+				
+				}
+			}
+		}
+	}
+}
