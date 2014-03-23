e2b62367800712299d6009947994f71f943f6e45
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-23 00:59:23 +0000

    Added in the BasePrimaryItem.cs script, the BoxingGloves.cs script, and the DerekProjectile.cs script.

---
+++ trunk/Assets/Scripts/Prototype/BoxingGloves.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxingGloves : BasePrimaryItem 
+{
+	GameObject m_Derek;
+	GameObject m_DerekProjectile;
+	bool m_EnableProjectile = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//m_Derek = getDerek ();
+		//m_DerekProjectile = m_BaseProjectile;
+	}
+	
+	public void fire(Vector3 currentTarget)
+	{
+		if(m_EnableProjectile == false)
+		{
+			Instantiate (m_DerekProjectile, m_DerekProjectile.transform.position, transform.rotation);
+			m_EnableProjectile = true;
+		}
+	}
+
+	public void aimFire(Vector3 currentTarget)
+	{
+		fire (currentTarget);
+	}
+
+}
