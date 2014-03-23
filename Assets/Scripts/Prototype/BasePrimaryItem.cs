aa684537f5bc19f5f286aef2ea715a6235d70db9
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-23 00:59:23 +0000

    Added in the BasePrimaryItem.cs script, the BoxingGloves.cs script, and the DerekProjectile.cs script.

---
+++ Assets/Scripts/Prototype/BasePrimaryItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BasePrimaryItem : MonoBehaviour 
+{
+	protected GameObject m_BaseProjectile;
+	
+	CameraController m_Camera;
+	
+	public virtual void fire(Vector3 currentTarget)
+	{
+		Instantiate(m_BaseProjectile);
+	}
+	
+	public virtual void aimFire(Vector3 currentTarget)
+	{
+		m_Camera.enableAiming ();
+		fire (currentTarget);
+	}
+}
---
+++ Assets/Scripts/Prototype/DerekProjectile.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+	public GameObject m_DebrisPrefab;
+	public float m_Speed = 10.0f;
+	public int m_ProjectileRange = 1;
+	Vector3 m_InitialPosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_InitialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position += transform.forward * m_Speed * Time.deltaTime;
+		float distance = Vector3.Distance (m_InitialPosition, transform.position);
+		if(distance > m_ProjectileRange)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		switch(other.gameObject.tag)
+		{
+		case "enemy":
+			//applyDamage();
+			Destroy (this.gameObject);
+
+			break;
+		case "destructableWall":
+			Instantiate(m_DebrisPrefab);
+			Destroy(other.gameObject);
+			Destroy(this.gameObject);
+
+			break;
+
+		default:
+			break;
+		}
+	}
+}
