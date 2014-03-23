0113ba57820e74c7016495dadbd6caf4cf3c6ea8
whit0717 <whit0717@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-23 19:41:59 +0000

    Added Whitlaw TestScene Added NerfGun and NerfGunProjectile scripts to prototypes.

---
+++ Assets/Scripts/Prototype/NerfGun.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NerfGun : MonoBehaviour 
+{
+
+	public GameObject m_Alex;
+
+	int m_NumberOfBullets;
+	const int maxBullets = 5;
+
+	int m_BulletsUsed = -1;
+
+	float m_ReloadTimer = 0.0f;
+	const float reloadTime = 3.0f;
+
+	GameObject[] BulletPool;
+	public Transform m_BulletPrefab;
+
+	//Create a public camera variable
+	
+	void Start () 
+	{
+		//Full ammo
+		m_NumberOfBullets = maxBullets;
+	}
+
+	void Update () 
+	{
+		if(m_NumberOfBullets >= maxBullets)
+		{
+			if(m_ReloadTimer >= reloadTime)
+			{
+				m_NumberOfBullets = maxBullets;
+				m_ReloadTimer = 0.0f;
+			}
+
+			m_ReloadTimer += Time.deltaTime;
+		}
+	}
+
+	public void Fire(Vector3 currentTarget)
+	{
+		if(m_NumberOfBullets > 0)                                
+		{
+			//Play animation/sounds
+			//grab a bullet from the pool of bullets
+			int i = m_BulletsUsed + 1;
+			GameObject tempBullet = BulletPool[i];
+			/*
+			BulletPool[i].transform = Instantiate(m_BulletPrefab, 
+			                                      transform.position, 
+			                                      Quaternion.identity);
+
+			BulletPool[i].transform.rotation = transform.rotation;
+			BulletPool[i].rigidbody.AddForce(currentTarget * 5000);
+
+			m_BulletsUsed++;
+			m_NumberOfBullets--;
+			*/
+		}
+	}
+		
+	public void aimFire(Vector3 currentTarget)
+	{
+		//Use the camera variable to get the reticle position
+		//Set that position to the currentTarget
+		if(m_NumberOfBullets > 0)
+		{
+			Fire(currentTarget);
+		}
+	}
+}
---
+++ Assets/Scripts/Prototype/NerfGunProjectile.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum NerfGunProjectileState
+{
+	Default = 0,
+	IsMoving,
+	IsPlatform,
+	Count,
+	Unknown
+};
+
+public class NerfGunProjectile : MonoBehaviour 
+{
+
+	NerfGunProjectileState m_State;
+
+	float m_BulletLifeSpan = 10.0f;
+	float m_PlatformLifeSpan = 20.0f;
+	float m_Timer = 0.0f;
+	float m_PlatformTimer = 0.0f;
+	float m_Speed = 15.0f;
+
+	string m_CollidedTag;
+
+	bool m_IsPlatform = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(m_State == NerfGunProjectileState.Default)
+		{
+			DefaultState();
+		}
+
+		switch(m_State)
+		{
+		case NerfGunProjectileState.IsPlatform :
+			IsPlatformState();
+			break;
+
+		case NerfGunProjectileState.IsMoving :
+			IsMovingState();
+			break;
+		}
+	}
+
+	void DefaultState()
+	{
+		//the Default state simply determines which of the two other states
+		//the projectile is in or should be in.
+		if(m_IsPlatform == true)
+		{
+			m_State = NerfGunProjectileState.IsPlatform;
+		}
+		else
+		{
+			m_State = NerfGunProjectileState.IsMoving;
+		}
+	}
+
+	void IsPlatformState()
+	{
+		//update the platform timer and if the timer has expired delete the platform
+		//potential for adding a growing and shrinking platform proportionally based
+		// on the time remaing.
+
+		m_PlatformTimer += Time.deltaTime;
+
+		if(m_PlatformTimer >= m_PlatformLifeSpan)
+			Destroy(this);
+	}
+	
+	void IsMovingState()
+	{
+		//update the bullets general timer and destroy it if it exceeds its set lifespan
+		m_Timer += Time.deltaTime;
+
+		if (m_Timer >= m_BulletLifeSpan)
+		{
+            Destroy(this);
+		}
+	}
+	
+	void OnCollisionEnter(Collision other)
+	{             
+		//Essentially this switch statement checks the tag of the object the bullet collided
+		// with and enters the corresponding case, if none of the 3 possible options were
+		// collided with the bullet by default is destroyed
+
+		m_CollidedTag = other.gameObject.tag;
+
+		switch(m_CollidedTag)
+		{
+			//All cases call a function that takes the gameobject of the object
+			//that the bullet collided with
+		case "Enemy" :
+				CollidedWithEnemy(other.gameObject);
+			break;
+			
+		case "NerfWall" :
+				CollidedWithNerfWall(other.gameObject);
+			break;
+			
+		case "NerfTarget" :
+				CollidedWithNerfTarget(other.gameObject);
+			break;
+			
+		default:
+			Destroy(this);
+			break;
+		}
+	}
+	
+	void CollidedWithEnemy(GameObject enemy)
+	{
+		Destroy(this);               
+		//enemy.applyDamage();
+	}
+	
+	void CollidedWithNerfWall(GameObject nerfWall)
+	{
+		//freeze the bullets position at the point of collision       
+		//preferably increase the size of the bullet or
+		// destroy the bullet and spawn a platform
+		transform.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+		transform.localScale = new Vector3 (0, transform.localScale .x + 1, 0);
+		m_IsPlatform = true;
+
+	}
+	
+	void CollidedWithNerfTarget(GameObject nerfTarget)
+	{             
+		//activate trigger is simply a placeholder name for a function within
+		//a nerf target script that will perform the intended response upon
+		//being hit
+		Destroy(this);               
+		//nerfTarget.activateTrigger();
+	}
+}
