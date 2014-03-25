31eaeb6023fa8446ed3462b22822b3213ce047e1
tack0012 <tack0012@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-25 09:15:41 +0000

    added fears implementation along with a simpleMoveForTesting.cs, not using character controllers just a quick tool to move for testing.
    i broke up the fears code into functions and added extra re usability to the fears code.

---
+++ Assets/Scripts/Prototype/FearScript.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FearScript : MonoBehaviour {
+
+    GameObject[] m_DarkFear;
+    GameObject[] m_ClaustrophobiaFear;
+    GameObject[] m_Players;
+    GameObject[] m_NerfDarts;
+    	
+	void Start ()
+    {
+
+        m_Players = GameObject.FindGameObjectsWithTag("Player");
+		m_NerfDarts = GameObject.FindGameObjectsWithTag("NerfDart");
+        m_DarkFear = GameObject.FindGameObjectsWithTag("Darkness");
+        m_ClaustrophobiaFear = GameObject.FindGameObjectsWithTag("Claustrophobia");
+
+        //calls a function that calls all the ignore collision voloumes
+        resetCollisionIgnores();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void setIgnoreFears(GameObject other)
+    {
+        setIgnoreDarkness(other);
+        setIgnoreClaustrophobia(other);        
+    }
+
+    public void setIgnoreDarkness(GameObject other)
+    {
+        //Loops through and set to ignore collision.
+        foreach (GameObject dark in m_DarkFear)
+        {
+            //tells the colliders to ignore each other
+            Physics.IgnoreCollision(other.collider, dark.collider);
+        }
+    
+    }
+
+    public void setIgnoreClaustrophobia(GameObject other)
+    {
+        foreach (GameObject claus in m_ClaustrophobiaFear)
+        {
+            //tells the colliders to ignore each other
+            Physics.IgnoreCollision(other.collider, claus.collider);
+        }    
+    }
+
+    public void setPlayerIgnore()
+    {
+        foreach (GameObject player in m_Players)
+        {
+            //Does a check to make sure only effects currect characters. 
+            if (this.gameObject.name == "Derek")
+            {
+                setIgnoreDarkness(player.gameObject);
+            }
+
+            if (this.gameObject.name == "Zoey")
+            {
+                setIgnoreClaustrophobia(player.gameObject);
+            }
+
+            if (this.gameObject.name == "Alex")
+            {
+                setIgnoreFears(player.gameObject);
+            }
+        
+        }
+    
+    }
+    public void setNerfDartIgnore()
+    {
+        foreach (GameObject nerf in m_NerfDarts)
+        {
+            setIgnoreFears(nerf.gameObject);       
+        }   
+    }
+
+    public void resetCollisionIgnores()
+    {
+        setPlayerIgnore();
+        setNerfDartIgnore();
+    }
+
+}
---
+++ Assets/Scripts/Prototype/simpleMoveForTesting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simpleMoveForTesting : MonoBehaviour {
+
+    public float m_MoveSpeed = 5;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * m_MoveSpeed * Time.deltaTime;
+        float verticle = Input.GetAxis("Vertical") * m_MoveSpeed * Time.deltaTime;
+
+        transform.Translate(horizontal, 0, verticle);
+
+	}
+}
