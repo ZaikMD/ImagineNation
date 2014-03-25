dc660db5f66014348901295b31cad1e4b578f2ea
tack0012 <tack0012@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-25 09:31:31 +0000

    added first version of the SecondairyBase.cs

---
+++ trunk/Assets/Scripts/Prototype/SecondairyBase.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class SecondairyBase : MonoBehaviour {
+
+    protected bool m_Enabled = false;
+    protected PlayerMovement m_playerMovement;
+	// Use this for initialization
+	void Start () 
+    {
+        m_playerMovement = gameObject.GetComponent<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+
+	}
+
+    bool isEnabled
+    {
+        get { return m_Enabled; }
+        set { m_Enabled = true; }
+    }
+
+    public abstract void Move();
+    
+}
