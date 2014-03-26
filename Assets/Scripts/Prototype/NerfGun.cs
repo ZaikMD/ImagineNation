08754c5457cc94b3552f2c16e301ea568b077b1a
whit0717 <whit0717@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-26 00:56:43 +0000

    Attempted BulletPooling, expected to be changed to simply instantiating/destroying every bullet.

--- Assets/Scripts/Prototype/NerfGun.cs
+++ Assets/Scripts/Prototype/NerfGun.cs
@@ -1,21 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NerfGun : MonoBehaviour 
+public class NerfGun : MonoBehaviour
 {
 
 	public GameObject m_Alex;
 
+	//Current number of bullets in the clip
 	int m_NumberOfBullets;
+
+	//Maximum number of bullets in a clip
 	const int maxBullets = 5;
 
 	int m_BulletsUsed = -1;
 
+	//Reload timer that begins after clip is emptied
 	float m_ReloadTimer = 0.0f;
+
+	//Number of seconds until the action of reloading is complete
 	const float reloadTime = 3.0f;
 
-	GameObject[] BulletPool;
+	//Pool of bullets
+	Transform[] BulletPool;
+
+	//Number of bullets in pool
+	int bulletPool = 10;
+
+	//Prefab that will be instantiated
 	public Transform m_BulletPrefab;
+
+	//List<Transform> m_BulletPool = new List<Transform>();
 
 	//Create a public camera variable
 	
@@ -23,48 +37,80 @@ public class NerfGun : MonoBehaviour
 	{
 		//Full ammo
 		m_NumberOfBullets = maxBullets;
-	}
 
+		//Instantiate the bullet pool
+		BulletPool = new Transform[bulletPool];
+
+		//Instantiate the bullet prefab, add them to the bullet pool, and set them to inactive
+		for(int i = 0; i < bulletPool; i++)
+		{
+
+			BulletPool[i] = (Transform) Instantiate(m_BulletPrefab,
+			                            			transform.position,
+			                            			Quaternion.identity);
+
+			//BulletPool[i].gameObject.GetComponent<NerfGunProjectile>().setActive(false);
+		}
+
+
+
+	}
+	
 	void Update () 
 	{
-		if(m_NumberOfBullets >= maxBullets)
+		//
+		if(m_NumberOfBullets <= 0)
 		{
 			if(m_ReloadTimer >= reloadTime)
 			{
 				m_NumberOfBullets = maxBullets;
 				m_ReloadTimer = 0.0f;
 			}
+			else
+			{
+				m_ReloadTimer += Time.deltaTime;
+			}
+		}
 
-			m_ReloadTimer += Time.deltaTime;
+		//Temporary code to test the fire function
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Fire(new Vector3(0,30,0));
 		}
 	}
 
 	public void Fire(Vector3 currentTarget)
 	{
+		//As long as the clip isn't empty
 		if(m_NumberOfBullets > 0)                                
 		{
+
+			Transform tempbullet;
 			//Play animation/sounds
-			//grab a bullet from the pool of bullets
-			int i = m_BulletsUsed + 1;
-			GameObject tempBullet = BulletPool[i];
-			/*
-			BulletPool[i].transform = Instantiate(m_BulletPrefab, 
-			                                      transform.position, 
-			                                      Quaternion.identity);
+			for(int i = 0; i < bulletPool; i++)
+			{
+				GameObject temp = BulletPool[i].gameObject;
+				NerfGunProjectile tempProj = temp.GetComponent<NerfGunProjectile>();
+				if(tempProj.getActive() == false)
+				{
+					tempbullet = BulletPool[i];
+					BulletPool[i].gameObject.GetComponent<NerfGunProjectile>().setActive(true);
 
-			BulletPool[i].transform.rotation = transform.rotation;
-			BulletPool[i].rigidbody.AddForce(currentTarget * 5000);
-
-			m_BulletsUsed++;
-			m_NumberOfBullets--;
-			*/
+					tempbullet.transform.rotation = transform.rotation;
+					tempbullet.rigidbody.AddForce(currentTarget * 5000);
+					
+					m_BulletsUsed++;
+					m_NumberOfBullets--;
+					break;
+				}
+			}
 		}
 	}
 		
 	public void aimFire(Vector3 currentTarget)
 	{
-		//Use the camera variable to get the reticle position
-		//Set that position to the currentTarget
+		//As long as the clip isn't empty call the fire function 
+		//with the new target
 		if(m_NumberOfBullets > 0)
 		{
 			Fire(currentTarget);
