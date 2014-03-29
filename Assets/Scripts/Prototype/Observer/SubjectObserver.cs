ffb1485d4d97b00a45b7f6e0a601e2f6cc3b6423
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-29 18:18:51 +0000

    made game compile secondary items base class was failing to compile
    added a SubjectObserver.cs for objects that need to be both subjects and observers
    
    moves the observer pattern event enum to an empty script called Events.cs

---
+++ Assets/Scripts/Prototype/Observer/SubjectObserver.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class SubjectObserver : Subject 
+{
+	public abstract void recieveEvent(Subject sender, ObeserverEvents recievedEvent);
+}
