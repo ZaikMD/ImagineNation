32e45969a094edcd3980273e9212cb3b7ca6aa2f
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-29 20:01:17 +0000

    changed observer to an interface and removed SubjectObserver.cs since the interface fixes it and its stupid to have both

---
+++ Assets/Scripts/Prototype/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : Subject 
+{
+
+	public static GameManager Instance{ get; private set; }
+
+	bool m_IsPaused = false;
+
+
+	void Awake()
+	{
+		//if theres another instance (there shouldnt be) destroy this... there can be only one
+		if(Instance != null && Instance != this)
+		{
+			//destroy all other instances
+			Destroy(gameObject);
+		}
+		
+		//set the instance
+		Instance = this;
+		
+		//prevents this object being destroyed between scene loads
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(PlayerInput.Instance.getPause())
+		{
+			if(m_IsPaused)
+			{
+				sendEvent(ObeserverEvents.StartGame);
+			}
+			else
+			{
+				sendEvent(ObeserverEvents.PauseGame);
+			}
+			m_IsPaused = !m_IsPaused;
+		}
+	}
+}
--- Assets/Scripts/Prototype/Observer/Observer.cs
+++ Assets/Scripts/Prototype/Observer/Observer.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 //inherits from mono develop since another class might need it later in the inheritance tree
-public abstract class Observer : MonoBehaviour 
+public interface Observer
 {
-	public abstract void recieveEvent(Subject sender, ObeserverEvents recievedEvent);
+	void recieveEvent(Subject sender, ObeserverEvents recievedEvent);
 }
