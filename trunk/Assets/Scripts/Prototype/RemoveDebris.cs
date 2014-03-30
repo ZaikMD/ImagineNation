da316be797e9dae04571d8bd4c1a36e028ee6931
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-30 19:40:51 +0000

    Fixed Destructible Wall Prefabs.

---
+++ trunk/Assets/Scripts/Prototype/RemoveDebris.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveDebris : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (DestroyDebris ());
+	}
+	
+	IEnumerator DestroyDebris()
+	{
+		yield return new WaitForSeconds (2.0f);
+		Destroy (this.gameObject);
+	}
+}
