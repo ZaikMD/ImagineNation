a78f1cd4693377f2cc52de909e7011241a1d55e8
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-03-30 21:14:24 +0000

    Added stuff back in. Finished Moving Platforms I think. Added in a prefab for them

--- Assets/Scripts/Prototype/MovingPlatforms.cs
+++ Assets/Scripts/Prototype/MovingPlatforms.cs
@@ -3,18 +3,27 @@ using System.Collections;
 
 public class MovingPlatforms : MonoBehaviour 
 {
-	public float m_XDistance;
-	public float m_YDistance;
-	public float m_ZDistance;
 	public float m_PauseTime;
-	public float m_MoveTimeInMilliseconds;
+	public float m_MoveTimeInSeconds;
+
 	public bool m_NeedsSwitch;
 	public bool m_MovesOnce;
 
+	public GameObject m_Destination;
+	
 	private float m_InitialPauseTime;
+	private float m_InitialMoveTime;
+	private float m_MoveTimeInMilliseconds;
+
 	private Vector3 m_DestinationPosition;
 	private Vector3 m_InitialPosition;
+
 	private bool m_HasMoved = false;
+	public  bool m_SwitchToggled = false;
+
+	private float m_XDistance;
+	private float m_YDistance;
+	private float m_ZDistance;
 
 	private float m_XMovePercent;
 	private float m_YMovePercent;
@@ -23,9 +32,16 @@ public class MovingPlatforms : MonoBehaviour
 	void Start () 
 	{
 		m_InitialPauseTime = m_PauseTime;
-		m_DestinationPosition = new Vector3 (transform.position.x + m_XDistance, transform.position.y + m_YDistance, transform.position.z + m_ZDistance);
+		m_InitialMoveTime = m_MoveTimeInSeconds;
 		m_InitialPosition = transform.position;
-	
+		m_MoveTimeInMilliseconds = m_MoveTimeInSeconds * 60.0f;
+
+		m_XDistance = m_Destination.transform.position.x - transform.position.x;
+		m_YDistance = m_Destination.transform.position.y - transform.position.y;
+		m_ZDistance = m_Destination.transform.position.z - transform.position.z;
+
+		m_DestinationPosition = new Vector3 (transform.position.x + m_XDistance,transform.position.y + m_YDistance,transform.position.z + m_ZDistance);
+
 		m_XMovePercent = m_XDistance/ m_MoveTimeInMilliseconds;
 		m_YMovePercent = m_YDistance/ m_MoveTimeInMilliseconds;
 		m_ZMovePercent = m_ZDistance/ m_MoveTimeInMilliseconds;
@@ -34,34 +50,67 @@ public class MovingPlatforms : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if(m_PauseTime > 0)
+		if (m_NeedsSwitch == false) 
 		{
-			m_PauseTime -= Time.deltaTime;
-		}
-		else if (m_HasMoved == false)
-		{
-			//transform.position = Vector3.Lerp(transform.position, m_LerpPosition, m_LerpTime);
-			transform.Translate(m_XMovePercent, m_YMovePercent, m_ZMovePercent);
+						if (m_PauseTime > 0) {
+								m_PauseTime -= Time.deltaTime;
+								m_MoveTimeInSeconds = m_InitialMoveTime;
+						} else if (m_HasMoved == false) {
+								//transform.position = Vector3.Lerp(transform.position, m_LerpPosition, m_LerpTime);
+								transform.Translate (m_XMovePercent, m_YMovePercent, m_ZMovePercent);
+								m_MoveTimeInSeconds -= Time.deltaTime;
 
-			if(transform.position == m_DestinationPosition)
+								if (m_MoveTimeInSeconds < 0) {
+
+										m_PauseTime = m_InitialPauseTime;
+										m_HasMoved = true;
+								} 
+						} else if (m_HasMoved == true && m_MovesOnce == false) {
+								//transform.position = Vector3.Lerp(transform.position, m_InitialPosition, m_LerpTime);
+								transform.Translate (-1 * m_XMovePercent, -1 * m_YMovePercent, -1 * m_ZMovePercent);
+								m_MoveTimeInSeconds -= Time.deltaTime;
+
+								if (m_MoveTimeInSeconds < 0) {
+										m_PauseTime = m_InitialPauseTime;
+										m_HasMoved = false;
+								}
+						}
+
+		}
+
+		else
+		{
+			if(m_SwitchToggled == true)
 			{
-				m_PauseTime = m_InitialPauseTime;
-				m_HasMoved = true;
+
+				if (m_PauseTime > 0) {
+					m_PauseTime -= Time.deltaTime;
+					m_MoveTimeInSeconds = m_InitialMoveTime;
+				} else if (m_HasMoved == false) {
+					//transform.position = Vector3.Lerp(transform.position, m_LerpPosition, m_LerpTime);
+					transform.Translate (m_XMovePercent, m_YMovePercent, m_ZMovePercent);
+					m_MoveTimeInSeconds -= Time.deltaTime;
+					
+					if (m_MoveTimeInSeconds < 0) {
+						
+						m_PauseTime = m_InitialPauseTime;
+						m_HasMoved = true;
+					} 
+				} else if (m_HasMoved == true && m_MovesOnce == false) {
+					//transform.position = Vector3.Lerp(transform.position, m_InitialPosition, m_LerpTime);
+					transform.Translate (-1 * m_XMovePercent, -1 * m_YMovePercent, -1 * m_ZMovePercent);
+					m_MoveTimeInSeconds -= Time.deltaTime;
+					
+					if (m_MoveTimeInSeconds < 0) {
+						m_PauseTime = m_InitialPauseTime;
+						m_HasMoved = false;
+					}
+				}
+
 			}
 		}
-		else if(m_HasMoved == true && m_MovesOnce == false)
-		{
-			//transform.position = Vector3.Lerp(transform.position, m_InitialPosition, m_LerpTime);
-			transform.Translate(-1*m_XMovePercent, -1*m_YMovePercent, -1*m_ZMovePercent);
-
-			if(transform.position == m_InitialPosition)
-			{
-				m_PauseTime = m_InitialPauseTime;
-				m_HasMoved = false;
-			}
-		}
-
-
 
 	}
+	
+
 }
