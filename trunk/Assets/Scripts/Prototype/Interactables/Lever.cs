52292d6e8a0dcb4a56eb45ae0f5b54a83aef9c98
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-01 02:28:21 +0000

    Added in Lever.cs

---
+++ trunk/Assets/Scripts/Prototype/Interactables/Lever.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lever : InteractableBaseClass 
+{
+
+	public bool m_IsOn;
+	// Use this for initialization
+	void Start () 
+	{
+		m_Type = InteractableType.Lever;
+		m_IsExitable = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public bool getIsOn()
+	{
+		return m_IsOn;
+	}
+}
