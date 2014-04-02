923911c38f7f1600f1efdcccd2960a37169d08be
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-02 22:32:17 +0000

    Added EnemySpawn.cs and EnemySpawnManager.cs.

---
+++ trunk/Assets/Scripts/Prototype/AI/Enemies/EnemySpawnManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnManager : MonoBehaviour 
+{
+	public EnemySpawn[] m_EnemySpawn;
+
+	public void respawnAll()
+	{
+		for(int i = 0; i < m_EnemySpawn.Length; i++)
+		{
+			m_EnemySpawn[i].spawn();
+		}
+	}
+}
