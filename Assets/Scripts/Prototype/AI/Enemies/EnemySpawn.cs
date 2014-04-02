530e308b59ec356d25fffcf03dcac1a56af2d941
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-02 22:32:17 +0000

    Added EnemySpawn.cs and EnemySpawnManager.cs.

---
+++ Assets/Scripts/Prototype/AI/Enemies/EnemySpawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour 
+{
+	public BaseEnemy m_Enemy;
+	//TODO: public enemy type variable
+
+	public void spawn()
+	{
+		if(m_Enemy != null)
+		{
+			//TODO: m_Enemy.reset();
+		}
+		else
+		{
+			//TODO: instantiate the enemy type specifed
+			//Instantiate (m_Enemy, this.transform.position, this.transform.rotation);
+		}
+	}
+}
