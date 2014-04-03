03ccf56c113deb5ae0d3a078a701a4263d8d8ed1
elia0034 <elia0034@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-03 22:22:56 +0000

    Added RCCarWheelAllignment.cs Added RCCarMovement.cs Continued RCCar.cs

---
+++ trunk/Assets/Scripts/Prototype/Items/RCCar/RCCar.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum RCCarStates
+{
+	Idle = 0,
+	Moving,
+	Interacting,
+	Exiting
+}
+
+public class RCCar : SecondairyBase
+{
+	AlexPlayerState m_Alex = null;
+
+	RCCarStates m_State = RCCarStates.Idle;
+	GameObject m_RCCar = null;
+
+	bool m_HasBegun = false;
+	bool m_Interacting;	
+
+	Vector3 m_StartingOffset = new Vector3(4,1,0);
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_Alex = this.gameObject.GetComponent<AlexPlayerState> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (m_HasBegun)
+		{
+		switch(m_State)
+		{
+			case RCCarStates.Idle:
+				{
+				Idle();
+				}
+				break;
+
+			case RCCarStates.Moving:
+				{
+				Move();
+				}
+				break;
+
+			case RCCarStates.Interacting:
+				{
+				Interacting();
+				}
+				break;
+
+			case RCCarStates.Exiting:
+				{
+				Exiting();
+				}
+				break;
+		}
+		}
+
+	}
+
+
+	public void BeginRCCar()
+	{
+		if (m_HasBegun == false)
+		{
+			m_RCCar = (GameObject)Instantiate(Resources.Load("Prefabs/RCCarPrefab"));
+			m_RCCar.transform.position = m_Alex.transform.position + m_StartingOffset; 
+			m_HasBegun = true;
+		}
+	}
+
+	void Idle()
+	{
+		if (PlayerInput.Instance.getEnviromentInteraction () && m_HasBegun) 
+		{
+			m_State = RCCarStates.Exiting;
+		}
+
+		else if (PlayerInput.Instance.getMovementInput() != new Vector2(0,0))
+		{
+			m_State = RCCarStates.Moving;
+		}
+
+	}
+
+	public override void Move ()
+	{
+		m_RCCar.GetComponent<RCCarMovement> ().RegularMove ();
+		m_State = RCCarStates.Idle;
+	}
+
+	void Interacting()
+	{
+		m_State = RCCarStates.Idle;
+	}
+
+	void Exiting()
+	{
+		Destroy (m_RCCar);
+		m_RCCar = null;
+		m_HasBegun = false;
+		m_PlayerMovement.setCanMove (true);
+		m_Alex.setExitingSecond (true);
+		m_State = RCCarStates.Idle;
+	}
+
+	public override bool ableToBeUsed()
+	{
+		if (m_PlayerMovement.IsGrounded ())
+						return true;
+				else 
+						return false;
+	}
+}
---
+++ trunk/Assets/Scripts/Prototype/Items/RCCar/RCCarWheelAllignment.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RCCarWheelAllignment : MonoBehaviour 
+{
+	public WheelCollider m_WheelCollider;
+	float m_Rotation = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		RaycastHit hit;
+		Vector3 colliderCenter = m_WheelCollider.transform.TransformPoint (m_WheelCollider.center);
+		
+		if (Physics.Raycast (colliderCenter, - m_WheelCollider.transform.up, out hit, m_WheelCollider.suspensionDistance + m_WheelCollider.radius))
+		{
+			transform.position = hit.point + (m_WheelCollider.transform.up * m_WheelCollider.radius);
+		}
+		else
+		{
+			transform.position = colliderCenter - (m_WheelCollider.transform.up * m_WheelCollider.suspensionDistance);
+		}
+		
+		transform.rotation = m_WheelCollider.transform.rotation * Quaternion.Euler (m_Rotation, m_WheelCollider.steerAngle, 0);
+		
+		m_Rotation += m_WheelCollider.rpm * (360 / 60) * Time.deltaTime;
+	}
+}
