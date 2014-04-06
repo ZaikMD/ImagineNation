f1e8583d208f8c96b710c8de173dfda9dc9f1403
whit0717 <whit0717@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-06 20:52:36 +0000

    Updated for Kris

---
+++ Assets/Scripts/Prototype/AI/Enemies/Crochuck.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Last updated 04/06/2014
+
+public class Crochuck : BaseEnemy 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	protected override void die()
+	{
+		//TODO:play death animation and instantiate ragdoll
+		Destroy (this.gameObject);
+	}
+
+	protected override void fightState()
+	{
+		//TODO:play attack animation and attack sound
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
