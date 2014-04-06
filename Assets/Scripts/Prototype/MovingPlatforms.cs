76e88f9a87ca7d43a2a3c2fac14a5771f9acb780
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-06 20:51:56 +0000

    Tagged stuff that was worked on

--- Assets/Scripts/Prototype/MovingPlatforms.cs
+++ Assets/Scripts/Prototype/MovingPlatforms.cs
@@ -4,41 +4,49 @@ using System.Collections;
 public class MovingPlatforms : MonoBehaviour , Observer
 {
 
-
+	//Public floats for designers
 	public float m_PauseTime;
 	public float m_MoveTimeInSeconds;
 	public float m_DelayTime;
 
+	//Bools for designers
 	public bool m_NeedsSwitch;
 	public bool m_MovesOnce;
 
+	//The destination
 	public GameObject m_Destination;
-	
+
+	//Floats
 	private float m_InitialPauseTime;
 	private float m_InitialMoveTime;
 	private float m_MoveTimeInMilliseconds;
 
+	//Positions
 	private Vector3 m_DestinationPosition;
 	private Vector3 m_InitialPosition;
-
+	private bool m_IsPaused = false;
+	//Bools
 	private bool m_HasMoved = false;
 	public  bool m_SwitchToggled = false;
 
+	//Distances
 	private float m_XDistance;
 	private float m_YDistance;
 	private float m_ZDistance;
 
+	//Move percentages per second
 	private float m_XMovePercent;
 	private float m_YMovePercent;
 	private float m_ZMovePercent;
 
-	bool m_IsPaused = false;
 
+	//Subject
 	public Subject m_Sender;
 
 	// Use this for initialization
 	void Start () 
 	{
+		//Initialize everything, calculate the move percentages and movement time
 		m_InitialPauseTime = m_PauseTime;
 		m_InitialMoveTime = m_MoveTimeInSeconds;
 		m_InitialPosition = transform.position;
@@ -67,34 +75,30 @@ public class MovingPlatforms : MonoBehaviour , Observer
 		{
 
 
-				if (m_NeedsSwitch == false) 
+				if (m_NeedsSwitch == false) //If it doesn't need a switch
 				{
-					m_DelayTime -= Time.deltaTime;
+					m_DelayTime -= Time.deltaTime; //Count down the delay timer
 
-					if(m_DelayTime < 0)
+					if(m_DelayTime <= 0)
 					{
-						if (m_PauseTime >= 0) 
+						if (m_PauseTime >= 0) //Check the pause timers
 						{
-							m_PauseTime -= Time.deltaTime;
+							m_PauseTime -= Time.deltaTime; //Count down pause timer and set the move time
 							m_MoveTimeInSeconds = m_InitialMoveTime;
 						} 
-						else if (m_HasMoved == false) 
+						else if (m_HasMoved == false) //Check to see if it's moved already
 						{
-
-							//transform.position = Vector3.Lerp(transform.position, m_LerpPosition, m_LerpTime);
-							transform.Translate (m_XMovePercent, m_YMovePercent, m_ZMovePercent);
-							m_MoveTimeInSeconds -= Time.deltaTime;
+							transform.Translate (m_XMovePercent, m_YMovePercent, m_ZMovePercent); //Move the platforms by the move percentage
+							m_MoveTimeInSeconds -= Time.deltaTime; //Count down move time
 
 							if (m_MoveTimeInSeconds < 0) 
 							{
-
-								m_PauseTime = m_InitialPauseTime;
+								m_PauseTime = m_InitialPauseTime; //Reset pause time
 								m_HasMoved = true;
 							} 
 						} 
 						else if (m_HasMoved == true && m_MovesOnce == false) 
 						{
-							//transform.position = Vector3.Lerp(transform.position, m_InitialPosition, m_LerpTime);
 							transform.Translate (-1 * m_XMovePercent, -1 * m_YMovePercent, -1 * m_ZMovePercent);
 							m_MoveTimeInSeconds -= Time.deltaTime;
 
