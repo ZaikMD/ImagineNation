65427dbec8a0c601144e6ff2d0d069a94a7c1f4a
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-09 22:55:18 +0000

    FearKillingLightOfMagicalAwesomenessMadeAtTheRequestOfMrAdamHollaway.cs added

---
+++ Assets/Scripts/Prototype/FearKillingLightOfMagicalAwesomenessMadeAtTheRequestOfMrAdamHollaway.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FearKillingLightOfMagicalAwesomenessMadeAtTheRequestOfMrAdamHollaway : MonoBehaviour, Observer 
+{
+	public Subject m_Sender;
+
+	public Light m_Light;
+	public GameObject m_Darkness;
+
+	Collider[] m_ZoeyColliders;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(m_Darkness.tag != "Darkness")
+		{
+			Destroy(this.gameObject);
+		}
+
+		m_Light.enabled = false;
+
+		m_Sender.addObserver (this);
+
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+		for(int i = 0; i < players.Length; i++)
+		{
+			if(players[i].name == "Zoey")
+			{
+				m_ZoeyColliders = players[i].GetComponents<Collider>();
+				break;
+			}
+		}
+	}
+
+	public void recieveEvent(Subject sender, ObeserverEvents recievedEvent)
+	{
+		if (recievedEvent == ObeserverEvents.Used) 
+		{
+			m_Light.enabled = true;
+
+			for(int i = 0; i < m_ZoeyColliders.Length; i++)
+			{
+				FearScript.Instance.setIgnoreDarkness(m_ZoeyColliders[i]);
+			}
+		}
+	}
+}
