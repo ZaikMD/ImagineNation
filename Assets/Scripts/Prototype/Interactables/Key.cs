24f515fd3f3fa7193f09867a8169c1775dfbdcb1
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-11 18:44:40 +0000

    added keys

---
+++ Assets/Scripts/Prototype/Interactables/Key.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : Subject 
+{
+	void OnCollisionEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			sendEvent(ObeserverEvents.Used);
+			Destroy(this.gameObject);
+		}
+	}
+}
