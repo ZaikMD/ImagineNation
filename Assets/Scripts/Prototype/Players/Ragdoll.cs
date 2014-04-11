f42943a23765df339e1045e66d62b94f8ed8c1ca
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-11 19:55:38 +0000

    mid progress ragdoll effects

---
+++ Assets/Scripts/Prototype/Players/Ragdoll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ragdoll : MonoBehaviour {
+
+	public GameObject m_DeathDebris;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+	
+}
