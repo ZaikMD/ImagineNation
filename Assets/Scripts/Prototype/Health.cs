19ddc9047f83340643382413883a3541aa70ad8f
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-11 19:34:36 +0000

    HEalth now has cylinders

--- Assets/Scripts/Prototype/Health.cs
+++ Assets/Scripts/Prototype/Health.cs
@@ -11,11 +11,14 @@ Call the takeDamage ( int damage ) function to deal damage to the character
 
 Call the resetHealth ( ) function to reset the players health when he revives
 
-Created by Jason Hein
+Created by Jason "The casual"  Hein
 
 
 3/16/2014
     Implemented
+
+4/11/2014
+Changed by Zach Dubuc to use three cylinders instead of light
 */
 
 
@@ -26,17 +29,19 @@ using System.Collections;
 public class Health : MonoBehaviour {
 
 	//Health
-	public int m_MaxHealth = 10;
-	int m_Health;
+	public int m_MaxHealth = 3;
+	public int m_Health;
 
 	//Default Intensity of  health light
 	float m_DefaultIntensity;
 
-	//Light object
-	GameObject m_Light = null; 
+	//GlowStick object
+	public GameObject[] m_Cylinders;
 
-	//You can set the colour of the health light
-	public Color m_HealthColor = Color.cyan;
+	Vector3[] m_CylinderPositions = new Vector3[3];
+	Vector3[] m_CylinderResets = new Vector3[3];
+
+	GameObject[] m_CylinderPositionReset = new GameObject[3];
 
 	//You can choose if the character regenerates health
 	public bool m_CanRegenerate = true;
@@ -44,24 +49,39 @@ public class Health : MonoBehaviour {
 	const float REGENERATION_DELAY = 10.0f;
 
 
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 
 		//characters health always starts at its max health
 		m_Health = m_MaxHealth;
 
-		//create a health light and make it follow inside the character
-		m_Light = (GameObject)Instantiate(Resources.Load("HealthLight"), this.transform.position, Quaternion.identity);
-		m_Light.transform.parent = this.transform;
+		for(int i = 0; i < m_CylinderPositionReset.Length; i ++)
+		{
+			m_CylinderPositionReset[i] = new GameObject();
+			m_CylinderPositionReset[i].transform.parent = this.gameObject.transform;
+		}
 
-		//Allows you to set the color of the light through unity
-		m_Light.light.color = m_HealthColor;
+		//create a health cylinders and make it follow  the character
+		for(int i = 0; i < m_Cylinders.Length; i++)
+		{
+			m_Cylinders[i].SetActive (true);
+			m_Cylinders[i].transform.parent = this.gameObject.transform;
+			m_CylinderPositions[i] = m_Cylinders[i].transform.position;
+			m_CylinderPositionReset[i].transform.position = m_Cylinders[i].transform.position;
+		}
 
-		//Light range must account for this characters size
-		m_Light.light.range *= this.transform.localScale.x;
+		for(int i = 0; i < m_CylinderPositions.Length; i ++)
+		{
+			m_CylinderResets[i] = new Vector3(gameObject.transform.position.x - m_CylinderPositions[i].x, 
+			                                  gameObject.transform.position.y - m_CylinderPositions[i].y,
+			                                  gameObject.transform.position.z - m_CylinderPositions[i].z);
 
-		//set the intensity to whatever intensity is set in the prefab
-		m_DefaultIntensity = m_Light.light.intensity;
+		}
+
+
+
 	}
 
 	//Regenerate the character's health.
@@ -74,11 +94,34 @@ public class Health : MonoBehaviour {
 			{
 				m_Health++;
 				m_RegeneratationTimer = 0.0f + REGENERATION_DELAY;
-				m_Light.light.intensity = m_DefaultIntensity * ((float)m_Health / m_MaxHealth);
 
 				if (m_Health > m_MaxHealth)
 				{
 					m_RegeneratationTimer = -1.0f;
+				}
+			}
+		} 
+
+		for(int i = 0; i < m_Cylinders.Length; i++)
+		{
+			if(i < m_Health)
+			{
+				m_Cylinders[i].SetActive(true);  
+
+				m_Cylinders[i].transform.position = m_CylinderPositionReset[i].transform.position;
+
+				Rigidbody rigid = m_Cylinders[i].GetComponent<Rigidbody>();
+				if( rigid != null)
+				{
+					Destroy(rigid);
+				}
+			}
+			else
+			{
+				if( m_Cylinders[i].GetComponent<Rigidbody>() == null)
+				{
+					Rigidbody rigid = m_Cylinders[i].AddComponent<Rigidbody>();
+					rigid.useGravity = true;
 				}
 			}
 		}
@@ -94,7 +137,7 @@ public class Health : MonoBehaviour {
 		m_Health -= damage;
 
 		//Update the intensity of the health light
-		m_Light.light.intensity = m_DefaultIntensity * ((float)m_Health / m_MaxHealth);
+		//m_Light.light.intensity = m_DefaultIntensity * ((float)m_Health / m_MaxHealth);
 
 		//Sets the regenration timer to start
 		m_RegeneratationTimer = 0.0f;
@@ -115,6 +158,6 @@ public class Health : MonoBehaviour {
 	public void resetHealth()
 	{
 		m_Health = m_MaxHealth;
-		m_Light.light.intensity = m_DefaultIntensity;
+		//m_Light.light.intensity = m_DefaultIntensity;
 	}
 }
