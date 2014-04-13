32aab7bbd45404a5769c2a674205c53fdc369840
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-13 15:34:32 +0000

    troll mage shield implemented and looking good

---
+++ trunk/Assets/Scripts/Prototype/AI/Enemies/Troll Mage/Shield/CirclingOject.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CirclingOject : MonoBehaviour 
+{
+	Vector3 m_Destination;
+
+	Vector3 m_RealDestination;
+
+	Transform m_ParentTransform;
+
+	bool m_AtDestination = false;
+	
+	public void setDestination(Vector3 destination)
+	{
+		m_Destination = destination;
+		m_AtDestination = false;
+	}
+
+	public bool getIfAtDestination()
+	{
+		return m_AtDestination;
+	}
+
+	void Start()
+	{
+		m_ParentTransform = gameObject.transform.parent.transform;
+	}
+
+	public void update () 
+	{
+		m_RealDestination = m_ParentTransform.position + m_Destination;
+
+		if(Vector3.Distance(gameObject.transform.position, m_RealDestination) < 0.5f)
+		{
+			m_AtDestination = true;
+		}
+
+		gameObject.transform.position = m_ParentTransform.position + Vector3.RotateTowards(gameObject.transform.position - m_ParentTransform.position, m_Destination, 0.05f, 0.05f);
+	}
+
+	public void adjustRadius(float radius)
+	{
+		m_Destination.Normalize();
+		m_Destination *= radius;
+
+		transform.position = m_ParentTransform.position + ((transform.position - m_ParentTransform.position).normalized * radius);
+	}
+}
