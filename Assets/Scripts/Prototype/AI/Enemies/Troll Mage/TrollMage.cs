ab09bc05fe2adc101aadd9f6eec2fe2d2f458c6f
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-13 16:05:38 +0000

    started troll mage kinda

---
+++ Assets/Scripts/Prototype/AI/Enemies/Troll Mage/TrollMage.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrollMage : BaseEnemy
+{
+	int m_ShieldHealth = 100;
+
+	NavMeshAgent m_Agent;
+
+	// Use this for initialization
+	void start () 
+	{
+		m_Health.resetHealth();
+		
+		m_CombatRange = 30.0f;
+		
+		m_Agent = this.gameObject.GetComponent<NavMeshAgent>();
+	}
+
+	protected override void die()
+	{
+		//TODO: Instantiate Ragdoll
+		Destroy(this.gameObject);
+	}
+
+	protected override void fightState()
+	{
+
+	}
+
+	public override void applyDamage (int amount)
+	{
+		//TODO: damage shield / take damage
+	}
+}
