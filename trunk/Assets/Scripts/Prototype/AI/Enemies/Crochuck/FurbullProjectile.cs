ec4ff3f9464989d2f77adb6ce91bca321aec9cdc
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-13 00:02:03 +0000

    crochuck shoots the furbulls

--- trunk/Assets/Scripts/Prototype/AI/Enemies/Crochuck/FurbullProjectile.cs
+++ trunk/Assets/Scripts/Prototype/AI/Enemies/Crochuck/FurbullProjectile.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class FurbullProjectile : MonoBehaviour {
 
 	Crochuck m_Spawner;
-	const float FIRE_SPEED = 2.0f;
+	const float FIRE_SPEED = 0.5f;
 
 	// Use this for initialization
 	public void onUse(Crochuck spawner)
 	{
 		m_Spawner = spawner;
-		rigidbody.velocity = transform.forward + transform.up * FIRE_SPEED * m_Spawner.getDistanceToTarget();
+		rigidbody.velocity = (transform.forward + transform.up) * FIRE_SPEED * m_Spawner.getDistanceToTarget();
 	}
 	
 	void OnCollisionEnter(Collision collision)
@@ -18,6 +18,11 @@ public class FurbullProjectile : MonoBehaviour {
 		//Do damage if we hit a player
 
 		//FurbulProjectile
+
+		if (collision.gameObject.name == "FurbulProjectile(Clone)")
+		{
+			return;
+		}
 
 		m_Spawner.instantiateFurbull(transform.position);
 		GameObject.Destroy (this.gameObject);
