301a20fb2c47752fc466fbac11715a76b1809b17
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-13 19:47:05 +0000

    Fixed cape.

--- Assets/Scripts/Prototype/Items/Cape.cs
+++ Assets/Scripts/Prototype/Items/Cape.cs
@@ -12,7 +12,7 @@ public class Cape : SecondairyBase
 			if ( m_Enabled )
 			{
 				m_Enabled = false;    //If you are on the ground disable the cape
-				m_PlayerMovement.setCanMove(true);
+				m_PlayerMovement.DisableCape();
 				gameObject.GetComponent<ZoeyPlayerState>().setExitingSecond(true);
 			}
 			return;
@@ -22,14 +22,11 @@ public class Cape : SecondairyBase
 		//Airborne
 		if ( m_Enabled )
 		{
-			  //Glide
-			Move();
-
 			//Turn cape off
 			if ( PlayerInput.Instance.getJumpHeld() == false )
 			{
 				m_Enabled = false;
-				m_PlayerMovement.setCanMove(true);
+				m_PlayerMovement.DisableCape();
 				gameObject.GetComponent<ZoeyPlayerState>().setExitingSecond(true);
 			}
 		}
@@ -41,7 +38,7 @@ public class Cape : SecondairyBase
 	/// </summary>
 	public override void Move ()
 	{
-		m_PlayerMovement.Glide(); 
+		//
 	}
 
 	/// <summary>
@@ -66,6 +63,8 @@ public class Cape : SecondairyBase
 	public void StartGliding()
 	{
 		m_Enabled = true;
+		m_PlayerMovement.ActivateCape ();
+		m_PlayerMovement.setCanMove (true);
 	}
 
 }
