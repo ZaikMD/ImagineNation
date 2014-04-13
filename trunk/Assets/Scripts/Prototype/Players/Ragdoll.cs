5c1d22bbd8a3fe863d5f1bf86b035489e589bd6f
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-13 21:28:40 +0000

    player death ragdoll effects

--- trunk/Assets/Scripts/Prototype/Players/Ragdoll.cs
+++ trunk/Assets/Scripts/Prototype/Players/Ragdoll.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class Ragdoll : MonoBehaviour {
 
-	public GameObject m_DeathDebris;
+	public GameObject m_GhostPrefab;
+	Vector3 m_InitialPos;
+	public float m_GhostRange = 0.0f;
+	public float m_Speed = 0.0f;
+	Vector3 m_Direction;
 
 	// Use this for initialization
 	void Start () {
+	
+		m_InitialPos = transform.position;
 	
 	}
 	
@@ -14,6 +20,14 @@ public class Ragdoll : MonoBehaviour {
 	void Update () {
 
 
+		m_GhostPrefab.transform.position = new Vector3 (m_GhostPrefab.transform.position.x, m_GhostPrefab.transform.position.y  + m_Speed * Time.deltaTime, m_GhostPrefab.transform.position.z);
+
+
+		float distance = Vector3.Distance (m_InitialPos, m_GhostPrefab.transform.position);
+		if(distance > m_GhostRange)
+		{
+			Destroy(this.gameObject);
+		}
 	
 	}
 	
