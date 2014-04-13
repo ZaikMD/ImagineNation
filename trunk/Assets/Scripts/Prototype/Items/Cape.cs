74db1754df568bffbcb0354c893e85cdf0845e82
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-13 23:45:33 +0000

    Fixed cape rotation.

--- trunk/Assets/Scripts/Prototype/Items/Cape.cs
+++ trunk/Assets/Scripts/Prototype/Items/Cape.cs
@@ -13,8 +13,7 @@ using System.Collections;
 public class Cape : SecondairyBase 
 {
 	public Transform m_Cape;
-	Vector3 m_Rotation = new Vector3(40.0f,0.0f,0.0f);
-	
+
 	void Update ( )
 	{
 		//Ground
@@ -23,36 +22,38 @@ public class Cape : SecondairyBase
 			//Cape flows in the wind
 			if (m_PlayerMovement.getControllerProjection() != Vector3.zero)
 			{
-				m_Cape.Rotate (Mathf.Sin(Time.realtimeSinceStartup * 8.0f) * 0.03f * m_Rotation);
+				//m_Cape.Rotate (Mathf.Sin(Time.realtimeSinceStartup * 8.0f) * 0.03f * m_Rotation);
+				m_Cape.localRotation = Quaternion.Euler(340 + (Mathf.Sin(Time.realtimeSinceStartup * 10.0f) * 0.15f * 40), 180, 180);
 			}
 			else
 			{
-				m_Cape.Rotate (Mathf.Sin(Time.realtimeSinceStartup) * 0.005f * m_Rotation);
+				m_Cape.localRotation = Quaternion.Euler(340 + (Mathf.Sin(Time.realtimeSinceStartup * 2.5f) * 0.04f * 40), 180, 180);
+				//m_Cape.Rotate (Mathf.Sin(Time.realtimeSinceStartup * 1.25f) * 0.005f * m_Rotation);
 			}
 
 			if ( m_Enabled )
 			{
 				m_Enabled = false;    //If you are on the ground disable the cape
 				m_PlayerMovement.DisableCape();
-				m_Cape.Rotate (-m_Rotation);
+				//m_Cape.Rotate (-m_Rotation);
 				gameObject.GetComponent<ZoeyPlayerState>().setExitingSecond(true);
 			}
-			return;
 		}
 
 
 		//Airborne
-		if ( m_Enabled )
+		else if ( m_Enabled )
 		{
 			//Cape flows in the wind
-			m_Cape.Rotate (Mathf.Sin(Time.realtimeSinceStartup * 10.0f) * 0.025f * m_Rotation);
+			//m_Cape.Rotate (Mathf.Sin(Time.realtimeSinceStartup * 10.0f) * 0.025f * m_Rotation);
+			m_Cape.localRotation = Quaternion.Euler(300 + (Mathf.Sin(Time.realtimeSinceStartup * 18.0f) * 0.18f * 40), 180, 180);
 
 			//Turn cape off
 			if ( PlayerInput.Instance.getJumpHeld() == false )
 			{
 				m_Enabled = false;
 				m_PlayerMovement.DisableCape();
-				m_Cape.Rotate (-m_Rotation);
+				//m_Cape.Rotate (-m_Rotation);
 				gameObject.GetComponent<ZoeyPlayerState>().setExitingSecond(true);
 			}
 		}
@@ -90,7 +91,6 @@ public class Cape : SecondairyBase
 		m_Enabled = true;
 		m_PlayerMovement.ActivateCape ();
 		m_PlayerMovement.setCanMove (true);
-		m_Cape.Rotate (m_Rotation);
 	}
 
 }
