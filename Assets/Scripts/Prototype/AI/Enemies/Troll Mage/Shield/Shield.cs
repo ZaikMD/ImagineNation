b3f1095dccd3ff1cfafa791c3bb7c152502e0001
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-13 15:34:32 +0000

    troll mage shield implemented and looking good

---
+++ Assets/Scripts/Prototype/AI/Enemies/Troll Mage/Shield/Shield.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour 
+{
+	GameObject[] m_Children;
+
+	ObjectCircler m_ObjectCircler;
+
+	public GameObject m_ColliderTexture;
+
+	public void setRadius(float radius)
+	{
+		m_ColliderTexture.gameObject.transform.localScale = new Vector3(radius * 2, radius * 2, radius * 2);
+		m_ObjectCircler.adjustRadius(radius);
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_ObjectCircler = gameObject.GetComponentInChildren<ObjectCircler>();
+		m_Children = new GameObject[transform.childCount];
+		for(int i = 0; i < transform.childCount; i++)
+		{
+			m_Children[i] = transform.GetChild(i).gameObject;
+		}
+
+		setRadius(m_ColliderTexture.transform.localScale.x / 2.0f);
+	}
+	
+	public void deActivate()
+	{
+		for(int i = 0; i < m_Children.Length; i++)
+		{
+			m_Children[i].SetActive(false);
+		}
+	}
+
+	public void reActivate()
+	{
+		for(int i = 0; i < m_Children.Length; i++)
+		{
+			m_Children[i].SetActive(true);
+		}
+	}
+}
