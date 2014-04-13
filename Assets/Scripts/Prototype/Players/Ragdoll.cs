2a8ce6c25189a090c6135643834d147ac2803396
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-13 23:49:14 +0000

    Made prefabs for death ragdolls

--- Assets/Scripts/Prototype/Players/Ragdoll.cs
+++ Assets/Scripts/Prototype/Players/Ragdoll.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ragdoll : MonoBehaviour {
+public class Ragdoll : MonoBehaviour 
+{
+	//used for timer
+	const float TIME = 4.0f;
+	float m_Timer = 0.0f;
 
+	//ghost movement
 	public GameObject m_GhostPrefab;
 	Vector3 m_InitialPos;
 	public float m_GhostRange = 0.0f;
@@ -12,23 +17,49 @@ public class Ragdoll : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	
+		//make the initial position equal to the gameobject position that the script is attached to
 		m_InitialPos = transform.position;
+
+		// Set Timer:
+		m_Timer = TIME; 
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-
+		//if there is a ghost to attached to the gameobject
+		if(m_GhostPrefab != null)
+		{
+		//ghost's position will go up
 		m_GhostPrefab.transform.position = new Vector3 (m_GhostPrefab.transform.position.x, m_GhostPrefab.transform.position.y  + m_Speed * Time.deltaTime, m_GhostPrefab.transform.position.z);
 
 
+			//create a float distance that will be used to check how far the ghost is to see if it should be deleted or not
 		float distance = Vector3.Distance (m_InitialPos, m_GhostPrefab.transform.position);
-		if(distance > m_GhostRange)
-		{
-			Destroy(this.gameObject);
+			if(distance > m_GhostRange)
+			{
+				Destroy(this.gameObject);
+			}
+	
 		}
-	
+
+		//if there are no ghosts attached
+		if (m_GhostPrefab == null)
+		{
+			// Decrement timer
+			m_Timer -= Time.deltaTime;
+
+			//once timer = 0 delete the gameobject
+			if (m_Timer <= 0)
+			{
+				Destroy(this.gameObject);
+			}
+		}
 	}
-	
+
+
+
 }
+
+
