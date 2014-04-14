409d2aeea3a5a6edabc04a06985ce3932ae42f13
holl0161 <holl0161@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-04-14 03:35:55 +0000

    Fixing stuff I guess

---
+++ trunk/Assets/Scripts/Prototype/ArmyMenTalkedTo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmyMenTalkedTo : Subject, Observer
+{
+	public static ArmyMenTalkedTo Instance{ get; private set; }
+	
+	void Awake()
+	{
+		//if theres another instance (there shouldnt be) destroy this... there can be only one
+		if(Instance != null && Instance != this)
+		{
+			//destroy all other instances
+			Destroy(gameObject);
+		}
+		
+		//set the instance
+		Instance = this;
+		
+		//prevents this object being destroyed between scene loads
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void Start()
+	{
+		GameManager.Instance.addObserver (this);
+	}
+
+	public void recieveEvent(Subject sender, ObeserverEvents recievedEvent)
+	{
+		if(recievedEvent == ObeserverEvents.SpokenToArmyMen)
+		{
+			sendEvent(ObeserverEvents.Used);
+		}
+	}
+}
