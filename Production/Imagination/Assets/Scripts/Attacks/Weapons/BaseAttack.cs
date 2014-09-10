7067b42ca547aee793defc39c36097f57d1ed850
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-10 20:24:21 +0000

    Added attack/collectable empty scripts that will be required

---
+++ Production/Imagination/Assets/Scripts/Attacks/Weapons/BaseAttack.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseAttack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
