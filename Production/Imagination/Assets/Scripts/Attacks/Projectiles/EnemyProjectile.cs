e4bfeb53b8f1796829cf2402221c07233e27d457
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-10 21:10:52 +0000

    started projectiles and attacking

--- Production/Imagination/Assets/Scripts/Attacks/Projectiles/EnemyProjectile.cs
+++ Production/Imagination/Assets/Scripts/Attacks/Projectiles/EnemyProjectile.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class EnemyProjectile : MonoBehaviour {
+/// <summary>
+/// Enemy projectile.
+/// 
+/// Inherits from BaseProjectile
+/// 
+/// Give it the type of EnemyProjectile
+/// </summary>
+public class EnemyProjectile : BaseProjectile {
 
 	// Use this for initialization
 	void Start () {
