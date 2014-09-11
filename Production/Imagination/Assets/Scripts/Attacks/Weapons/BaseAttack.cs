be430cb8ce32c507b9f29b36b060ccba8687e3c3
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-11 00:37:00 +0000

    Working on attacking

--- Production/Imagination/Assets/Scripts/Attacks/Weapons/BaseAttack.cs
+++ Production/Imagination/Assets/Scripts/Attacks/Weapons/BaseAttack.cs
@@ -6,16 +6,20 @@ using System.Collections;
 /// </summary>
 public class BaseAttack : MonoBehaviour 
 {
-	GameObject m_Projectile;
+	protected GameObject m_Projectile;
 
 	float m_AttackTimer; //The animation Timer (Hopefully)
 	float m_GraceTimer = 1.0f; //How long the player will have to combo the attack, starts .5 seconds before m_AttackTime ends
 
 	bool m_AnimationDone; //Bool for if the animation is done or not
+	bool m_Attacking;
 
 
 	protected float m_SaveAttackTimer;
 	protected float m_SaveGraceTimer;
+
+	protected Vector3 m_InitialPosition;
+	protected Quaternion m_initialRotation;
 
 	// Use this for initialization
 	void Start () 
@@ -26,23 +30,50 @@ public class BaseAttack : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		if(m_Attacking)
+		{
+			m_AttackTimer -= Time.deltaTime;
+		}
+
+		if(m_AttackTimer <= 0.5f)
+		{
+			m_GraceTimer -= Time.deltaTime;
+		}
 	}
 
-	void startAttack()
+	public void loadPrefab()
 	{
+		m_Projectile = (GameObject)Resources.Load("Prefab_PlayerProjectile", typeof (GameObject));
+	}
+
+	public void startAttack(Vector3 pos, Quaternion rotation)
+	{
+
+
 		//Reset Timers
 		m_AttackTimer = m_SaveAttackTimer;
 		m_GraceTimer = m_SaveGraceTimer;
 
+		m_InitialPosition = pos;
+		m_initialRotation = rotation;
 		//Start Animation
 		//TODO Animation
 
 		createProjectile ();
 	}
 
+	public float getAttackTimer()
+	{
+		return m_AttackTimer;
+	}
+
+	public float getGraceTimer()
+	{
+		return m_GraceTimer;
+	}
+
 	public virtual void createProjectile()
 	{
-		Instantiate (m_Projectile, this.transform.position, this.transform.rotation);
+		Instantiate (m_Projectile,m_InitialPosition, transform.rotation);
 	}
 }
