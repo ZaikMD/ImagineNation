7012b14a40c9afd65749a0ed933e93d18b3c11a7
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-12 20:18:31 +0000

    started movement script for alex

---
+++ trunk/Production/Imagination/Assets/Scripts/Movement/AlexMovementMoonBoots.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlexMovementMoonBoots : BaseMovementAbility {
+
+	private const float MOON_BOOTS_JUMP = 15.0f;
+	// Use this for initialization
+	void Start () {
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.Update ();
+	
+	}
+
+	protected override void HeldAirMovement ()
+	{
+		base.HeldAirMovement ();
+	}
+}
