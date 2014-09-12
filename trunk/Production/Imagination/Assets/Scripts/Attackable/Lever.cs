86b7813d5802aa8ac974bbd33cce89999322de46
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-12 20:02:03 +0000

    Added Lever.cs

---
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Lever.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lever : MonoBehaviour, Attackable
+{
+
+    public bool m_OnTimer;
+    public float m_Timer;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
