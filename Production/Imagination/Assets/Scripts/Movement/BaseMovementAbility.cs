b540e29574a4e444304369204cdb95b4e72df557
whit0717 <whit0717@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-12 16:29:51 +0000

    Started BaseMovementAbility and created Whitlaw TestScene.unity

---
+++ Production/Imagination/Assets/Scripts/Movement/BaseMovementAbility.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class BaseMovementAbility : MonoBehaviour 
+{
+	public CharacterController m_CharacterController;
+	protected float m_VerticalVelocity;
+	protected const float JUMP_SPEED = 15.0f;
+	protected const float MAX_FALL_SPEED = 15.0f;
+	protected const float FALL_ACCELERATION = 2.0f;
+	private bool m_CurrentlyJumping;
+
+	void Start () 
+	{
+		m_CharacterController = GetComponent<CharacterController> ();
+		m_VerticalVelocity = 0.0f;
+		m_CurrentlyJumping = false;
+	}
+
+	void Update () 
+	{
+		if(InputManager.getJumpUp)
+		{
+			m_CurrentlyJumping = false;
+		}
+
+		if(GetIsGrounded())
+		{
+			if(InputManager.getJumpDown)
+			{
+				Jump();
+				m_CurrentlyJumping = true;
+			}
+			else
+			{
+				m_VerticalVelocity = 0.0f;
+			}
+		}
+		else
+		{
+			if(InputManager.getJump && m_CurrentlyJumping == true)
+			{
+				AirMovement();
+			}
+			else
+			{
+				m_VerticalVelocity = 0.0f;
+			}
+		}
+	}
+
+	protected abstract void Jump()
+	{
+		m_VerticalVelocity = JUMP_SPEED;
+	}
+
+	protected abstract void AirMovement()
+	{
+		if(m_VerticalVelocity > MAX_FALL_SPEED)
+		{
+			m_VerticalVelocity -= Time.deltaTime * FALL_ACCELERATION;
+		}
+
+		m_CharacterController.Move (transform.up * m_VerticalVelocity * Time.deltaTime);
+	}
+
+	public bool GetIsGrounded()
+	{
+		return m_CharacterController.isGrounded;
+	}
+}
