407b25e3382eb7d30fc5d69f6e90560f936a729a
tack0012 <tack0012@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-12 18:30:07 +0000

    Fixed movement for new input methods

--- Production/Imagination/Assets/Scripts/Movement/PlayerMovement.cs
+++ Production/Imagination/Assets/Scripts/Movement/PlayerMovement.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(AcceptInputFrom))]
 
 public class PlayerMovement : MonoBehaviour {
 
     //Component pointers
     private CharacterController m_Controller;
     private Transform m_Camera;
-
-    public GamepadInput.GamePad.Index m_Index;
+    private AcceptInputFrom m_Accepted;
+    
     public float m_Speed;
     
 	// Use this for initialization
@@ -18,21 +19,19 @@ public class PlayerMovement : MonoBehaviour {
 
         m_Controller = GetComponent<CharacterController>();
         m_Camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
-
+        m_Accepted = GetComponent<AcceptInputFrom>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         Movement();   
- 
-        print(InputManager.getMove(m_Index));
-	}
+    }
 
     Vector3 GetProjection()
     {
-        Vector3 projection = m_Camera.forward * InputManager.getMove(m_Index).y;
-        projection += m_Camera.right * InputManager.getMove(m_Index).x;
+        Vector3 projection = m_Camera.forward * InputManager.getMove(m_Accepted.ReadInputFrom).y;
+        projection += m_Camera.right * InputManager.getMove(m_Accepted.ReadInputFrom).x;
 
         projection.y = 0;
         return projection.normalized;        
@@ -43,7 +42,7 @@ public class PlayerMovement : MonoBehaviour {
         Vector3 move = Vector3.zero;
         m_Controller.SimpleMove(move);
 
-        if (InputManager.getMove(m_Index) == Vector2.zero)
+        if (InputManager.getMove(m_Accepted.ReadInputFrom) == Vector2.zero)
          {
            return;
          }
