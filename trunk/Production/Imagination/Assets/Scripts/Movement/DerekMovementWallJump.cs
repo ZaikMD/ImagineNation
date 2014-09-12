6a27380f68631213dca1fd8bf943db384b49542c
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-12 22:24:05 +0000

    Working on Wall Jump

--- trunk/Production/Imagination/Assets/Scripts/Movement/DerekMovementWallJump.cs
+++ trunk/Production/Imagination/Assets/Scripts/Movement/DerekMovementWallJump.cs
@@ -4,14 +4,18 @@ using System.Collections;
 public class DerekMovementWallJump : BaseMovementAbility
 {
 	private const float MAX_WALL_HANG = 2.0f;
-	private const float WALL_HANG_FALL_ACCELERATION = 3.0f;
+	private const float WALL_MAX_FALL_SPEED = -5.0f;
+	private const float WALL_JUMP_SPEED_VERTICAL = 25.0f;
 	private float m_WallHangTimer = 0.0f;
+	private GameObject[] m_Player;
 
 	private bool m_OnWall = false;
 
 	// Use this for initialization
 	void Start () 
 	{
+		m_Player = GameObject.FindGameObjectsWithTag ("Player");
+
 		base.Start ();
 	}
 	
@@ -21,17 +25,18 @@ public class DerekMovementWallJump : BaseMovementAbility
 		if(m_OnWall)
 		{
 			m_WallHangTimer += Time.deltaTime;
-			HeldAirMovement();
+ 			HeldAirMovement();
 			if(m_WallHangTimer >= MAX_WALL_HANG)
 			{
-				base.Update	();
+				m_WallHangTimer = 0.0f;
+				m_OnWall = false;
 			}
 			else
 			{
-				if(InputManager.getJumpDown(/*m_AcceptInputFrom*/))
+				if(InputManager.getJumpDown(m_AcceptInputFrom.ReadInputFrom))
 				{
-					transform.Rotate(0.0f, 180.0f, 0.0f);
-					Jump ();
+					JumpOffWall ();
+					m_OnWall = false;
 				}
 			}
 		}
@@ -43,19 +48,25 @@ public class DerekMovementWallJump : BaseMovementAbility
 
 	protected override void HeldAirMovement()
 	{
-		if(m_VerticalVelocity > MAX_FALL_SPEED)
+		if(m_VerticalVelocity > WALL_MAX_FALL_SPEED)
 		{
-			m_VerticalVelocity -= Time.deltaTime * WALL_HANG_FALL_ACCELERATION;
+			m_VerticalVelocity -= Time.deltaTime * HELD_FALL_ACCELERATION;
 		}
 		
 		m_CharacterController.Move (transform.up * m_VerticalVelocity * Time.deltaTime);
 	}
 
-	void OnCollisionEnter(Collision other)
+	void OnControllerColliderHit(ControllerColliderHit other)
 	{
-		if(other.gameObject.CompareTag("Wall"))
+		if(other.gameObject.CompareTag("Wall") && !m_CharacterController.isGrounded)
 		{
 			m_OnWall = true;
 		}
+	}
+
+	void JumpOffWall()
+	{
+		m_VerticalVelocity = WALL_JUMP_SPEED_VERTICAL;
+		transform.Rotate(0.0f, 180.0f, 0.0f);
 	}
 }
