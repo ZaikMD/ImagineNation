40b5d34304cadb26c789454de97c0b86cd3680ba
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-12 22:19:58 +0000

    Made it so alex if jump is held down will jump higher than everyone else

--- Production/Imagination/Assets/Scripts/Movement/AlexMovementMoonBoots.cs
+++ Production/Imagination/Assets/Scripts/Movement/AlexMovementMoonBoots.cs
@@ -3,20 +3,52 @@ using System.Collections;
 
 public class AlexMovementMoonBoots : BaseMovementAbility {
 
-	private const float MOON_BOOTS_JUMP = 15.0f;
+
+	private bool isSuperJump;
+	private const float MOON_BOOTS_JUMP = 5.0f;
 	// Use this for initialization
 	void Start () {
+		isSuperJump = false;
 		base.Start ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if(InputManager.getJumpUp(m_AcceptInputFrom.ReadInputFrom))
+		{
+			isSuperJump = false;
+		}
+		else 
+		{
+			if(InputManager.getJump(m_AcceptInputFrom.ReadInputFrom) && m_CurrentlyJumping == true)
+			{
+				isSuperJump = true;
+			}
+		}
 		base.Update ();
 	
 	}
+	
 
 	protected override void HeldAirMovement ()
 	{
-		base.HeldAirMovement ();
+		//isSuperJump = false;
+		if(m_VerticalVelocity > MAX_FALL_SPEED)
+		{
+
+			m_VerticalVelocity -= Time.deltaTime * HELD_FALL_ACCELERATION;
+		}
+
+		if (isSuperJump == true)
+		{
+			m_CharacterController.Move (transform.up * (m_VerticalVelocity + MOON_BOOTS_JUMP) * Time.deltaTime);
+		}
+
+		else
+		{
+			m_CharacterController.Move (transform.up * m_VerticalVelocity * Time.deltaTime);
+		}
 	}
 }
