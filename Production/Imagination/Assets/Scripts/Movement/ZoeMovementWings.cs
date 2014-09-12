0771d6dfe25b0d5a7ddfc652c3375ee0fe76a387
whit0717 <whit0717@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-12 22:22:03 +0000

    More debugging on winged jump

--- Production/Imagination/Assets/Scripts/Movement/ZoeMovementWings.cs
+++ Production/Imagination/Assets/Scripts/Movement/ZoeMovementWings.cs
@@ -6,7 +6,8 @@ public class ZoeMovementWings : BaseMovementAbility {
 	private bool m_CanGlide;
 	private bool m_IsGliding;
 	private int m_NumberOfJumps;
-	private const float GLIDE_FALL_ACCELERATION = 2.0f;
+	private const float GLIDE_FALL_ACCELERATION = 1.0f;
+	private const float GLIDE_MAX_FALL_SPEED = -0.001f;
 
 	// Use this for initialization
 	void Start () 
@@ -25,9 +26,10 @@ public class ZoeMovementWings : BaseMovementAbility {
 		{
 			m_CanGlide = true;
 			m_NumberOfJumps = 0;
+			m_IsGliding = false;
 		}
 
-		if(InputManager.getJumpUp(m_AcceptInputFrom.ReadInputFrom))
+		if(InputManager.getJumpDown(m_AcceptInputFrom.ReadInputFrom))
 		{
 			m_NumberOfJumps++;
 			if(m_NumberOfJumps == 2)
@@ -43,7 +45,11 @@ public class ZoeMovementWings : BaseMovementAbility {
 
 		if(m_IsGliding && InputManager.getJump(m_AcceptInputFrom.ReadInputFrom))
 		{
-			HeldAirMovement();
+			if(m_VerticalVelocity < GLIDE_MAX_FALL_SPEED)
+			{
+				m_VerticalVelocity = GLIDE_MAX_FALL_SPEED;
+			}
+ 			HeldAirMovement();
 		}
 		else
 		{
@@ -53,9 +59,9 @@ public class ZoeMovementWings : BaseMovementAbility {
 
 	protected override void HeldAirMovement()
 	{
-		if(m_VerticalVelocity > MAX_FALL_SPEED)
+		if(m_VerticalVelocity >= GLIDE_MAX_FALL_SPEED)
 		{
-			m_VerticalVelocity -= Time.deltaTime * HELD_FALL_ACCELERATION;
+			m_VerticalVelocity -= Time.deltaTime * GLIDE_FALL_ACCELERATION;
 		}
 		
 		m_CharacterController.Move (transform.up * m_VerticalVelocity * Time.deltaTime);
