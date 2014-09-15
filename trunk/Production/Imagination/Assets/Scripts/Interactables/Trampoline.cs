9b64ad548f57533128611054d1b039fdaa67b624
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-15 20:57:53 +0000

    Started trampoline, commented alex movement boots script

--- trunk/Production/Imagination/Assets/Scripts/Interactables/Trampoline.cs
+++ trunk/Production/Imagination/Assets/Scripts/Interactables/Trampoline.cs
@@ -3,13 +3,40 @@ using System.Collections;
 
 public class Trampoline : MonoBehaviour {
 
+	public GameObject m_JumpGameObject;
+
+	Vector3 m_TrampolinePosition = Vector3.zero;
+	Vector3 m_LaunchDirection = Vector3.zero;
+	//Vector3 m_JumpPosition = Vector3.zero;
+
 	// Use this for initialization
 	void Start () {
+
+		m_TrampolinePosition = gameObject.transform.position;
+		Debug.Log (m_TrampolinePosition);
+		//m_JumpGameObject.transform.position; //gameObject.transform.position;
+		Debug.Log (m_JumpGameObject.transform.position);
+
+		Vector3 m_LaunchDirection = new Vector3 ((m_JumpGameObject.transform.position.x - m_TrampolinePosition.x), (m_JumpGameObject.transform.position.y - m_TrampolinePosition.y), 
+		                                         (m_JumpGameObject.transform.position.z - m_TrampolinePosition.z));
+
+		 m_LaunchDirection =  m_JumpGameObject.transform.position - gameObject.transform.position;
+		Debug.Log (m_LaunchDirection);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if (m_JumpGameObject != null)
+		{
+
+		}
+
+	}
+
+	void onTriggerEnter(Collider other)
+	{
+
 	}
 }
--- trunk/Production/Imagination/Assets/Scripts/Movement/AlexMovementMoonBoots.cs
+++ trunk/Production/Imagination/Assets/Scripts/Movement/AlexMovementMoonBoots.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class AlexMovementMoonBoots : BaseMovementAbility {
 
-
+	//a boolean that checks if Alex is doing a super jump
 	private bool isSuperJump;
+	//value of super moon boot jump
 	private const float MOON_BOOTS_JUMP = 5.0f;
+
 	// Use this for initialization
 	void Start () {
+		//The isSuperJump starts as false and then calls the base start of BaseMovementAbility
 		isSuperJump = false;
 		base.Start ();
 
@@ -16,36 +19,42 @@ public class AlexMovementMoonBoots : BaseMovementAbility {
 	// Update is called once per frame
 	void Update () {
 
+		//If The player releases the jump key then isSuperJump will turn to false
 		if(InputManager.getJumpUp(m_AcceptInputFrom.ReadInputFrom))
-		{
+		{ 
 			isSuperJump = false;
 		}
 		else 
 		{
+			//If the player did not release the jump key, it will check if the button is being held down. If yes then isSuperJump is true
 			if(InputManager.getJump(m_AcceptInputFrom.ReadInputFrom) && m_CurrentlyJumping == true)
 			{
 				isSuperJump = true;
 			}
 		}
+		//then calls base Update script from BaseMovementAbility
 		base.Update ();
 	
 	}
 	
-
+	//HeldAirMovement needs to be overriden because the values of held air movement of Alex is Higher than anyone else's
 	protected override void HeldAirMovement ()
 	{
-		//isSuperJump = false;
+		//When the m_VerticalVolicty is greater than MAX_FALL_SPEED (which it is from the start). A downwards force is applied. (Same as BaseMovementAbility script).
 		if(m_VerticalVelocity > MAX_FALL_SPEED)
 		{
 
 			m_VerticalVelocity -= Time.deltaTime * HELD_FALL_ACCELERATION;
 		}
 
+		//If isSuperJump is true, the equation for his upwards velocity is increased by the value of MOON_BOOTS_JUMP). Currently the way BaseMovementAbility works,
+		//isSuper will be true the second the button is pressed, so Alex will have a slightly higher jump than other players even though the button is only tapped
 		if (isSuperJump == true)
 		{
 			m_CharacterController.Move (transform.up * (m_VerticalVelocity + MOON_BOOTS_JUMP) * Time.deltaTime);
 		}
 
+		//if isSuperJump is False then is does exactly what the BaseMovementAbility is sending the player upwards.
 		else
 		{
 			m_CharacterController.Move (transform.up * m_VerticalVelocity * Time.deltaTime);
