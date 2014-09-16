3fa45e5ae313d6ca85e6f1b652cd7f10a998ca78
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-16 20:43:53 +0000

    Working on Moving Blocks

--- trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/MovingBlock.cs
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/MovingBlock.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof (CharacterController))]
+
 public class MovingBlock : Destructable
 {
 
-	public GameObject m_Respawn;
+	Vector3 m_Respawn;
 
 	public float m_Distance;
 
@@ -17,11 +19,20 @@ public class MovingBlock : Destructable
 	bool m_Hit = false;
 	bool m_Moving = false;
 
+	protected int m_SaveHealth;
+
 
 	// Use this for initialization
 	void Start () 
 	{
 		gameObject.renderer.material = m_Materials [m_CurrentMaterial];
+
+		m_Respawn = transform.position;
+
+		m_SaveHealth = m_Health;
+
+		m_Destination = transform.position;
+
 	}
 	
 	// Update is called once per frame
@@ -37,19 +48,30 @@ public class MovingBlock : Destructable
 		{
 			respawn();
 		}
+
+		transform.position = m_Destination;
 	}
 
 	void respawn()
 	{
 		m_CurrentMaterial = 0;
-		transform.position = m_Respawn.transform.position;
+		transform.position = m_Respawn;
+
+		gameObject.renderer.material = m_Materials [m_CurrentMaterial];
+		m_Health = m_SaveHealth;
 	}
 
 	public override void onHit(PlayerProjectile proj)
 	{
 		if(!m_Hit)
 		{
+			m_Health --;
 			m_CurrentMaterial ++;
+
+			if(m_CurrentMaterial >= m_Materials.Length)
+			{
+				m_CurrentMaterial = 0;
+			}
 			gameObject.renderer.material = m_Materials [m_CurrentMaterial];
 			m_Hit = true;
 		}
@@ -62,11 +84,57 @@ public class MovingBlock : Destructable
 
 	void OnTriggerEnter(Collider obj)
 	{
-		GameObject newObj = obj.gameObject;
+
+		if(obj.gameObject.tag == "PlayerProjectile")
+		{
+			GameObject newObj = obj.gameObject;
+			setDestination (obj.gameObject);
+		}
 	}
 
 	void setDestination(GameObject obj)
 	{
+		Vector3 rayDirection = transform.position - obj.transform.position;
+
+		Ray ray = new Ray(obj.transform.position, rayDirection);
+
+		RaycastHit rayHit;
+
+
+		Physics.Raycast (ray, out rayHit);
+
+
+
+		Vector3 normal = rayHit.normal;
+
+		normal = rayHit.transform.TransformDirection (normal);
+
+
+
+		if(normal == rayHit.transform.right)
+		{
+			//Hit right side of bock
+			m_Destination = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+		}
+
+		if(normal == -rayHit.transform.right)
+		{
+			//hit left side
+			m_Destination = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+		}
+
+		if(normal == rayHit.transform.forward)
+		{
+			//hit left side
+			m_Destination = new Vector3(transform.position.x , transform.position.y, transform.position.z - 1);
+		}
+
+		if(normal == -rayHit.transform.forward)
+		{
+			//hit left side
+			m_Destination = new Vector3(transform.position.x , transform.position.y, transform.position.z + 1);
+		}
+
 
 	}
 
