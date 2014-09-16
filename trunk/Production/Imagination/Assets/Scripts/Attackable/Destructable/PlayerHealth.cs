ce88eb4c225da89de0e8f724a45c138e0d9020eb
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-16 21:14:16 +0000

    starting health stuff

---
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/PlayerHealth.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GUITexture))]
+
+public class PlayerHealth : Destructable 
+{
+	public Texture[] textures;
+
+	public float HealthRegenTime = 5.0f;
+	float m_HealthRegenTimer;
+
+	public float StopHealthRegenTime = 20.0f;
+	float m_StopHealthRegenTimer;
+
+	int m_TotalHealth;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_HealthRegenTimer = HealthRegenTime;
+		m_StopHealthRegenTimer = StopHealthRegenTime;
+		m_TotalHealth = m_Health;
+	}
+	
+	// Update is called once per frame
+	protected void Update () 
+	{
+		if (m_Health <= 0)
+		{
+			Instantiate(m_Ragdoll, transform.position, transform.rotation);
+			//TODO: Hide this object
+			//TODO: tell Dead player Manager that this player is dead
+		}
+		else
+		{
+			if(m_Health < m_TotalHealth)
+			{
+				if(m_StopHealthRegenTimer < 0)
+				{
+					if(m_HealthRegenTimer < 0)
+					{
+						m_Health++;
+						m_HealthRegenTimer = HealthRegenTime;
+					}
+					else
+					{
+						m_HealthRegenTimer-= Time.deltaTime;
+					}
+				}
+			}
+
+			if(m_StopHealthRegenTimer > 0)
+			{
+				m_StopHealthRegenTimer -= Time.deltaTime;
+			}
+		}
+	}
+
+	public virtual void onHit(PlayerProjectile proj)
+	{        
+		return;
+	}
+	
+	public virtual void onHit(EnemyProjectile proj)
+	{
+		m_Health -= 1;  
+		m_StopHealthRegenTimer = StopHealthRegenTime;
+	}
+}
