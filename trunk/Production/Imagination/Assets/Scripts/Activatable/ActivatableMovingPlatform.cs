4a2dbb8f008f0fec2e6a6ede886992eaef10eebd
whit0717 <whit0717@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-19 21:44:10 +0000

    Finished Activatable Finished Activatable/ActivatableLight.cs Began Activatable/ActivatableMovingPlatform.cs

--- trunk/Production/Imagination/Assets/Scripts/Activatable/ActivatableMovingPlatform.cs
+++ trunk/Production/Imagination/Assets/Scripts/Activatable/ActivatableMovingPlatform.cs
@@ -5,19 +5,68 @@ public class ActivatableMovingPlatform : Activatable
 {
 	bool m_CanBeActivated;
 	bool m_IsActive;
+
 	bool m_AtFinalDestination;
+	bool m_AtDestination;
 	bool m_IsReversing;
-	float m_AtCheckPointTimer;
+
+	float m_AtDestinationTimer;
+	public float m_TimerLimit;
+
+	public float m_PlatformSpeed;
+	float m_DistanceToNextPlatform;
+
 	public GameObject[] m_ForwardDestinations;
 	public GameObject[] m_ReverseDestinations;
+	GameObject m_CurrentDestination;
+	GameObject m_NextDestination;
+	int m_DestinationCounter;
 
 	void Start () 
 	{
-	
+		m_PlatformSpeed = 3.0f;
+		m_TimerLimit = 3.0f;
+		m_DestinationCounter = 0;
+		m_CanBeActivated = true;
+		m_IsActive = true;
 	}
 
 	void Update () 
 	{
-	
+		if(CheckSwitches())
+		{
+			if(m_IsActive)
+			{
+				MoveToDestination();
+
+				if(m_DistanceToNextPlatform <= 0.5f)
+				{
+					m_IsActive = false;
+				}
+			}
+			else
+			{
+				m_AtDestinationTimer += Time.deltaTime;
+
+				if(m_AtDestinationTimer >= m_TimerLimit)
+				{
+					m_AtDestinationTimer = 0.0f;
+					m_DestinationCounter++;
+					m_IsActive = true;
+				}
+			}
+		}
 	}
+
+	void MoveToDestination()
+	{
+		Vector3 currentPosition = transform.position;
+		Vector3 destinationPosition = m_ForwardDestinations [m_DestinationCounter].transform.position;
+
+		Vector3 destinationDirection = destinationPosition - currentPosition;
+		m_DistanceToNextPlatform = destinationDirection.magnitude;
+
+		transform.position += destinationDirection.normalized * m_PlatformSpeed * Time.deltaTime;
+	}
+
 }
