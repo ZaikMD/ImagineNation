e7004b8ff913a94656b36d766643e3a6f4d443a9
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-19 20:08:26 +0000

    Finished moving blocks and pressure plates

--- Production/Imagination/Assets/Scripts/Interactables/PressurePlates.cs
+++ Production/Imagination/Assets/Scripts/Interactables/PressurePlates.cs
@@ -1,15 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PressurePlates : MonoBehaviour {
+public class PressurePlates : MonoBehaviour 
+{
+
+	bool m_Activated = false;
+
+	ArrayList m_List;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		m_List = new ArrayList ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+
+
+		if(m_List.Count > 0 )
+		{
+			m_Activated = true;
+			Debug.Log(m_Activated);
+		}
+	}
+
+	void OnTriggerEnter(Collider obj)
+	{
+		if(obj.gameObject.tag == "Player")
+		{
+			m_List.Add(obj.gameObject);
+		}
+
+		if(obj.gameObject.tag == "MovingBlock")
+		{
+			m_List.Add(obj.gameObject);
+		}
+	}
+
+	void OnTriggerExit(Collider obj)
+	{
+		if(obj.gameObject.tag == "Player")
+		{
+			m_List.Remove(obj.gameObject);
+		}
+		
+		if(obj.gameObject.tag == "MovingBlock")
+		{
+			m_List.Remove(obj.gameObject);
+		}
+	}
+
+	bool getActivated()
+	{
+		return m_Activated;
 	}
 }
