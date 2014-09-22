8533d3997b5a7fa63d6bd4537f127e412412b829
whit0717 <whit0717@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-22 20:59:45 +0000

    Worked on moving platform. Added reversing functionality

--- Production/Imagination/Assets/Scripts/Activatable/ActivatableMovingPlatform.cs
+++ Production/Imagination/Assets/Scripts/Activatable/ActivatableMovingPlatform.cs
@@ -16,8 +16,7 @@ public class ActivatableMovingPlatform : Activatable
 	public float m_PlatformSpeed;
 	float m_DistanceToNextPlatform;
 
-	public GameObject[] m_ForwardDestinations;
-	public GameObject[] m_ReverseDestinations;
+	public GameObject[] m_Destinations;
 	GameObject m_CurrentDestination;
 	GameObject m_NextDestination;
 	int m_DestinationCounter;
@@ -33,25 +32,59 @@ public class ActivatableMovingPlatform : Activatable
 
 	void Update () 
 	{
+		//Platforms can only move if the switches are active
 		if(CheckSwitches())
 		{
+			//If the platform is moving from one destination to another
 			if(m_IsActive)
 			{
-				MoveToDestination();
+				MoveToDestination();//Move the platform toward its destination
 
+				//If the distance to the next platform is smaller than then the minimum required distance
 				if(m_DistanceToNextPlatform <= 0.5f)
 				{
+					//Check if the current destination is at the last element,
+					//if it is start reversing.
+					if(m_DestinationCounter == (m_Destinations.Length -1))
+					{
+						m_IsReversing = true;
+					}
+
+					//If the current destination is the first element,
+					//then move the platform forwards through the array
+					if(m_DestinationCounter == 0)
+					{
+						m_IsReversing = false;
+					}
+
+					//Set inactive, and allow the platform to pause at each destination
 					m_IsActive = false;
 				}
 			}
-			else
+			else //If the platform is at destination
 			{
+				//Increment the timer while at the destination
 				m_AtDestinationTimer += Time.deltaTime;
 
+				//If the timer reached the limit
 				if(m_AtDestinationTimer >= m_TimerLimit)
 				{
+					//Reset the timer
 					m_AtDestinationTimer = 0.0f;
-					m_DestinationCounter++;
+
+					//Determine whether to increment or decrement
+					//the destination based on whether the path is 
+					//going forwards or reversing.
+					if(!m_IsReversing)
+					{
+						m_DestinationCounter++;
+					}
+					else
+					{
+						m_DestinationCounter--;
+					}
+
+					//Set to active and start moving the platform to the next destination
 					m_IsActive = true;
 				}
 			}
@@ -60,12 +93,17 @@ public class ActivatableMovingPlatform : Activatable
 
 	void MoveToDestination()
 	{
+		//Get the two positions required for calculation, the platforms current position
+		//and the next destinations position
 		Vector3 currentPosition = transform.position;
-		Vector3 destinationPosition = m_ForwardDestinations [m_DestinationCounter].transform.position;
+		Vector3 destinationPosition = m_Destinations [m_DestinationCounter].transform.position;
 
+		//Get the direction vector between them
 		Vector3 destinationDirection = destinationPosition - currentPosition;
+		//Get the magnitude of that direction to use for a proximity check once close enough
 		m_DistanceToNextPlatform = destinationDirection.magnitude;
 
+		//Move the platform along that direction over time.
 		transform.position += destinationDirection.normalized * m_PlatformSpeed * Time.deltaTime;
 	}
 
