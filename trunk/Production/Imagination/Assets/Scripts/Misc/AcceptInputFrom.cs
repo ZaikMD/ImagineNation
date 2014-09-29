c16a5223d3c31ff36bb31e8be0c6bbc3b41257e3
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-09-29 20:23:02 +0000

    cameras do split screen

--- trunk/Production/Imagination/Assets/Scripts/Misc/AcceptInputFrom.cs
+++ trunk/Production/Imagination/Assets/Scripts/Misc/AcceptInputFrom.cs
@@ -5,4 +5,14 @@ using System.Collections;
 public class AcceptInputFrom : MonoBehaviour 
 {
 	public PlayerInput ReadInputFrom = PlayerInput.GamePadOne;
+
+    public AcceptInputFrom CopySettingFrom;
+
+    void Start()
+    {
+        if (CopySettingFrom != null)
+        {
+            ReadInputFrom = CopySettingFrom.ReadInputFrom;
+        }
+    }
 }
