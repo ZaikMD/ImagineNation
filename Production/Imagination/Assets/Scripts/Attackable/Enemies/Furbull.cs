96c0c81db3aaab9c7c9d00e2ec99c00c1aacca55
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-01 21:05:38 +0000

    Furbulls Finished, Base Enemy Updates, and Base Enemy Group Functionality in progress.

--- Production/Imagination/Assets/Scripts/Attackable/Enemies/Furbull.cs
+++ Production/Imagination/Assets/Scripts/Attackable/Enemies/Furbull.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class Furbull : BaseEnemy 
 {
-    private const float FURBULL_ATTACK_DELAY = 2.0f;
-    EnemyProjectile m_EnemyProjectile;
-
+    public EnemyProjectile EnemyProjectile;
+    private const float FURBULL_ATTACK_DELAY = 1.5f;
+    
 	// Use this for initialization
 	void Start () 
     {
-        m_AttackTimer = FURBULL_ATTACK_DELAY;
+        m_AttackTimer = 0.0f;
 		m_CombatRange = 1.5f;
 
 		base.Start ();
@@ -18,17 +18,18 @@ public class Furbull : BaseEnemy
 	// Update is called once per frame
     void Update () 
     {
-		UpdateState ();
+        base.Update();
 	}
 
     protected override void Fight()
     {
+        m_Agent.stoppingDistance = m_InitialStoppingDistance;
+
         if (m_Target != null)
         {
             if (m_AttackTimer <= 0.0f)
             {
-                //Projectile Needed
-                //Flag Damage to Player
+                ShootProjectile();
                 m_AttackTimer = FURBULL_ATTACK_DELAY;
             }
             else
@@ -44,5 +45,10 @@ public class Furbull : BaseEnemy
         m_IsAlive = false;
         //Instantiate Ragdoll
         Destroy(this.gameObject);
+    }
+
+    private void ShootProjectile()
+    {
+        Instantiate(EnemyProjectile.gameObject, transform.position, transform.rotation);
     }
 }
