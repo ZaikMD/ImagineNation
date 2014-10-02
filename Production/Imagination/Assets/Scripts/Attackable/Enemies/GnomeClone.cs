6970fb7dd8ed10f59d4f78fdd6f515753b29f546
elia0034 <elia0034@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-02 00:27:21 +0000

    Fixed the gnome clones

--- Production/Imagination/Assets/Scripts/Attackable/Enemies/GnomeClone.cs
+++ Production/Imagination/Assets/Scripts/Attackable/Enemies/GnomeClone.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class GnomeClone : MonoBehaviour 
 {
-	private GameObject m_Target;
+	private Transform m_Target;
 	public GameObject m_ProjectilePrefab;
-	
+	private float m_TimeBetweenShots = 1.5f;
+	private float m_ShotTimer = 0.0f;
 
 	// Use this for initialization
 	void Start () 
@@ -16,13 +17,18 @@ public class GnomeClone : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.LookAt(m_Target.transform.position);
+		transform.LookAt(m_Target.position);
 
-		if (m_ProjectilePrefab != null)
-			Instantiate (m_ProjectilePrefab, transform.position, transform.rotation);
+		m_ShotTimer -= Time.deltaTime;
+		if (m_ShotTimer <= 0)
+		{
+			if (m_ProjectilePrefab != null)
+				Instantiate (m_ProjectilePrefab, transform.position, transform.rotation);
+			m_ShotTimer = m_TimeBetweenShots;
+		}
 	}
 
-	public void SetTarget(GameObject target)
+	public void SetTarget(Transform target)
 	{
 		m_Target = target;
 	}
