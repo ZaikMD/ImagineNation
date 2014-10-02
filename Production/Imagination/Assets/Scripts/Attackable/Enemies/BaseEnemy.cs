76eebd615383e9564342d072b2c91e3f0c194077
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-02 22:07:44 +0000

    Updated and comment Base Enemy and Furbulls

--- Production/Imagination/Assets/Scripts/Attackable/Enemies/BaseEnemy.cs
+++ Production/Imagination/Assets/Scripts/Attackable/Enemies/BaseEnemy.cs
@@ -2,15 +2,15 @@
 using System.Collections;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public abstract class BaseEnemy : MonoBehaviour, Attackable
+public abstract class BaseEnemy : Destructable
 {
     protected enum State
     {
         Default,
+        Idle,
         Fight,
         Chase,
         Patrol,
-        Idle,
     }
 
     //Boolean to check if the enemy is Alive
@@ -23,8 +23,6 @@ public abstract class BaseEnemy : MonoBehaviour, Attackable
     //Check to see what State the StateMachine is in
     protected State m_State;
 
-    //Health for combat damage
-    protected float m_Health = 1.0f;
     //Aggro range for distance when the enemy chases the player
     protected float m_AggroRange = 20.0f;
     //Combat range for how close the enemy has to be to hit the player, differs for enemies
@@ -80,10 +78,10 @@ public abstract class BaseEnemy : MonoBehaviour, Attackable
 
 		//Find both the players based off their tag
 		m_Players = GameObject.FindGameObjectsWithTag ("Player");
-
+        
         //Set Initial Stopping Distance for the NavMeshAgent
         m_InitialStoppingDistance = m_Agent.stoppingDistance;
-
+        
 		//Set to make sure CombatRange does not exceed Aggro Range
         if (m_AggroRange <= m_CombatRange)
         {
@@ -96,6 +94,7 @@ public abstract class BaseEnemy : MonoBehaviour, Attackable
 	
 	public void Reset()
     {
+        //Reset Function to bring the enemy back to a default state
         m_InCombat = false;
         m_IsAlive = true;
         m_Target = null;
@@ -106,39 +105,33 @@ public abstract class BaseEnemy : MonoBehaviour, Attackable
     // Update is called once per frame
     protected void Update()
     {
-		if (m_Health <= 0.0f)
-        {
-            Die();
-        }
-
+        //Call our Update State function
 		UpdateState ();
     }
 
 	protected void UpdateState()
 	{
+        //Check if Enemy is alive then run our State Machine
 		if (m_IsAlive)
 		{
+            //Case for each State the Enemy can be in, and calls
+            //the corresponding function for each state
 			switch (m_State)
 			{
 				case State.Default:
 					Default();
-					Debug.Log("Default");
 					break;
 				case State.Idle:
 					Idle();
-					Debug.Log("Idle");
 					break;
+                case State.Fight:
+                    Fight();
+                    break;
 				case State.Chase:
 					Chase();
-					Debug.Log("Chase");
-					break;
-				case State.Fight:
-					Fight();
-					Debug.Log("Fight");
-					break;
+                    break;
 				case State.Patrol:
 					Patrol();
-					Debug.Log("Patrol");
 					break;
 				default:
 					break;
@@ -148,29 +141,89 @@ public abstract class BaseEnemy : MonoBehaviour, Attackable
 
     protected virtual void Default()
     {
+        //Check if the enemy is in combat
         if (m_InCombat)
         {
+            //Cycle through our enemy if it is considered a group
+            for (int enemyIndex = 0; enemyIndex < GroupOfEnemies.Length; enemyIndex++)
+            {
+                //Check if the elements of the group array are null
+                if (GroupOfEnemies[enemyIndex] != null)
+                {
+                    //Cycle through our Player objects
+                    for (int i = 0; i < m_Players.Length; i++)
+                    {
+                        //Check if our player is the target or not
+                        if (m_Players[i] != m_Target)
+                        {
+                            //If the player isnt the target, check the distance between the enemy and the player
+                            float distance = Vector3.Distance(gameObject.transform.position, m_Players[i].transform.position);
+                            
+                            //Check the distance against the enmy target
+                            if (GetDistanceToTarget() >= distance)
+                            {
+                                //If the target is greater or equal to the distance of the player, set our target to the player
+                                m_Target = m_Players[i].gameObject.transform;
+
+                                //Check if the target is within aggro range
+                                if (GetDistanceToTarget() <= m_AggroRange)
+                                {
+                                    //Check Combat Range of the target
+                                    if (GetDistanceToTarget() <= m_CombatRange)
+                                    {
+                                        //Enter fight state
+                                        GroupOfEnemies[enemyIndex].SetState(State.Fight);
+
+                                        //Reset combat timer
+                                        m_CombatTimer = EXIT_COMBAT_TIME;
+                                        return;
+                                    }
+                                    //If the target is it will set the target to the player for every enemy in the group
+									GroupOfEnemies[enemyIndex].SetTarget(m_Players[i].gameObject.transform);
+                                    //This will set the state Chase to each enemy in the group, so if you aggro one
+                                    //You aggro them all
+									GroupOfEnemies[enemyIndex].SetState(State.Chase);
+                                    //Reset our Combat Timer cause we are remaining in combat
+                                    m_CombatTimer = EXIT_COMBAT_TIME;  
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            //Loop through our PlayerObjects
             for (int i = 0; i < m_Players.Length; i++)
             {
+                //Check if the Player isn't our target
                 if (m_Players[i] != m_Target)
                 {
+                    //Check distance between Enemy and Player
                     float distance = Vector3.Distance(gameObject.transform.position, m_Players[i].transform.position);
 
+                    //Check the distance to our target is greater than our distance
                     if (GetDistanceToTarget() >= distance)
                     {
+                        //Set our target to the player
                         m_Target = m_Players[i].gameObject.transform;
 
+                        //Check aggro range of the Target
                         if (GetDistanceToTarget() <= m_AggroRange)
                         {
+                            //Check Combat Range of the target
                             if (GetDistanceToTarget() <= m_CombatRange)
                             {
+                                //Enter fight state
                                 m_State = State.Fight;
 
+                                //Reset combat timer
                                 m_CombatTimer = EXIT_COMBAT_TIME;
                                 return;
                             }
+                            //Enter Chase state
                             m_State = State.Chase;
 
+                            //Reset combat timer
                             m_CombatTimer = EXIT_COMBAT_TIME;
                             return;
                         }
@@ -178,40 +231,54 @@ public abstract class BaseEnemy : MonoBehaviour, Attackable
                 }
             }
 
+            //Check if the Combat Timer has reached 0 or less than 0
             if (m_CombatTimer <= 0)
             {
+                //Reset Timer
                 m_CombatTimer = EXIT_COMBAT_TIME;
+                //Set Enemy out of Combat
                 m_InCombat = false;
+                //Enemy back to patrol
                 m_State = State.Patrol;
             }
             else
             {
+                //Decrement timer by deltaTime
                 m_CombatTimer -= Time.deltaTime;
+                //Set back to default
                 m_State = State.Default;
             }
 
         }
 
+        //Loop through player objects
         for (int i = 0; i < m_Players.Length; i++)
         {
+            //Check distance from the enemy to the player
             float distance = Vector3.Distance(transform.position, m_Players[i].transform.position);
+            //Check if that distance is within aggro range
             if (distance <= m_AggroRange)
             {
+                //Set Combat to true, the target to the player and return to default
                 m_InCombat = true;
                 m_Target = m_Players[i].gameObject.transform;
                 m_State = State.Default;
             }
             else
             {
+                //Get a random number from 1 to 600
                 int idleRandom = Random.Range(1, 600);
+                //Check if it is 1
                 if (idleRandom == 1)
                 {
+                    //Set idling to true, clear target, and set to idle
                     m_Idling = true;
 					m_Target = null;
                     m_State = State.Idle;
                 }
                 else
                 {
+                    //Clear target, and set state to Patrol, if the random number is not 1
 					m_Target = null;
                     m_State = State.Patrol;
                 }
@@ -222,24 +289,31 @@ public abstract class BaseEnemy : MonoBehaviour, Attackable
     //Idle state for when the enemy is standing still
     protected virtual void Idle()
     {
+        //If target is null set it to the Idle Node
 		if(m_Target == null)
 		{
         	m_Target = IdleNode;
 		}
 
+        //Set Nav Mesh destination to the target
         m_Agent.SetDestination(m_Target.position);
 
+        //If we have reached our target enter this statement
         if (GetDistanceToTarget() <= 2.0f)
         {
+            //Check Idle Timer
             if (m_IdlingTime <= 0)
             {
+                //Reset timer, set idling to false and return to default
                 m_IdlingTime = MAX_IDLE_TIME;
                 m_Idling = false;
                 m_State = State.Default;
             }
             else
             {
+                //Decrement timer, so it is still idling
                 m_IdlingTime -= Time.deltaTime;
+                //Check for any Player Aggro when idling
                 for (int i = 0; i < m_Players.Length; i++)
                 {
                     float distance = Vector3.Distance(transform.position, m_Players[i].transform.position);
@@ -258,46 +332,61 @@ public abstract class BaseEnemy : MonoBehaviour, Attackable
     //Enter Chase state for when the player aggros the enemy
     protected virtual void Chase()
     {
+        //Speed the Enemy up when chasing
 		m_Agent.speed = 9.0f;
+        //Set Stopping Distance back to initial so it doesnt stop on top of player
         m_Agent.stoppingDistance = m_InitialStoppingDistance;
+
+        //If target isn't null it will set destination
         if (m_Target != null)
         {
             m_Agent.SetDestination(m_Target.position);
         }
 
+        //Return to default
         m_State = State.Default;
     }
 
     //Patrol state when the enemy needs to patrol between nodes
     protected virtual void Patrol()
     {
+        //Check if the players array isn't null
         if (m_Players != null)
         {
+            //Set stopping Distance to reach node without stopping short
             m_Agent.stoppingDistance = 0;
+
+            //If the target is null it will set the Patrol Path Node
             if (m_Target == null)
             {
                 m_Target = PathNodes[m_NodeIndex].transform;
             }
 
-            if (GetDistanceToTarget() <= 5.0f)
+            //Check if you have reached the node, set boolean if so
+            if (GetDistanceToTarget() <= 2.0f)
             {
                 m_ReachedNode = true;
             }
 
+            //Check if enemy has reached the node
             if (m_ReachedNode == true)
             {
+                //We are no longer mid node, increment our index
                 m_MidNode = false;
                 m_NodeIndex++;
 
+                //Randomize enemy speed between 3 and 7 after reaching each node, organic feel
                 float randomSpeed = Random.Range(3.0f, 7.0f);
                 m_Agent.speed = randomSpeed;
             }
             else
             {
+                //Haven't reached node, still mid node
                 m_ReachedNode = false;
                 m_MidNode = true;
             }
 
+            //Check if we are mid node, if we are and have a target set the destination
             if (m_MidNode == true)
             {
                 if (m_Target != null)
@@ -307,17 +396,20 @@ public abstract class BaseEnemy : MonoBehaviour, Attackable
             }
             else
             {
+                //If we reach the end of our array, the last patrol node, set it back to 0
                 if (m_NodeIndex >= NodeCount)
                 {
                     m_NodeIndex = 0;
                 }
 
+                //Set target to patrol path node, we are now mid node
                 m_Target = PathNodes[m_NodeIndex].transform;
                 m_MidNode = true;
                 m_ReachedNode = false;
             }
         }
 
+        //Return to Default
         m_State = State.Default;
 
     }
@@ -325,30 +417,58 @@ public abstract class BaseEnemy : MonoBehaviour, Attackable
     //Returns the distance between the enemy and the Target
     protected float GetDistanceToTarget()
     {
+        //Returns the distance between the enemy and the target
         return Vector3.Distance(transform.position, m_Target.position);
     }
-
+    
     //Functionality for if the enemy is hit by a player projectile
-    public void onHit(PlayerProjectile proj)
+    public override void onHit(PlayerProjectile proj)
     {
-        m_Health--;   
+		base.onHit (proj); 
     }
 
     //If Enemy gets hit by Enemy Projectile
-    public void onHit(EnemyProjectile proj)
+    public override void onHit(EnemyProjectile proj)
     {
-        return;
+        base.onHit(proj);
     }
 
+    //If our enemy dies, calls abstract die function for inheritance
+    protected override void onDeath()
+    {
+        Die();
+        base.onDeath();
+    }
+
+    //Set whether the enemy is alive or not
     public void SetIsAlive(bool isAlive)
     {
         m_IsAlive = isAlive;
     }
 
+    //Get function to check IsAlive boolean
     public bool GetIsAlive()
     {
         return m_IsAlive;
     }
+
+    //Set aggroRange for custom areas within the game
+    public void SetAggroRange(float range)
+    {
+        m_AggroRange = range;
+    }
+
+    //Ability to set state
+    protected void SetState(State state)
+    {
+        m_State = state;
+    }
+
+    //Set target for the enemy
+	protected void SetTarget(Transform target)
+	{
+		m_Target = target;
+	}
 
     //Attack state when the player is in range to hit
     protected abstract void Fight();
--- Production/Imagination/Assets/Scripts/Attackable/Enemies/Furbull.cs
+++ Production/Imagination/Assets/Scripts/Attackable/Enemies/Furbull.cs
@@ -3,52 +3,64 @@ using System.Collections;
 
 public class Furbull : BaseEnemy 
 {
+    //Enemy Projectile for the Furbull to attack the Player
     public EnemyProjectile EnemyProjectile;
+    //Const for Attack Delay, so enemy doesn't constantly hit player
     private const float FURBULL_ATTACK_DELAY = 1.5f;
     
 	// Use this for initialization
 	void Start () 
     {
+        //Set attack timer from base
         m_AttackTimer = 0.0f;
+        //Set Combat Range from base
 		m_CombatRange = 1.5f;
 
+        //Call base class Start()
 		base.Start ();
 	}
 	
 	// Update is called once per frame
     void Update () 
     {
+        //Call base class Update()
         base.Update();
 	}
 
     protected override void Fight()
     {
+        //Set inital stopping distance
         m_Agent.stoppingDistance = m_InitialStoppingDistance;
 
+        //Check our target
         if (m_Target != null)
         {
+            //If our target isn't null we check our attack timer
             if (m_AttackTimer <= 0.0f)
             {
+                //Attack player then reset our timer
                 ShootProjectile();
                 m_AttackTimer = FURBULL_ATTACK_DELAY;
             }
             else
             {
+                //Decrement timer until delay is over
                 m_AttackTimer -= Time.deltaTime;
             }
+            //Return to default
 			m_State = State.Default;
         }
     }
 
     protected override void Die()
     {
+        //Set base boolean to false
         m_IsAlive = false;
-        //Instantiate Ragdoll
-        Destroy(this.gameObject);
     }
 
     private void ShootProjectile()
     {
+        //Instantiate our projectile from enemy position and rotation
         Instantiate(EnemyProjectile.gameObject, transform.position, transform.rotation);
     }
 }
