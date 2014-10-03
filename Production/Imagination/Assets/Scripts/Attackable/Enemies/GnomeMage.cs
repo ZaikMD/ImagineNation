1c55a1e1ed0401fcfa2ef5b6a87034c376405cd6
elia0034 <elia0034@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-03 18:11:37 +0000

    Commented gnome mage, fixed the clone spawning

--- Production/Imagination/Assets/Scripts/Attackable/Enemies/GnomeMage.cs
+++ Production/Imagination/Assets/Scripts/Attackable/Enemies/GnomeMage.cs
@@ -23,7 +23,7 @@ public class GnomeMage : BaseEnemy
 	//Clones
 	public GameObject m_ClonePrefab;
 	private List<GameObject> m_Clones;
-	private Vector3 m_CloneSpawnDistance;
+	private float m_CloneSpawnDistance;
 	private int m_NumberOfClones;
 	private bool m_ClonesCreated;
 
@@ -48,54 +48,64 @@ public class GnomeMage : BaseEnemy
 
 		m_CurrentFightState = FightStates.Regular;
 
-
+		// Set the health/shield variables
 		m_Health = m_MaxHealth;
 		m_ShieldRechargeTime = 2.0f;
 	    m_ShieldTimer = 0.0f;
 
+		// Set the movement variables
 		m_RotSpeed = 15f;
 		m_BackSpeed = 5f;
 		m_SwitchRotTimer = Random.Range(0.3f, 3);
 		m_MoveDist = 7.0f;
 		m_MoveAngle = 90;
 
+		// Shooting variables
 		m_TimeBetweenShots = 1.5f;
 		m_ShotTimer = 0.0f;
 		m_CanShoot = true;
 
-
+		//Combat variables
 		m_AggroRange = 20.0f;
 		m_CombatRange = 10.0f;
 
+		// Clone variables
 		m_Clones = new List<GameObject>();
 		m_NumberOfClones = 2;
-		m_CloneSpawnDistance = new Vector3 (5, 0, 0);
+		m_CloneSpawnDistance = 5.0f;
 		m_ClonesCreated = false;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		// Random Test Input, 
+		//TODO: Remove
 		if (Input.GetKeyDown (KeyCode.K))
 						m_Health--;
 
+		// Call the Base Enemies Update
 		base.Update ();
 
+		// Do to the way base enemie was built I am just using when his health goes down to one to remove his shield
 		if (m_Health == 1)
 		{
 			if (!m_ClonesCreated)
 			{
+			// When his shield is down create his two clones
 			CreateClones();
 			m_CurrentFightState = FightStates.Cloned;
 			}
 		}
 
+		// If his shot timer is smaller or equal to zero he can shoot
 		m_ShotTimer -= Time.deltaTime;
 		if (m_ShotTimer <= 0)
 		{
 			m_CanShoot = true;
 		}
 
+		// If his clones have been created, start decreasing the timer for when the clones disappear and his health(Shield) comes back
 		if (m_ClonesCreated)
 		{
 			m_ShieldTimer -= Time.deltaTime;
@@ -111,7 +121,9 @@ public class GnomeMage : BaseEnemy
 		}
 	}
 
-
+	/// <summary>
+	/// Override of the base ennemies chase function
+	/// </summary>
 	protected override void Chase ()
 	{
 		//This enemy does not Chase He will just start shooting the player
@@ -120,9 +132,12 @@ public class GnomeMage : BaseEnemy
 
 	}
 
+	/// <summary>
+	/// override of the base enemies fight state
+	/// </summary>
 	protected override void Fight ()
 	{
-
+		// If we are in the regular fight state or the clone one
 		switch (m_CurrentFightState)
 		{
 		case FightStates.Regular:
@@ -139,6 +154,9 @@ public class GnomeMage : BaseEnemy
 		m_State = State.Default;
 	}
 
+	/// <summary>
+	/// Regular Fight State
+	/// </summary>
 	private void Regular()
 	{
 		MoveAway ();
@@ -146,45 +164,75 @@ public class GnomeMage : BaseEnemy
 
 	}
 
+	/// <summary>
+	/// Cloned fight state for when his shields are down and he spawns his clones
+	/// </summary>
 	private void Cloned()
 	{
 		Shoot();
 	}
 
+	/// <summary>
+	/// Shoot your current target. The target is chosen in base enemy
+	/// </summary>
 	private void Shoot()
 	{
 		m_CanShoot = false;
 		if (m_CanShoot)
 		{
+			// Look at the target
 			transform.LookAt(m_Target.position);
-			m_CanShoot = false;
-			m_ShotTimer = m_TimeBetweenShots;
+			// Create the projectile
 			Instantiate (m_ProjectilePrefab, transform.position, transform.rotation);
+			// bring the shot timer back up
+			m_ShotTimer = m_TimeBetweenShots;
+			// He just shot so set his can shoot to false
+			m_CanShoot = false;
 		}
 	}
 
+	/// <summary>
+	/// Move away from the target 
+	/// </summary>
 	private void MoveAway ()
 	{
+		// Update the switch rotation timer
 		m_SwitchRotTimer -= Time.deltaTime;
 
+		// If the timer is smaller or equal to zero  then simply switch the direction of his rotation
 		if (m_SwitchRotTimer <= 0)
 		{
 			m_MoveAngle *= -1;
-			m_SwitchRotTimer = Random.Range(0.3f, 3);
+			// Reset the timer to a random value between .3 and 3 seconds
+			m_SwitchRotTimer = Random.Range(0.3f, 3.0f);
 		}
 
+		// Apply the rotation while trying to stay a certain dist from the player
 		Vector3 point = RotateAboutOrigin (transform.position, m_Target.position, m_MoveAngle);
 		point += (transform.position - m_Target.position).normalized * m_MoveDist;
 		m_Agent.SetDestination(point);
 
 	}
 
+	/// <summary>
+	/// Creates the clones and chooses random positions in which to put the real gnome and his clones
+	/// </summary>
 	private void CreateClones()
 	{
-		Vector3[] positions = new Vector3[3];
-		positions [0] = transform.position;
-		positions [1] = m_Target.position + m_CloneSpawnDistance;
-		positions[2] = m_Target.position - m_CloneSpawnDistance;
+		Vector3[] positions = new Vector3[m_NumberOfClones + 1];
+	
+		for (int i = 0; i < positions.Length; i++)
+		{
+			
+			float angle = UnityEngine.Random.Range(0.0f, 2.0f * Mathf.PI);
+			
+			Vector3 loc = new Vector3( Mathf.Cos(angle),0,Mathf.Sin(angle));			
+			loc = (loc.normalized * m_CloneSpawnDistance) + m_Target.position;
+
+			positions[i] = loc;			
+			
+		}
+
 		Vector3 pos;
 		int range;
 
@@ -220,8 +268,12 @@ public class GnomeMage : BaseEnemy
 		m_ClonesCreated = true;
 	}
 
+	/// <summary>
+	/// Destroys all the created clones
+	/// </summary>
 	private void DestroyClones()
 	{
+		// Loop through and destroy all created clones
 		for (int i = 0; i < m_NumberOfClones; i++)
 		{
 			Destroy(m_Clones[i]);
@@ -229,17 +281,28 @@ public class GnomeMage : BaseEnemy
 		m_Clones.Clear ();
 	}
 
+	/// <summary>
+	/// call this when you die
+	/// </summary>
 	protected override void Die ()
 	{
+		// If there are clones this will destroy them, then destroy the enemy
 		DestroyClones ();
 		Destroy (this.gameObject);
 	}
 
-	
+	/// <summary>
+	/// Grab a point along a rotation around an object
+	/// <returns>The about origin.</returns>
+	/// <param name="point">Point.</param>
+	/// <param name="origin">Origin.</param>
+	/// <param name="angle">Angle.</param>
 	public Vector3 RotateAboutOrigin(Vector3 point, Vector3 origin, float angle)
 	{
+		// Convert the angle to radians
 		angle = angle * (Mathf.PI / 180);
 
+		// Find out the new x and z locations 
 		float rotatedX = Mathf.Cos (angle) * (point.x - origin.x) - Mathf.Sin (angle) * (point.z - origin.z) + origin.x;
 		float rotatedZ = Mathf.Sin (angle) * (point.x - origin.x) - Mathf.Cos (angle) * (point.z - origin.z) + origin.z;
 
