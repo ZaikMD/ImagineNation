87d6d66a34424f0f30c2b42d7d537fd2b8df0484
elia0034 <elia0034@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-06 19:17:19 +0000

    Destructable wall ragdoll prefab created

---
+++ trunk/Production/Imagination/Assets/Scripts/Interactables/DestructableWallRagDoll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DestructableWallRagDoll : MonoBehaviour 
+{
+	public float m_DestroyTime = 3.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+				m_DestroyTime -= Time.deltaTime;
+
+				if (m_DestroyTime <= 0)
+				{
+					Destroy (this.gameObject);
+				}
+
+				
+	}
+}
