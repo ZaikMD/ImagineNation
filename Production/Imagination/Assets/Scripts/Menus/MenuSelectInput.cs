99948817ecbaca365297c2dce21c874bd1cf8519
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-06 15:50:06 +0000

    the menu portion for picking input should be done

--- Production/Imagination/Assets/Scripts/Menus/MenuSelectInput.cs
+++ Production/Imagination/Assets/Scripts/Menus/MenuSelectInput.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class MenuSelectInput : Menu
 {
-    PlayerInputSelection m_PlayerOne = new PlayerInputSelection();
-    PlayerInputSelection m_PlayerTwo = new PlayerInputSelection();
+    PlayerInputSelection m_PlayerOne;
+    PlayerInputSelection m_PlayerTwo;
 
     public GameObject PlayerOneSelection;
     public GameObject PlayerTwoSelection;
@@ -12,12 +12,25 @@ public class MenuSelectInput : Menu
     public GameObject GamePadPrefab;
     public GameObject KeyboardPrefab;
 
+    public GUITexture ReadyTexturePlayerOne;
+    public GUITexture ReadyTexturePlayerTwo;
 
     public Menu NextMenu;
 
+    enum UpdateType
+    {
+        Selection,
+        readyState
+    }
+
     protected override void start()
 	{
-		//used for inheritance
+        ReadyTexturePlayerOne.enabled = false;
+        ReadyTexturePlayerTwo.enabled = false;
+
+
+        m_PlayerOne = new PlayerInputSelection(PlayerOneSelection, ReadyTexturePlayerOne);
+        m_PlayerTwo = new PlayerInputSelection(PlayerTwoSelection, ReadyTexturePlayerTwo);        
 	}
 	
 	// Update is called once per frame
@@ -27,6 +40,12 @@ public class MenuSelectInput : Menu
         {
             if (NextMenu != null)
             {
+                GameData.Instance.m_PlayerOneInput = m_PlayerOne.CurrentSelection;
+                GameData.Instance.m_PlayerTwoInput = m_PlayerTwo.CurrentSelection;
+
+                m_PlayerOne.reset();
+                m_PlayerTwo.reset();
+
                 MenuManager.Instance.changeMenu(NextMenu);
             }
             else
@@ -41,6 +60,65 @@ public class MenuSelectInput : Menu
         }
 	}
 
+    void showPlayerSelection(PlayerInputSelection player, UpdateType updateType)
+    {
+        switch (updateType)
+        {
+            case UpdateType.Selection:
+                {
+                    if (player.m_SelectionMountPoint != null)
+                    {
+                        foreach(Transform child in player.m_SelectionMountPoint.transform)
+                        {
+                            GameObject.Destroy(child.gameObject);
+                        }
+
+                        switch (player.CurrentSelection)
+                        {
+                            case PlayerInput.Keyboard:
+                                {
+                                    GameObject newObj = (GameObject)GameObject.Instantiate(KeyboardPrefab, player.m_SelectionMountPoint.transform.position, player.m_SelectionMountPoint.transform.rotation);
+                                    newObj.transform.parent = player.m_SelectionMountPoint.transform;
+                                    break;
+                                }
+                            case PlayerInput.GamePadOne:
+                                {
+                                    GameObject newObj = (GameObject)GameObject.Instantiate(GamePadPrefab, player.m_SelectionMountPoint.transform.position, player.m_SelectionMountPoint.transform.rotation);
+                                    newObj.transform.parent = player.m_SelectionMountPoint.transform;
+                                    break;
+                                }
+
+                            case PlayerInput.GamePadTwo:
+                                {
+                                    GameObject newObj = (GameObject)GameObject.Instantiate(GamePadPrefab, player.m_SelectionMountPoint.transform.position, player.m_SelectionMountPoint.transform.rotation);
+                                    newObj.transform.parent = player.m_SelectionMountPoint.transform;
+                                    break;
+                                }
+                            default:
+                                break;
+                        }
+                    }
+                }
+                break;
+            case UpdateType.readyState:
+                {
+                    if (player.IsReady)
+                    {
+                        player.m_ReadyTexure.enabled = true;
+
+                        Vector3 screenPoint = Camera.main.WorldToScreenPoint(player.m_SelectionMountPoint.transform.position);
+
+                        player.m_ReadyTexure.transform.position = new Vector3(screenPoint.x / Screen.width, screenPoint.y / Screen.height, 0.0f);
+                    }
+                    else
+                    {
+                        player.m_ReadyTexure.enabled = false;
+                    }
+                }
+                break;
+        }
+    }
+
     PlayerInputSelection setInputSelection(PlayerInputSelection playerToSet, PlayerInputSelection playerToNotSet)
     {
         if (playerToSet.IsReady)
@@ -48,27 +126,34 @@ public class MenuSelectInput : Menu
             if (InputManager.getShowHudDown(playerToSet.CurrentSelection))
             {
                 playerToSet.IsReady = false;
+                showPlayerSelection(playerToSet, UpdateType.readyState);
             }
         }
         else
         {
             if (playerToSet.isSet())
             {
-                if (InputManager.getJumpDown())
+                if (InputManager.getJumpDown(playerToSet.CurrentSelection))
                 {
                     playerToSet.IsReady = true;
+                    showPlayerSelection(playerToSet, UpdateType.readyState);
                 }
                 else
                 {
                     if (InputManager.getShowHudDown(playerToSet.CurrentSelection))
                     {
                         playerToSet.CurrentSelection = PlayerInput.None;
+                        showPlayerSelection(playerToSet, UpdateType.Selection);
                     }
                 }
             }
             else
             {
                 playerToSet = getInput(playerToSet, playerToNotSet);
+                if (playerToSet.CurrentSelection != PlayerInput.None)
+                {
+                    showPlayerSelection(playerToSet, UpdateType.Selection);
+                }
             }
         }
         return playerToSet;
@@ -133,6 +218,27 @@ public class MenuSelectInput : Menu
     protected class PlayerInputSelection
     {
         PlayerInput m_CurrentSelection = PlayerInput.None;
+        public GameObject m_SelectionMountPoint;
+        public GUITexture m_ReadyTexure;
+
+        public PlayerInputSelection(GameObject selectionMountPoint, GUITexture readyTexture)
+        {
+            m_SelectionMountPoint = selectionMountPoint;
+            m_ReadyTexure = readyTexture;
+        }
+
+        public void reset()
+        {
+            m_ReadyTexure.enabled = false;
+            m_CurrentSelection = PlayerInput.None;
+            m_IsReady = false;
+
+            foreach (Transform child in m_SelectionMountPoint.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+
         public PlayerInput CurrentSelection
         {
             get { return m_CurrentSelection; }
@@ -150,6 +256,5 @@ public class MenuSelectInput : Menu
         {
             return m_CurrentSelection != PlayerInput.None;
         }
-
     }
 }
