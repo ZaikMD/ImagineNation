b546acc4a85644aa86b8c140e683e265493ef65b
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-08 17:41:16 +0000

    added more constants

--- Production/Imagination/Assets/Scripts/Misc/Constants.cs
+++ Production/Imagination/Assets/Scripts/Misc/Constants.cs
@@ -7,4 +7,11 @@ public static class Constants
 	public const string ALEX_STRING = "Alex";
 	public const string DEREK_STRING = "Derek";
 	public const string ZOE_STRING = "Zoe";
+
+	public const string PLAYER_PROJECTILE_STRING = "PlayerProjectile";
+	public const string ENEMY_PROJECTILE_STRING = "EnemyProjectile";
+
+	public const string CHECK_POINT_1_STRING = "CheckPoint_1";
+	public const string CHECK_POINT_2_STRING = "CheckPoint_2";
+	public const string CHECK_POINT_3_STRING = "CheckPoint_3";
 }
