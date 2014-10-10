edf69813f86b2e44afe4f769edb49ba815985432
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-10 19:27:01 +0000

    adding new menu files and movine the selection menu and changing it so it doesnt use internal classes

---
+++ Production/Imagination/Assets/Scripts/Menus/NewMenus/MenuV2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuV2 : MonoBehaviour 
+{
+    //camera mount point
+    //look point
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
