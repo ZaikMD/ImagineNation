3d32c886e9d6c35b5471acff346427fc73d23424
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-14 21:05:38 +0000

    updated code got rid of magic numbers and such

--- Production/Imagination/Assets/Scripts/Misc/BaseGoal.cs
+++ Production/Imagination/Assets/Scripts/Misc/BaseGoal.cs
@@ -3,34 +3,22 @@ using System.Collections;
 
 public class BaseGoal : MonoBehaviour {
 
-	//KEEP COMMENTED CODE, WILL BE USED WHEN ANIMATIONS ARE IMPLEMENTED
-
 	public Transform m_LevelEnd;
 	public string m_NextScene;
-	//public GameObject m_TemporaryHidingSpot;
 
-	//public Rigidbody m_AlexPrefab;
-	//public Rigidbody m_ZoePrefab;
-	//public Rigidbody m_DerekPrefab;
-
-
-	//public GameObject m_Zipper;
 	protected bool[] m_AtEnd = new bool[2];
 	protected float m_Speed;
 
 	CharacterController[] m_Players = new CharacterController[2];
-
-	//private bool[] m_PlayerID = new bool[3];
-
 	Vector3[] m_PlayerPositionHolder = new Vector3[2];
 
 	int m_PlayerWaitingToExit = 0;
+	int m_MaxPlayersPossible = 2;
 
-
-
+	float m_DistanceToEnd = 1.0f;
+	
 	void Start()
 	{
-
 		m_PlayerWaitingToExit = 0;
 
 		for(int i = 0; i < m_Players.Length; i++)
@@ -38,30 +26,26 @@ public class BaseGoal : MonoBehaviour {
 			m_AtEnd[i] = false;
 		}
 
-	m_Speed = GameObject.FindGameObjectWithTag ("Player").GetComponent<BaseMovementAbility> ().m_GroundSpeed * Time.deltaTime;
-	
+		m_Speed = GameObject.FindGameObjectWithTag (Constants.PLAYER_STRING).GetComponent<BaseMovementAbility> ().m_GroundSpeed * Time.deltaTime;
 	}
 
 	void Update()
 	{
 	
-	for(int i = 0; i < m_Players.Length; i++)
-	{
-		if(m_PlayerWaitingToExit == 2)
+		for(int i = 0; i < m_Players.Length; i++)
 		{
-			MoveToEnd();
+			if(m_PlayerWaitingToExit == m_MaxPlayersPossible)
+			{
+				MoveToEnd();
+			}
 		}
-	}
 		
-		
-		if (m_AtEnd[1])
-		{
-			LoadNext();
-			return;
+			if (m_AtEnd[1])
+			{
+				LoadNext();
+				return;
+			}
 		}
-
-	}
-
 
 	public void MoveToEnd()
 	{  
@@ -75,45 +59,32 @@ public class BaseGoal : MonoBehaviour {
 				m_Players[i].transform.LookAt(m_LevelEnd);
 				Vector3 vect3 = new Vector3(0, 0, 0);
 
-
-				//BaseMovementAbility tempPlayerMovement = m_Players[i].GetComponent<BaseMovementAbility>();
-
-				//tempPlayerMovement.m_Anim.Play("Run");
-	            
-
 	            m_Players[i].SimpleMove(vect3);
-
-				//m_Players[i].transform.position = m_TemporaryHidingSpot.transform.position;
-
 
 				float distToFin = Vector3.Distance(m_Players[i].transform.position, m_LevelEnd.position);
 
-				if( distToFin > 1.0)
+				if( distToFin > m_DistanceToEnd)
 				{
 					m_Players[i].Move( m_Players[i].transform.forward * m_Speed);
 				}
 
 			
-				if(distToFin < 1.0)
+				if(distToFin < m_DistanceToEnd)
 				{
 					m_AtEnd[i] = true;
 				}
 			}
 		}
-
-		//}
-
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
-		if(other.tag == "Player")
+		if(other.tag == Constants.PLAYER_STRING)
 		{
 			if (m_Players[0] == null)
 			{
 				m_Players[0] = (CharacterController)other.GetComponent(typeof (CharacterController));
 				AddWaitingPlayer();
-				Debug.Log(m_PlayerWaitingToExit);
 				m_PlayerPositionHolder[0] = m_Players[0].transform.position;
 
 			}
@@ -122,35 +93,14 @@ public class BaseGoal : MonoBehaviour {
 			{
 				m_Players[1] = (CharacterController)other.GetComponent(typeof (CharacterController));
 				AddWaitingPlayer();
-				Debug.Log(m_PlayerWaitingToExit);
 				m_PlayerPositionHolder[1] = m_Players[1].transform.position;
 			}
-
-			//Alex is ID number 0
-			//if(other.name == "Alex")
-			//{
-			//	m_PlayerID[0] = true; // is Alex
-			//}
-
-			//Zoe is ID number 1
-			//if (other.name == "Zoe")
-			//{
-			//	m_PlayerID[1] = true; // is Zoe
-			//}
-
-			//Derek is ID number 2
-			//else 
-			//{
-			//	m_PlayerID[2] = true; // is Derek
-			//}
-
 		}
 	}
 
 	void OnTriggerExit(Collider other)
 	{
 		m_PlayerWaitingToExit--;
-		Debug.Log (m_PlayerWaitingToExit);
 	}
 
 	public void LoadNext()
