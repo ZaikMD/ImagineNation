79accfcd275f164d6ad275393afe64e89ef327ee
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-17 05:36:21 +0000

    comments for build info and crawlspace also rotates the player but camera DOES NOT rotate aswell

--- trunk/Production/Imagination/Assets/Scripts/Misc/BuildInfo.cs
+++ trunk/Production/Imagination/Assets/Scripts/Misc/BuildInfo.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿/*
+*Created by Kristopher Matis and Gregory Fortier
+*The script makes a texture that says Alpha Build appear in the bottom left of the screen
+*
+*/
+
+using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(GUITexture))]
@@ -10,14 +16,15 @@ public class BuildInfo : MonoBehaviour
 
 	void Awake()
 	{
+		//makes sure when the next scene loads that the texture doesn't get destroyed
 		GameObject.DontDestroyOnLoad (this.gameObject);
 	}
 
 	// Use this for initialization
 	void Start () 
 	{
+		//Gets the texture and sets its position
 		m_Texture = gameObject.GetComponent<GUITexture> ();
-
 		m_Texture.pixelInset = new Rect (0, 0, m_Texture.texture.width * TextureScale.x, m_Texture.texture.height * TextureScale.y);
 	}
 }
