27c22acaf0e034f756c57e69f6d1b562564613a1
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-17 04:55:33 +0000

    adding the alpha build texture

---
+++ Production/Imagination/Assets/Scripts/Misc/BuildInfo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GUITexture))]
+public class BuildInfo : MonoBehaviour 
+{
+	GUITexture m_Texture;
+
+	public Vector2 TextureScale =  new Vector2(1.0f, 1.0f);
+
+	void Awake()
+	{
+		GameObject.DontDestroyOnLoad (this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_Texture = gameObject.GetComponent<GUITexture> ();
+
+		m_Texture.pixelInset = new Rect (0, 0, m_Texture.texture.width * TextureScale.x, m_Texture.texture.height * TextureScale.y);
+	}
+}
