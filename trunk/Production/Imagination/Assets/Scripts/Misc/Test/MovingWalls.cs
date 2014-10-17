f712721141088de1442d3e8d77451d43e0e5eb46
whit0717 <whit0717@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-17 01:25:17 +0000

    MovingWalls is a new script needed for functionality in the first release.

---
+++ trunk/Production/Imagination/Assets/Scripts/Misc/Test/MovingWalls.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingWalls : MonoBehaviour 
+{
+	public GameObject m_DestinationObject;
+	public float m_Speed;
+
+	private Vector3 m_OriginalPosition;
+	private Vector3 m_DestinationPosition;
+	private Vector3 m_Direction;
+	private bool m_MovingForward;
+	private float m_PushPlayerSpeed;
+	private float m_FinalPush;
+	private Transform m_Other;
+	private bool m_PlayerReachedLimit;
+
+	
+	void Start () 
+	{
+		m_OriginalPosition = transform.position;
+		m_DestinationPosition = m_DestinationObject.transform.position;
+		m_Direction = m_DestinationPosition - m_OriginalPosition;
+		m_MovingForward = true;
+		m_PushPlayerSpeed = 0.55f;
+		m_FinalPush = 2.0f;
+		m_PlayerReachedLimit = false;
+	}
+
+	void Update () 
+	{
+		Move ();
+
+		if(m_PlayerReachedLimit && m_Other != null)
+		{
+			if(m_MovingForward)
+			{
+				m_Other.position -= m_Direction * Time.deltaTime * m_PushPlayerSpeed;
+			}
+			else
+			{
+				m_Other.position += m_Direction * Time.deltaTime * m_PushPlayerSpeed;
+			}
+		}
+
+	}
+
+	void Move ()
+	{
+		if(m_MovingForward)
+		{
+			transform.position += m_Direction * Time.deltaTime * m_Speed;
+			float distance = Vector3.Distance(transform.position, m_DestinationPosition);
+			if(distance < 0.1f)
+			{
+				m_MovingForward = false;
+
+				if(m_Other != null)
+					m_PlayerReachedLimit = true;
+			}
+		}
+		else
+		{
+			transform.position -= m_Direction * Time.deltaTime * m_Speed;
+			float distance = Vector3.Distance(transform.position, m_OriginalPosition);
+			if(distance < 0.1f)
+			{
+				m_MovingForward = true;
+
+				if(m_Other != null)
+					m_PlayerReachedLimit = true;
+			}
+		}
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.tag == Constants.PLAYER_STRING)
+		{
+			m_Other = other.transform;
+
+			if(m_MovingForward)
+			{
+				other.transform.position += m_Direction * Time.deltaTime * m_PushPlayerSpeed;
+			}
+			else
+			{
+				other.transform.position -= m_Direction * Time.deltaTime * m_PushPlayerSpeed;
+			}
+
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == Constants.PLAYER_STRING)
+		{
+			m_Other = null;
+			m_PlayerReachedLimit = false;
+		}
+	}
+}
