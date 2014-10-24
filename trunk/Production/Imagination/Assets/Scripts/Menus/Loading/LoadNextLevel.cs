59408e4bcef6101549f6a64b59c8af123e54e46f
tack0012 <tack0012@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-24 18:48:46 +0000

    added collides to level 1 floor, and loading and SplashScreen

---
+++ trunk/Production/Imagination/Assets/Scripts/Menus/Loading/LoadNextLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNextLevel : MonoBehaviour {
+
+	AsyncOperation m_Async;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_Async = Application.LoadLevelAsync(Application.loadedLevel + 1);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log(m_Async.isDone);
+		if(m_Async.isDone)
+		{
+			Application.LoadLevel(Application.loadedLevel +1);
+		}
+	}
+}
