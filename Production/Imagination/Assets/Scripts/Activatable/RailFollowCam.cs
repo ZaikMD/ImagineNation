29911a51bb3cbc3f706dc06d5969b5a0217bbc68
elia0034 <elia0034@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-27 20:31:32 +0000

    Temp CutScene Camera

---
+++ Production/Imagination/Assets/Scripts/Activatable/RailFollowCam.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RailFollowCam : Activatable 
+{
+	public Camera m_Camera;
+	public BezierSpline m_Rail;
+	public float m_Time;
+	public Transform m_LookTarget;
+
+	GameObject[] m_PLayerCams;
+	bool m_Active = false;
+	bool m_IsDone = false;
+	float m_Loc = 0.0f;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		transform.position = m_Rail.transform.position;
+		transform.LookAt (m_LookTarget);
+
+		m_Camera.enabled = false;
+		m_PLayerCams = GameObject.FindGameObjectsWithTag (Constants.MAIN_CAMERA_STRING);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!m_IsDone)
+		{
+			if (!m_Active)
+			{
+				m_Active = CheckSwitches();
+				if (m_Active)
+				{
+					for (int i = 0; i < m_PLayerCams.Length; i ++)
+					{
+						m_PLayerCams[i].SetActive(false);
+					}
+
+					m_Camera.enabled = true;
+				}
+			}
+			else
+			{
+				m_Loc += Time.deltaTime / m_Time;
+				
+				transform.LookAt (m_LookTarget.position);
+				transform.position = m_Rail.GetPoint(m_Loc);
+
+				if (IsDone())
+				{
+					m_Camera.enabled = false;
+
+					for (int i = 0; i < m_PLayerCams.Length; i ++)
+					{
+						m_PLayerCams[i].SetActive(true);
+					}		
+
+				}
+			}
+		}
+	}
+
+	public bool IsDone()
+	{
+		if (m_Loc >= 1.0f)
+			return true;
+		else
+			return false;
+	}
+}
