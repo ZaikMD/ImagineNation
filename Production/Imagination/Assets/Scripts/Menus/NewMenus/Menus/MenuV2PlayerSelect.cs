aa5a829c8f4247d8a8cd7186ee575f0dec8ec104
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-28 00:03:17 +0000

    more menu stuff

--- Production/Imagination/Assets/Scripts/Menus/NewMenus/Menus/MenuV2PlayerSelect.cs
+++ Production/Imagination/Assets/Scripts/Menus/NewMenus/Menus/MenuV2PlayerSelect.cs
@@ -34,15 +34,72 @@ public class MenuV2PlayerSelect : MenuV2
     public GameObject PedestalKeyboardPrefab;
     public GameObject PedestalGamepadPreafab;
 
+    PlayerSectionV2[] m_PlayerSelections;
+
+    int[] m_PlayerIndexs = { 0, 0 };
+
 	// Use this for initialization
 	void Start () 
     {
-	
+        m_PlayerSelections = new PlayerSectionV2[CharacterPrebafs.Length];
+        for (int i = 0; i < m_PlayerSelections.Length; i++)
+        {
+            m_PlayerSelections[i] = new PlayerSectionV2((GameObject)GameObject.Instantiate(CharacterPrebafs[i], CharacterPrefabMountPoints[i].transform.position, CharacterPrefabMountPoints[i].transform.rotation), 
+                                                        CharacterPrefabMountPoints[i].transform, 
+                                                        Characters[i]);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () 
+
+    // Update is called once per frame
+    protected override void  update()
     {
-	
-	}
+        int total = 0;
+        for(int i = 0; i < m_PlayerSelections.Length; i++)
+        {
+            if(m_PlayerSelections[i].IsReady)
+            {
+                total++;
+            }
+        }
+
+ 	    if(total >= 2)
+        {
+            //TODO: set things
+            //TODO: goto game scene
+            return;
+        }
+
+        Vector2 playerOneMoveInput = InputManager.getMenuChangeSelection(GameData.Instance.m_PlayerOneInput);
+        Vector2 playerTwoMoveInput = InputManager.getMenuChangeSelection(GameData.Instance.m_PlayerTwoInput);
+
+        #region change selection
+        if (playerOneMoveInput.y > 0.0f)
+        {
+            m_PlayerIndexs[PLAYER_ONE]++;
+        }
+        else if (playerOneMoveInput.y < 0.0f)
+        {
+            m_PlayerIndexs[PLAYER_ONE]--;
+        }
+
+        m_PlayerIndexs[PLAYER_ONE] = Mathf.Clamp(m_PlayerIndexs[PLAYER_ONE], 0, CharacterPrefabMountPoints.Length - 1);
+        PlayerArrows[PLAYER_ONE].transform.position = PlayerOneArrowMountPoints[m_PlayerIndexs[PLAYER_ONE]].transform.position;
+
+
+        if (playerTwoMoveInput.y > 0.0f)
+        {
+            m_PlayerIndexs[PLAYER_TWO]++;
+        }
+        else if (playerTwoMoveInput.y < 0.0f)
+        {
+            m_PlayerIndexs[PLAYER_TWO]--;
+        }
+
+        m_PlayerIndexs[PLAYER_TWO] = Mathf.Clamp(m_PlayerIndexs[PLAYER_TWO], 0, CharacterPrefabMountPoints.Length - 1);
+        PlayerArrows[PLAYER_TWO].transform.position = PlayerTwoArrowMountPoints[m_PlayerIndexs[PLAYER_TWO]].transform.position;
+
+        #endregion
+
+
+    }
 }
