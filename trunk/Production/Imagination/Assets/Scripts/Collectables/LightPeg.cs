180cfd50deb320b9e19327d9957eb6546bb18ca1
tack0012 <tack0012@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-29 20:52:30 +0000

    did work on collectibles, added a light peg manager

--- trunk/Production/Imagination/Assets/Scripts/Collectables/LightPeg.cs
+++ trunk/Production/Imagination/Assets/Scripts/Collectables/LightPeg.cs
@@ -1,15 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LightPeg : MonoBehaviour {
+/*Created by Kole
+ * 
+ * Light peg will act simularly to coins in mario
+ * When a peg is Collected, it will increment the total  
+ *  
+ */
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+[RequireComponent(typeof(CharacterController))]
+
+public class LightPeg : BaseCollectable
+{
+  //  public  int m_ID;
+
+    CharacterController m_Controller;
+
+    LightPegManager m_LightPegManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Controller = gameObject.GetComponent<CharacterController>();
+        m_LightPegManager = GameObject.FindGameObjectWithTag(Constants.COLLECTABLE_MANAGER).GetComponent<LightPegManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This will apply gravity for us
+        Vector3 speed = Vector3.zero;
+        m_Controller.SimpleMove(speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == Constants.PLAYER_STRING)
+        {
+            //Tell GameData this peg was collected
+            GameData.Instance.LightPegCollected(m_ID);
+
+            //increment collectable counter
+            m_LightPegManager.IncrementCounter();
+
+            //destroy this gameobject
+            Destroy(this.gameObject);            
+        }
+    }
+ 
+
 }
