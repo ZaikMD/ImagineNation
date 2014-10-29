35516b53ce95e2fa237cc0b74f2a5f22a4054c03
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-29 20:19:17 +0000

    Added a Activatable/ActivatableColliders.cs

---
+++ Production/Imagination/Assets/Scripts/Activatable/ActivatableColliders.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Activatable colliders. Script will destroy colliders
+/// Created by Zach Dubuc  10/29/14
+/// </summary>
+public class ActivatableColliders : Activatable 
+{
+	//The array of colliders so there can be more than one
+	public GameObject[] m_Colliders;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(CheckSwitches()) //If checkSwitches() returns true, destroy all colliders
+		{
+			for(int i = 0; i < m_Colliders.Length; i++)
+			{
+				Destroy(m_Colliders[i]);
+			}
+		}
+	}
+}
