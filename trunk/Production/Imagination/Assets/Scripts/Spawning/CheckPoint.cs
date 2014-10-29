bba2be14e8749c5976d644f8c0a4749899faee1a
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-29 20:05:00 +0000

    worked on checkpoints making the texture appear, doesn't work yet

--- trunk/Production/Imagination/Assets/Scripts/Spawning/CheckPoint.cs
+++ trunk/Production/Imagination/Assets/Scripts/Spawning/CheckPoint.cs
@@ -4,6 +4,8 @@
 *resposible for informing GameData that a checkpoint might need changeing
 *
 *Created by: Kris Matis
+*
+*Edit: Oct 29th 2014- Adding a text message that pops up when the player activates the checkpoint -Greg Fortier
 */
 
 #region ChangeLog
@@ -17,8 +19,16 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(GUITexture))]
 public class CheckPoint : MonoBehaviour 
 {
+	//Texture is for the player to acknowledge that they reached a checkpoint
+	GUITexture m_Texture;
+	public Vector2 TextureScale =  new Vector2(1.0f, 1.0f);
+	bool m_DrawGUI;
+	float m_TimerGUI = 3.0f;
+	float m_baseTimeValue;
+
 
 	public Material m_OffMaterial;
 	public Material m_OnMaterial;
@@ -31,9 +41,12 @@ public class CheckPoint : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+		m_baseTimeValue = m_TimerGUI;
 		m_ColorSection.renderer.material = m_OffMaterial;
 		//gets sound manager
 		m_SFX = GameObject.FindGameObjectWithTag(Constants.SOUND_MANAGER).GetComponent<SFXManager>();
+
+		m_DrawGUI = false;
 	}
 	
 	// Update is called once per frame
@@ -51,6 +64,28 @@ public class CheckPoint : MonoBehaviour
 			m_SFX.playSound(this.gameObject, Sounds.Collectable);
 			GameData.Instance.CurrentCheckPoint = m_Value;
 			m_ColorSection.renderer.material = m_OnMaterial;
+			m_DrawGUI = true;
+
 		}
+	}
+
+	void OnGui()
+	{
+		if(m_DrawGUI)
+		{
+			if(m_TimerGUI >=0)
+			{
+				m_Texture = gameObject.GetComponent<GUITexture> ();
+				m_Texture.pixelInset = new Rect (0, 0, m_Texture.texture.width * TextureScale.x, m_Texture.texture.height * TextureScale.y);
+				m_TimerGUI -= Time.deltaTime;
+			}
+
+			else 
+			{
+				m_DrawGUI = false;
+				m_TimerGUI = m_baseTimeValue;
+			}
+		}
+
 	}
 }
