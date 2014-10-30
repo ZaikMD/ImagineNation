3e32d2b45d9092dd918167d8a88d778780cb5ce0
fort0222 <fort0222@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-30 20:57:21 +0000

    most of checkpoint done

--- Production/Imagination/Assets/Scripts/Spawning/CheckPoint.cs
+++ Production/Imagination/Assets/Scripts/Spawning/CheckPoint.cs
@@ -19,15 +19,15 @@
 using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(GUITexture))]
 public class CheckPoint : MonoBehaviour 
 {
 	//Texture is for the player to acknowledge that they reached a checkpoint
-	GUITexture m_Texture;
+	public GUITexture m_Texture;
 	public Vector2 TextureScale =  new Vector2(1.0f, 1.0f);
 	bool m_DrawGUI;
-	float m_TimerGUI = 3.0f;
-	float m_baseTimeValue;
+	float m_TimerGUI =0;
+	float m_baseTimeValue = 5.0f;
+	int m_DivideByTwo = 2;
 
 
 	public Material m_OffMaterial;
@@ -41,17 +41,18 @@ public class CheckPoint : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		m_baseTimeValue = m_TimerGUI;
 		m_ColorSection.renderer.material = m_OffMaterial;
 		//gets sound manager
 		m_SFX = GameObject.FindGameObjectWithTag(Constants.SOUND_MANAGER).GetComponent<SFXManager>();
 
-		m_DrawGUI = false;
+		m_DrawGUI = true;
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		OnGUI ();
 
 	
 	}
@@ -60,32 +61,41 @@ public class CheckPoint : MonoBehaviour
 	{
 		if(obj.tag == Constants.PLAYER_STRING)
 		{
+
+			if(m_DrawGUI)
+			{
+				m_TimerGUI = m_baseTimeValue;
+
+			}
+
 			//Plays the collectable sound
 			m_SFX.playSound(this.gameObject, Sounds.Collectable);
 			GameData.Instance.CurrentCheckPoint = m_Value;
 			m_ColorSection.renderer.material = m_OnMaterial;
-			m_DrawGUI = true;
 
 		}
 	}
 
-	void OnGui()
+	void OnGUI()
 	{
-		if(m_DrawGUI)
-		{
-			if(m_TimerGUI >=0)
+		//if(m_DrawGUI)
+		//{
+			if(m_TimerGUI >0)
 			{
-				m_Texture = gameObject.GetComponent<GUITexture> ();
-				m_Texture.pixelInset = new Rect (0, 0, m_Texture.texture.width * TextureScale.x, m_Texture.texture.height * TextureScale.y);
+				m_Texture.transform.parent.gameObject.GetComponentInChildren<GUITexture>();
+				m_Texture.pixelInset = new Rect (0, 0, (m_Texture.texture.width * TextureScale.x)/m_DivideByTwo, (m_Texture.texture.height * TextureScale.y)/m_DivideByTwo);
 				m_TimerGUI -= Time.deltaTime;
+				m_Texture.enabled = true;
+				Debug.Log(m_TimerGUI);
 			}
 
 			else 
 			{
 				m_DrawGUI = false;
-				m_TimerGUI = m_baseTimeValue;
+				//m_TimerGUI = m_baseTimeValue;
+				m_Texture.enabled = false;
 			}
-		}
+		//}
 
 	}
 }
