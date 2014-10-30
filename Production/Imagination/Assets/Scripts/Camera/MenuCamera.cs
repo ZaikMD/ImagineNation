d50704d6152097a8cc38d4f71d950c46acf205bf
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-30 20:47:45 +0000

    ShutterCamera.cs is now a base class

--- Production/Imagination/Assets/Scripts/Camera/MenuCamera.cs
+++ Production/Imagination/Assets/Scripts/Camera/MenuCamera.cs
@@ -9,6 +9,8 @@
 * 23/10/2014 edit: kris matis  created the script and commented it
  * 
  * 27/10/2014 edit: kris matis  set isDoneMoving to false as soon as a new menu is set
+ * 
+ * 30/10/2014 edit: kris matis  shutter stuff is now in a base class
  */
 #endregion
 
@@ -16,37 +18,8 @@ using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(Camera))]
-public class MenuCamera : MonoBehaviour
+public class MenuCamera : ShutterCamera
 {
-    #region Shutter Variables
-    //used by other classes to check if the shutter is done moving
-    bool m_IsDoneShutterMove = true;
-    public bool IsDoneShutterMove
-    {
-        get { return m_IsDoneShutterMove; }
-    }
-
-    //whether or not we should be showing the shutter
-    public bool m_ShowShutter = false;
-    public bool ShowShutter
-    {
-        get { return m_IsDoneShutterMove; }
-        protected set { m_ShowShutter = value; }
-    }
-
-    //the difference in euler angles for the shutter when its shown
-    Vector3 m_ShowShutterEulers = new Vector3(0.0f, 0.0f, -90.0f);
-    //the shutters initail euler angles (hidden)
-    Vector3 m_ShutterInitialEuler;
-
-    //object used to rotate the shutter
-    public GameObject ShutterRotationPoint;
-
-    //timer for the shutter and constant for the shutter speed
-    const float SHUTTER_SPEED = 0.15f;
-    float m_ShutterTimer = SHUTTER_SPEED;
-    #endregion
-
     protected MenuV2 m_NewMenu = null;
 
     #region Random Move
@@ -68,10 +41,9 @@ public class MenuCamera : MonoBehaviour
 
     public Transform LookAt;
 
-    void Start()
+    protected override void Start()
     {
-        //set the inital euler angles for the shutter
-        m_ShutterInitialEuler = ShutterRotationPoint.transform.eulerAngles;
+        base.Start();
 
         //set the initial position
         m_InitialPosition = transform.position;
@@ -80,10 +52,14 @@ public class MenuCamera : MonoBehaviour
         pickInitialTargetPosition();
     }
 
-    void Update()
+    protected override void Update()
     {
-        //update the shutter
-        updateShutter();
+        base.Update();
+
+		if(IsDoneShutterMove && ShowShutter)
+		{
+			changePosition(m_NewMenu.CameraMountPoint.transform.position, m_NewMenu.transform);
+		}
 
         //update the position
         updatePosition();
@@ -149,43 +125,5 @@ public class MenuCamera : MonoBehaviour
         m_LastTargetPosition = m_TargetPosition;
         //pick the new target
         m_TargetPosition = m_InitialPosition + new Vector3(Random.value % MAX_TARGET_DISTANCE, Random.value % MAX_TARGET_DISTANCE, 0.0f);
-    }
-
-    void updateShutter()
-    { 
-        //set that the shutter is not done
-        m_IsDoneShutterMove = false;
-        if (m_ShowShutter && m_ShutterTimer < SHUTTER_SPEED)
-        {
-            //need to show the shutter so increment the shutter timer up
-            m_ShutterTimer += Time.deltaTime;
-            if (m_ShutterTimer > SHUTTER_SPEED)
-            {
-                m_ShutterTimer = SHUTTER_SPEED;
-            }
-        }
-        else if(!m_ShowShutter && m_ShutterTimer > 0.0f)
-        {
-            //need to hide the shutter so increment the shutter timer down
-            m_ShutterTimer -= Time.deltaTime;
-            if (m_ShutterTimer < 0.0f)
-            {
-                m_ShutterTimer = 0.0f;
-            }
-        }
-        else
-        {
-            //dont need to move the shutter so set the bool
-            m_IsDoneShutterMove = true;
-
-            if (m_NewMenu != null)
-            {
-                changePosition(m_NewMenu.CameraMountPoint.transform.position, m_NewMenu.transform);
-                m_NewMenu = null;
-            }
-        }
-        
-        //move the shutter
-        ShutterRotationPoint.transform.eulerAngles = m_ShutterInitialEuler + (m_ShowShutterEulers * (m_ShutterTimer/SHUTTER_SPEED));
     }
 }
