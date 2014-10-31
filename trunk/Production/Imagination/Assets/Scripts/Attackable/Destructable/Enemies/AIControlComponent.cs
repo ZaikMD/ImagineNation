4d696b06e05989c16e6f4437047ccf847e887740
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-10-31 20:46:58 +0000

    Started working on the new enemy ai base classes.

---
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/AIControlComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIControlComponent : Object
+{
+
+	public void Load ()
+	{
+
+	}
+
+	//Update this components logic
+	public void Update ()
+	{
+
+	}
+}
---
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/BaseEnemyWithMovement.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by Jason Hein October 31/2014
+ * Designed to be an extension of the BaseEnemyAI script with a NavMash (for movement)
+ */ 
+#region ChangeLog
+/* 
+ * 
+ */
+#endregion
+
+using UnityEngine;
+using System.Collections;
+
+//Require our NavMeshAgent for EnemyAI
+[RequireComponent(typeof(NavMeshAgent))]
+public class BaseEnemyWithMovement : BaseEnemyAI
+{
+	//Load a NavMeshAgent for the AI
+	protected NavMeshAgent m_Agent;
+	protected void Start ()
+	{
+		m_Agent = gameObject.GetComponent<NavMeshAgent>();
+	}
+}
