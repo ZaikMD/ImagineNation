be3cc849cf7a681bea9c9a7c9802be096b417e15
elia0034 <elia0034@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-11-19 21:47:31 +0000

    GnomeIdle

--- trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/GnomeMage/GnomeIdle.cs
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/GnomeMage/GnomeIdle.cs
@@ -25,7 +25,7 @@ public class GnomeIdle : BaseIdleBehaviour
 
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame 
 	public override void update()
 	{
 		m_Movement.Movement ();
