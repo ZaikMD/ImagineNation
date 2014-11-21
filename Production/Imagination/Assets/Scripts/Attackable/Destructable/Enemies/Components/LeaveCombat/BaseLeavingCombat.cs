76649e703cc63475ac2da147210cd349cf21467e
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-11-21 19:45:09 +0000

    Added Component Folders

---
+++ Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/Components/LeaveCombat/BaseLeavingCombat.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by Joe Burchill November 14/2014
+ * Base Component to determine when the enemy
+ * should be leaving combat, used within the
+ * Chase Behaviour(s)
+ */
+
+#region ChangeLog
+/*
+ * 
+ */
+#endregion
+
+using UnityEngine;
+using System.Collections;
+
+public abstract class BaseLeavingCombat : BaseComponent
+{
+    public override void start(BaseBehaviour baseBehaviour)
+    {
+
+    }
+
+    public abstract bool LeaveCombat();
+}
