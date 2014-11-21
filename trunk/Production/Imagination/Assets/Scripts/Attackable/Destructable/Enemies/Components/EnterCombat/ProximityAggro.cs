0ca0653fa1c1a71525756dbdf7c15df979751b5f
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-11-21 20:43:33 +0000

    Created Proximity Aggro component and updated Enemy component folder structure.

---
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/Components/EnterCombat/ProximityAggro.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProximityAggro : BaseEnterCombat 
+{
+    public float AggroRange = 20.0f;
+    BaseTargeting m_Targeting;
+
+    public override bool EnterCombat()
+    {
+        if (Vector3.Distance(transform.position, m_Targeting.CurrentTarget().transform.position) < AggroRange)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
