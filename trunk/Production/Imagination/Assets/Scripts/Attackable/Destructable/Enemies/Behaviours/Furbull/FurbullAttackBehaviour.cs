d5d99b22ca8728db12d0cfc0b2823fbbdc4a4d1b
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-11-24 22:12:58 +0000

    Updated Furbull Behaviours and Added 2 Constants - Furbull and Spin Top Attack

--- trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/Behaviours/Furbull/FurbullAttackBehaviour.cs
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/Behaviours/Furbull/FurbullAttackBehaviour.cs
@@ -15,8 +15,37 @@ using System.Collections;
 
 public class FurbullAttackBehaviour : BaseAttackBehaviour 
 {
+    GameObject m_Target;
+
+    void Start()
+    {
+        m_CombatComponent.start(this);
+        m_TargetingComponent.start(this);
+        m_MovementComponent.start(this);
+    }
+
 	public override void update()
 	{
+        m_Target = Target();
 
+        if (m_Target == null)
+        {
+            m_EnemyAI.SetState(EnemyAI.EnemyState.Idle);
+            return;
+        }
+
+        if (GetDistanceToTarget() >= Constants.FURBULL_ATTACK_RANGE)
+        {
+            m_EnemyAI.SetState(EnemyAI.EnemyState.Chase);
+        }
+
+        Movement();
+
+        Combat();
 	}
+
+    private float GetDistanceToTarget()
+    {
+        return Vector3.Distance(transform.position, m_Target.transform.position);
+    }
 }
--- trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/Behaviours/Furbull/FurbullIdleBehaviour.cs
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/Behaviours/Furbull/FurbullIdleBehaviour.cs
@@ -15,15 +15,23 @@ using System.Collections;
 
 public class FurbullIdleBehaviour : BaseIdleBehaviour 
 {
+
+    void Start()
+    {
+        m_MovementComponent.start(this);
+        m_EnterCombatComponent.start(this);
+    }
+
     public override void update()
 	{
+        if (m_MovementComponent != null)
+        {
+            m_MovementComponent.Movement();
+        }
+
         if (m_EnterCombatComponent.EnterCombat() == true)
         {
             m_EnemyAI.SetState(EnemyAI.EnemyState.Chase);
-        }
-        else
-        {
-            m_MovementComponent.Movement();
         }
 	}
 }
