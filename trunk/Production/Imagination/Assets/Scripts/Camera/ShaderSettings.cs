470b875502b0738dfa49368bc8ece29b86ac800d
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-11-24 20:23:57 +0000

    Added the ability to set brightness from a material at Assets/Materials/Camera/ShaderSettingsRenderMaterial.
    If a camera renders pink, it means that it has the settings script attached but does not have the required material set through the unity editor.

---
+++ trunk/Production/Imagination/Assets/Scripts/Camera/ShaderSettings.cs
@@ -0,0 +1,24 @@
+﻿//Created by Jason Hein Nov 24, 2014
+#region Changelog
+/*
+ * 
+ */ 
+#endregion
+
+
+using UnityEngine;
+using System.Collections;
+
+//This script calls OnRenderImage to set a custom shader to render the final image
+public class ShaderSettings : MonoBehaviour
+{
+	//The material with our shader settings
+	public Material m_SettingsRenderMaterial;
+
+	//When this camera is about to render
+	void OnRenderImage (RenderTexture src, RenderTexture dst)
+	{
+		//Render the new screen with our custom shader settings
+		Graphics.Blit (src, dst, m_SettingsRenderMaterial);
+	}
+}
