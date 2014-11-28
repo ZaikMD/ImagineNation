54f0b9fc165a3701e0089ac0f246fc431d51950b
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-11-28 21:14:11 +0000

    made buttons that raise and lower the Resolution

---
+++ trunk/Production/Imagination/Assets/Scripts/Menus/NewMenus/Buttons/Settings/Resolution/ButtonV2ResolutionDown.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonV2ResolutionDown : ButtonV2Resolution 
+{
+    public override void use()
+    {
+        m_CurrentResolution = Mathf.Clamp(m_CurrentResolution - 1, 0, Screen.resolutions.Length - 1);
+        setResolution();
+    }
+}
---
+++ trunk/Production/Imagination/Assets/Scripts/Menus/NewMenus/Buttons/Settings/Resolution/ButtonV2ResolutionUp.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonV2ResolutionUp : ButtonV2Resolution
+{
+    public override void use()
+    {
+        m_CurrentResolution = Mathf.Clamp(m_CurrentResolution + 1, 0, Screen.resolutions.Length - 1);
+        setResolution();
+    }
+}
