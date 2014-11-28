d20754d1974dd0e64a12c5148752a788f55276fd
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-11-28 21:14:11 +0000

    made buttons that raise and lower the Resolution

---
+++ Production/Imagination/Assets/Scripts/Menus/NewMenus/Buttons/Settings/Resolution/ButtonV2Resolution.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ButtonV2Resolution : ButtonV2 
+{
+    protected int m_CurrentResolution = -1;
+
+    protected override void start()
+    {
+        for (int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            if(Screen.currentResolution.width == Screen.resolutions[i].width && Screen.currentResolution.height == Screen.resolutions[i].height)
+            {
+                m_CurrentResolution = i;
+                break;
+            }
+        }
+    }
+
+    protected void setResolution()
+    {
+        Screen.SetResolution(Screen.resolutions[m_CurrentResolution].width, Screen.resolutions[m_CurrentResolution].height, true);
+    }
+}
