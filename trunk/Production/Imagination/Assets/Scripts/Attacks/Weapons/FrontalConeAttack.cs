30ae672f408a9c6ef8f7d01075a4d35d841a31d0
dubu0017 <dubu0017@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-12-01 22:01:10 +0000

    Fixed attacks again. They now spawn multiple projectiles if the combo requires it.

--- trunk/Production/Imagination/Assets/Scripts/Attacks/Weapons/FrontalConeAttack.cs
+++ trunk/Production/Imagination/Assets/Scripts/Attacks/Weapons/FrontalConeAttack.cs
@@ -11,7 +11,7 @@ public class FrontalConeAttack : BaseAttack
 	Vector3 m_Rotation;
 	void Start()
 	{
-		m_Damage = 0.5f;
+		m_Damage = 1.0f;
 		m_AttackTimer = 0.4f;
 		m_SaveAttackTimer = m_AttackTimer;
 		m_GraceTimer = 0.5f;
@@ -26,12 +26,14 @@ public class FrontalConeAttack : BaseAttack
 
 		for(int i = 0; i < m_NumberOfProjectiles; i++)
 		{
-			LightProjectile proj = (LightProjectile)GameObject.Instantiate (m_Projectile,
+			GameObject proj =  (GameObject)GameObject.Instantiate (m_Projectile,
 			                        new Vector3(m_InitialPosition.x, m_InitialPosition.y + m_FirePointYOffSet, m_InitialPosition.z),
 			                        Quaternion.Euler(m_Rotation));
+
+			HeavyProjectile projS = proj.GetComponent (typeof(HeavyProjectile)) as HeavyProjectile;
 			m_Rotation.y -= (m_Angle);
-			proj.setDamage (m_Damage);
-			proj.setCharacter (m_Character);
+			projS.setDamage (m_Damage);
+			projS.setCharacter (m_Character);
 		}		
 	}
 }
