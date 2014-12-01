c127ba072854a9b1b3c0ed7967dacfa6805908a1
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-12-01 21:34:13 +0000

    added a test script so duschand can get in the game without a gamepad

---
+++ Production/Imagination/Assets/Scripts/Menus/NewMenus/Buttons/TEST/TEST_LoadTwoCharactersWithKeyboardInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TEST_LoadTwoCharactersWithKeyboardInput : ButtonV2 
+{
+	public Characters i_PlayerOne;
+	public Characters i_PlayerTwo;
+
+	public string i_SceneToLoad = "LoadingScreen";
+
+	public override void use (PlayerInput usedBy)
+	{
+		GameData.Instance.PlayerOneCharacter = i_PlayerOne;
+		GameData.Instance.PlayerTwoCharacter = i_PlayerTwo;
+
+		GameData.Instance.m_PlayerOneInput = PlayerInput.Keyboard;
+		GameData.Instance.m_PlayerTwoInput = PlayerInput.Keyboard;
+
+		Application.LoadLevel (i_SceneToLoad);
+	}
+}
