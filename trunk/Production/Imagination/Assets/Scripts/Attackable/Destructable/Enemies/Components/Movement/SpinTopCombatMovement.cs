476111ff60174c2792518022ec23fa0c1992a2bc
burc0058 <burc0058@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-12-01 21:55:13 +0000

    Continue working on Spin Top Scripts

---
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/Components/Movement/SpinTopCombatMovement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinTopCombatMovement : BaseMovement 
+{
+    public override Vector3 Movement(GameObject target)
+    {
+        return target.transform.position;
+    }
+}
