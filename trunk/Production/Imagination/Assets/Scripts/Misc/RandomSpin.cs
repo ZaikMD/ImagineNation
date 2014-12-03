b674ebc2c5d28d6325dd286fa61235e9825de4d1
tack0012 <tack0012@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-12-03 22:12:17 +0000

    fixed error in surround control, fixed the light pegs, added color changing for light pegs, added functionality to random spin

--- trunk/Production/Imagination/Assets/Scripts/Misc/RandomSpin.cs
+++ trunk/Production/Imagination/Assets/Scripts/Misc/RandomSpin.cs
@@ -8,6 +8,7 @@
 
 #region Changelog
 /*
+ * edit: Added options such as which axis to rotate on, and added random rotation on the y axis, and random Constant - Kole 
  * 
  */ 
 #endregion
@@ -18,28 +19,87 @@ using System.Collections;
 
 public class RandomSpin : MonoBehaviour
 {
+	//Options
+	public bool RandomizeXAxis;
+	public bool RandomizeYAxis;
+	public bool RandomizeZAxis;
+	public bool RandomizeOnlyAtStart;
+
+
 	//Magnitude of rotation
-	public float m_MaxRandomizedSpeed = 50.0f;
+	public Vector3 m_MaxRandomizedSpeed;
 	Vector3 m_RandomizedSpeedMagnitude = Vector3.one;
-	public Vector3 m_ConstantSpeed = new Vector3 (50.0f, 0.0f, 50.0f);
+	public Vector3 m_ConstantSpeed = new Vector3 (0.0f, 0.0f, 50.0f);
+
+	public bool UseRandomConstantSpeed;
+	public Vector3 m_MaxConstantSpeed;
+	public Vector3 m_MinConstantSpeed;
 
 	//Timer
 	const float MAX_DELAY = 1.0f;
-	Vector2 i_Delay = Vector3.one;
-	Vector2 m_Timer;
+	Vector3 i_Delay = Vector3.one;
+	Vector3 m_Timer;
 
 	//Set randomized rotation values
 	protected void Start()
-	{
+	{	
+		float X;
+		float MagX;
+		float Y;
+		float MagY;
+		float Z;
+		float MagZ;
+
+		if(RandomizeXAxis)
+		{
+			X = Random.Range (0.0f, 360.0f);
+		}
+		else
+		{
+			X = transform.rotation.x;
+		}
+
+
+		if(RandomizeYAxis)
+		{
+			Y = Random.Range (0.0f, 360.0f);
+			Debug.Log(Y);
+		}
+		else
+		{
+			Y = transform.rotation.y;
+		}
+
+		if(RandomizeZAxis)
+		{
+			Z = Random.Range (0.0f, 360.0f);
+		}
+		else
+		{
+			Z = transform.rotation.z;
+		}
+
+		if(UseRandomConstantSpeed)
+		{
+			float x = Random.Range(m_MinConstantSpeed.x, m_MaxConstantSpeed.x);
+			float y = Random.Range(m_MinConstantSpeed.y, m_MaxConstantSpeed.y);
+			float z = Random.Range(m_MinConstantSpeed.z, m_MaxConstantSpeed.z);
+
+			m_ConstantSpeed.Set(x, y, z);
+		}
+
+
 		//Set the initial rotation to something random
-		transform.rotation = new Quaternion(Random.Range (0.0f, 360.0f), Random.Range (0.0f, 360.0f), Random.Range (0.0f, 360.0f), 1.0f);
+		transform.rotation = new Quaternion(X, Y, Z, 1.0f);
+
+		transform.Rotate(transform.rotation.x, Y, transform.rotation.z);
 
 		//Set the new speed of the object
 		newSpeed ();
 
 		//Set timers and magnitude to be random for each peg
-		i_Delay = new Vector2 (Random.Range (0.0f, MAX_DELAY), Random.Range (0.0f, MAX_DELAY));
-		m_RandomizedSpeedMagnitude = new Vector3 (Random.Range (0.0f, m_MaxRandomizedSpeed), 0.0f, Random.Range (0.0f, m_MaxRandomizedSpeed));
+		i_Delay = new Vector3 (Random.Range (0.0f, MAX_DELAY), Random.Range (0.0f, MAX_DELAY), Random.Range (0.0f, MAX_DELAY));
+		m_RandomizedSpeedMagnitude = new Vector3 (Random.Range (0.0f, m_MaxRandomizedSpeed.x), Random.Range (0.0f, m_MaxRandomizedSpeed.y), Random.Range (0.0f, m_MaxRandomizedSpeed.z));
 
 		//Set the timer
 		m_Timer = i_Delay;
@@ -49,7 +109,9 @@ public class RandomSpin : MonoBehaviour
 	protected void Update () 
 	{
 		//Rotate the object
+		if(!RandomizeOnlyAtStart)
 		transform.Rotate (m_RandomizedSpeedMagnitude * Time.deltaTime);
+
 		transform.Rotate (m_ConstantSpeed * Time.deltaTime);
 
 		//Update timers
@@ -64,9 +126,16 @@ public class RandomSpin : MonoBehaviour
 
 		if(m_Timer.y < 0.0f)
 		{
-			newSpeedZ();
+			newSpeedY();
 			m_Timer.y = i_Delay.y;
 		}
+
+		if(m_Timer.z < 0.0f)
+		{
+			newSpeedZ();
+			m_Timer.z = i_Delay.z;
+		}
+
 	}
 
 	//Set a new speed
@@ -74,17 +143,45 @@ public class RandomSpin : MonoBehaviour
 	{
 		newSpeedX ();
 		newSpeedZ ();
+		newSpeedY ();
 	}
 
 	//Set a new speed on the x axis
 	void newSpeedX()
 	{
-		m_RandomizedSpeedMagnitude.x = Random.Range (0.0f, m_MaxRandomizedSpeed);
+		if(RandomizeXAxis)
+		{
+			m_RandomizedSpeedMagnitude.x = Random.Range (0.0f, m_MaxRandomizedSpeed.x);
+		}
+		else
+		{
+			m_RandomizedSpeedMagnitude.x = 0;
+		}		
 	}
 
 	//Set a new speed on the z axis
 	void newSpeedZ()
 	{
-		m_RandomizedSpeedMagnitude.z = Random.Range (0.0f, m_MaxRandomizedSpeed);
+		if(RandomizeZAxis)
+		{
+			m_RandomizedSpeedMagnitude.z = Random.Range (0.0f, m_MaxRandomizedSpeed.z);
+		}
+		else
+		{
+			m_RandomizedSpeedMagnitude.z = 0;
+		}
+	}
+
+	void newSpeedY()
+	{
+		if(RandomizeYAxis)
+		{
+			m_RandomizedSpeedMagnitude.y = Random.Range (0.0f, m_MaxRandomizedSpeed.y);
+		}
+		else
+		{
+			m_RandomizedSpeedMagnitude.y = 0;
+		}
+
 	}
 }
