f9c5661396586ea264faabbd4979c97c66e97b3a
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-12-03 22:13:40 +0000

    Started JackInTheBoss.

---
+++ Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/JackInTheBoss/JackInTheBoss.cs
@@ -0,0 +1,60 @@
+﻿// Created by Jason Hein Dec 3, 2014
+//
+// In progress
+//
+//
+//
+
+#region Changelog
+
+#endregion
+
+
+using UnityEngine;
+using System.Collections;
+
+public class JackInTheBoss : MonoBehaviour
+{
+	//The current state
+	public enum JackInTheBossStates
+	{
+		PreFight = 0,
+		Idle,
+		Vulnerable,
+		SpringForward,
+		Spin,
+		Slap,
+		TotalStates
+	}
+	JackInTheBossStates m_State = JackInTheBossStates.PreFight;
+
+	//Attack states of the JackInTheBoss
+	JackInTheBossState[] m_AttackStates;
+
+
+	//Load attack states
+	void Start ()
+	{
+		m_AttackStates = new JackInTheBossState[(int)JackInTheBossStates.TotalStates];
+		m_AttackStates[(int)JackInTheBossStates.PreFight] = new PreFight();
+		m_AttackStates[(int)JackInTheBossStates.Idle] = new PreFight();
+		m_AttackStates[(int)JackInTheBossStates.Vulnerable] = new PreFight();
+		m_AttackStates[(int)JackInTheBossStates.SpringForward] = new PreFight();
+		m_AttackStates[(int)JackInTheBossStates.Spin] = new PreFight();
+		m_AttackStates[(int)JackInTheBossStates.Slap] = new PreFight();
+	}
+	
+	//Update the jack in the boss
+	void Update ()
+	{
+		m_AttackStates[(int)m_State].UpdateState ();
+	}
+
+	/// <summary>
+	/// Switchs the state of the Boss. It is called by the boss's states in order to switch
+	/// </summary>
+	public void SwitchState (JackInTheBossStates state)
+	{
+		m_State = state;
+	}
+}
