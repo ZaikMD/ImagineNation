28ae0244d473b9cda8b817255849c888304474eb
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-12-03 22:13:40 +0000

    Started JackInTheBoss.

---
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/JackInTheBoss/JackInTheBossState.cs
@@ -0,0 +1,83 @@
+﻿// Created by Jason Hein Dec 3, 2014
+//
+// In progress
+//
+//
+//
+
+#region Changelog
+
+#endregion
+
+
+using UnityEngine;
+using System.Collections;
+
+public class JackInTheBossState : MonoBehaviour
+{
+	//Where along the animation this attack is
+	public enum JackInTheBossAttackState
+	{
+		Entering = 0,
+		Attacking,
+		Exiting
+	}
+	protected JackInTheBossAttackState m_AttackState = JackInTheBossAttackState.Entering;
+
+	/// <summary>
+	/// Updates the state. It is called by JackInTheBoss.
+	/// </summary>
+	public void UpdateState ()
+	{
+		switch (m_AttackState)
+		{
+		case JackInTheBossAttackState.Entering:
+		{
+			Entering();
+			break;
+		}
+		case JackInTheBossAttackState.Attacking:
+		{
+			Attacking();
+			break;
+		}
+		case JackInTheBossAttackState.Exiting:
+		{
+			Exiting();
+			break;
+		}
+		}
+	}
+
+	/// <summary>
+	/// Enters the attack.
+	/// </summary>
+	public void EnterAttack()
+	{
+		m_AttackState = JackInTheBossAttackState.Entering;
+	}
+
+	/// <summary>
+	/// Entering this attack.
+	/// </summary>
+	protected virtual void Entering()
+	{
+
+	}
+
+	/// <summary>
+	/// During the attack
+	/// </summary>
+	protected virtual void Attacking()
+	{
+		
+	}
+
+	/// <summary>
+	/// Exiting the attack
+	/// </summary>
+	protected virtual void Exiting()
+	{
+		
+	}
+}
---
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/JackInTheBoss/PreFight.cs
@@ -0,0 +1,38 @@
+﻿// Created by Jason Hein Dec 3, 2014
+//
+// In progress
+//
+//
+//
+
+#region Changelog
+
+#endregion
+
+
+using UnityEngine;
+using System.Collections;
+
+public class PreFight : JackInTheBossState {
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	protected override void Entering()
+	{
+
+	}
+
+	protected override void Attacking()
+	{
+		
+	}
+	
+	protected override void Exiting()
+	{
+		
+	}
+}
