f6e96d649e1d89e1405c60c72dcbf3c490ee8b15
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-12-03 21:17:14 +0000

    made buttons for inverting x and y camera controls

---
+++ trunk/Production/Imagination/Assets/Scripts/Menus/NewMenus/Buttons/Settings/Camera/ButtonV2InvertX.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonV2InvertX : buttonV2Invert
+{
+    public override void use(PlayerInput usedBy = PlayerInput.None)
+    {
+        base.use(usedBy);
+        if (PlayerInfo.getPlayer(usedBy).m_Player == Players.PlayerOne)
+        {
+            GameData.Instance.invertPlayerOneX();
+        }
+        else if (PlayerInfo.getPlayer(usedBy).m_Player == Players.PlayerTwo)
+        {
+            GameData.Instance.invertPlayerTwoX();
+        }
+    }
+}
---
+++ trunk/Production/Imagination/Assets/Scripts/Menus/NewMenus/Buttons/Settings/Camera/ButtonV2InvertY.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonV2InvertY : buttonV2Invert
+{
+    public override void use(PlayerInput usedBy = PlayerInput.None)
+    {
+        base.use(usedBy);
+        if (PlayerInfo.getPlayer(usedBy).m_Player == Players.PlayerOne)
+        {
+            GameData.Instance.invertPlayerOneY();
+        }
+        else if (PlayerInfo.getPlayer(usedBy).m_Player == Players.PlayerTwo)
+        {
+            GameData.Instance.invertPlayerTwoY();
+        }
+    }
+}
