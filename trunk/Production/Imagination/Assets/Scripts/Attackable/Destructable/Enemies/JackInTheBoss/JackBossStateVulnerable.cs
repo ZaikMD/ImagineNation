74952a35a537aaf4af52cb9947069f81fbeb668b
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2014-12-04 02:37:28 +0000

    Added other fight state classes for the JackInTheBoss.

---
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/JackInTheBoss/JackBossStateVulnerable.cs
@@ -0,0 +1,37 @@
+﻿// Created by Jason Hein Dec 3, 2014
+//
+// In progress
+//
+//
+//
+
+#region Changelog
+
+#endregion
+
+
+using UnityEngine;
+using System.Collections;
+
+public class JackBossStateVulnerable : JackInTheBossState {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	protected override void Entering()
+	{
+		
+	}
+	
+	protected override void Attacking()
+	{
+		
+	}
+	
+	protected override void Exiting()
+	{
+		
+	}
+}
