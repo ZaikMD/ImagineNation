1a71355ddb21275e74114bf7dbf265e39a3d4f63
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-01-09 21:01:09 +0000

    working on the InitializeScene button had to move the prefabs folder so i can load the prefabs with file paths, everything looks fine so hopefully nothing breaks horribly

--- Production/Imagination/Assets/Editor/InitializeScene.cs
+++ Production/Imagination/Assets/Editor/InitializeScene.cs
@@ -1,15 +1,74 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
-public class InitializeScene : MonoBehaviour {
+public class InitializeScene : EditorWindow 
+{
+    const string GROUND_NAME = "Ground";
+    const string MANAGERS_NAME = "Managers";
+    const string PLAYER_SPAWNER_NAME = "PlayerSpawner  DO NOT ADD TO THIS";
+    const string CHECK_POINT_NAME = "Checkpoint";
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //const string CHECKPOINT_PREFAB_PATH = "Assets/Prefabs/CheckpointPrefab.prefab";
+    const string CHECKPOINT_PREFAB_PATH = "test/Ground";
+
+    [MenuItem("Tools/InitializeScene")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(InitializeScene));
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Initialize Scene (warning will delete old scene)"))
+        {
+            clearScene();
+            buildScene();
+        }     
+    }
+
+    void clearScene()
+    {
+        object[] obj = GameObject.FindObjectsOfType(typeof(GameObject));
+        foreach (object o in obj)
+        {
+            GameObject.DestroyImmediate((GameObject)o);
+        }
+    }
+
+    void buildScene()
+    {
+        //land(ground)
+        GameObject ground = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        ground.name = GROUND_NAME;
+        ground.transform.position = new Vector3();
+
+        GameObject managers = new GameObject();
+        managers.name = MANAGERS_NAME;
+        managers.AddComponent<GameData>();//gamedata
+        managers.AddComponent<TESTSetGameData>();//set gamedata
+        managers.AddComponent<SFXManager>();//sound manager
+
+        GameObject playerSpawner = new GameObject();
+        playerSpawner.name = PLAYER_SPAWNER_NAME;
+        playerSpawner.AddComponent<PlayerSpawner>();//player spawner
+
+        Object loaded = Resources.Load(CHECKPOINT_PREFAB_PATH);
+        GameObject test = (GameObject)Instantiate(loaded);
+       //GameObject.Instantiate(checkpoint);
+        //checkpoint.name = CHECK_POINT_NAME;
+        //checkpoint.transform.position = new Vector3(0.0f, 2.5f, 0.0f);
+
+        //CheckPoint checkpointScript = checkpoint.AddComponent<CheckPoint>();
+        //checkpointScript.m_OnMaterial = new 
+
+        GameObject[] playerSpawns = new GameObject[2];
+
+
+        //dead player manager /grave
+        
+        //check Point
+        
+        //pause screen
+    }
 }
