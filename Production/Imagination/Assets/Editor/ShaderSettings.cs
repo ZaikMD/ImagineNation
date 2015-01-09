0609bb6f122a4de59367156d950174b22dd94668
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-01-09 20:15:05 +0000

    Added shader settings tool. RIght now it allows you to set the shadow setting of all the lights in the scene.
    THeir will likely be more options in it in the future.

---
+++ Production/Imagination/Assets/Editor/ShaderSettings.cs
@@ -0,0 +1,61 @@
+﻿//Created by Jason Hein on Jan 9th 2015
+//
+//
+//Can be found in the Tools Tab
+
+#region changelog
+/*
+ * 
+ */ 
+#endregion
+
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ShaderSettings : EditorWindow
+{
+	bool lightMenuToggle;
+
+	//Creates the window
+	[MenuItem ("Tools/ShaderSettings")]
+	static void Init () {
+		ShaderSettings window = (ShaderSettings)EditorWindow.GetWindow(typeof(ShaderSettings));
+	}
+
+	//Draws the window
+	void OnGUI ()
+	{
+		GUILayout.Label ("Shader Settings", EditorStyles.boldLabel);
+
+		lightMenuToggle = EditorGUILayout.BeginToggleGroup ("Lights", lightMenuToggle);
+
+		if (lightMenuToggle)
+		{
+			if(GUILayout.Button("No Shadows"))
+			{
+				SetShadowsOfAllLights(LightShadows.None);
+			}
+			if(GUILayout.Button("Soft Shadows"))
+			{
+				SetShadowsOfAllLights(LightShadows.Soft);
+			}
+			if(GUILayout.Button("Hard Shadows"))
+			{
+				SetShadowsOfAllLights(LightShadows.Hard);
+			}
+		}
+		EditorGUILayout.EndToggleGroup ();
+	}
+
+	//Sets the shadows for all of the lights to none, soft or hard
+	void SetShadowsOfAllLights (LightShadows lightShadows)
+	{
+		Light[] lights = Object.FindObjectsOfType<Light> ();
+		foreach (Light light in lights)
+		{
+			light.shadows = lightShadows;
+		}
+	}
+}
