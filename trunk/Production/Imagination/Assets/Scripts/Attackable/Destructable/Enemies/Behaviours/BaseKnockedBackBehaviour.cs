b3ee953ca646102c620f9ba87bebb60922c5e8ce
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-01-22 01:16:47 +0000

    Fixed knockback.
    There are a few bus related to falling off the nav mesh.

--- trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/Behaviours/BaseKnockedBackBehaviour.cs
+++ trunk/Production/Imagination/Assets/Scripts/Attackable/Destructable/Enemies/Behaviours/BaseKnockedBackBehaviour.cs
@@ -29,14 +29,19 @@ public class BaseKnockedBackBehavouir : BaseBehaviour
 	CharacterController m_Controller;
 
 	//Amount to multiply the amount knocked back for this enemy
-	protected float m_LaunchAmount = 8.0f;
+	//Should be changed in inheriting classes
+	protected float m_LaunchAmount = 0.0f;
 
 	//The enemy should move upward more than they are actually launched in order to leave the ground
-	protected float m_UpwardDirectionAmount = 0.5f;
+	protected float m_UpwardDirectionAmount = 0.4f;
+
+	//Collision with other enemies
+	protected const float ONCOLLISION_SLOW_SELF_MULTIPLIER = 0.4f;
+	protected const float ONCOLLISION_SLOW_ENEMY_MULTIPLIER = 0.6f;
 
 
 	//Loads the character controller and sets it to disabled
-	protected virtual void start ()
+	protected override void start ()
 	{
 		m_Controller = GetComponentInParent<CharacterController> ();
 		m_Controller.enabled = false;
@@ -62,23 +67,57 @@ public class BaseKnockedBackBehavouir : BaseBehaviour
 	{
 		if (m_Active == true)
 		{
+			GameObject hitObject = hit.gameObject;
+
 			//Ignore stopping velocity from collisions with enemies
-			if (hit.gameObject.CompareTag("Enemy"))
+			if (hitObject.CompareTag("Enemy"))
 			{
-				return;
+				BaseKnockedBackBehavouir knockedBack = hitObject.GetComponentInChildren<BaseKnockedBackBehavouir>();
+
+				//Do not bother colliding with already flying enemies
+				if (knockedBack == null || knockedBack.isActive())
+				{
+					return;
+				}
+
+				//The enemy collided with is knocked back with a slower speed
+				knockedBack.SetKnockBack(m_Velocity.magnitude * ONCOLLISION_SLOW_ENEMY_MULTIPLIER / knockedBack.getLaunchMultiplier(), m_Velocity.normalized);
+
+				//This enemy slows down after the collision
+				m_Velocity *= ONCOLLISION_SLOW_SELF_MULTIPLIER;
+			}
+			if (hitObject.CompareTag("Player"))
+			{
+				//Knockback the player
+				/*PlayerHealth health = hitObject.GetComponent<PlayerHealth>();
+
+				//The enemy falls
+				if (m_Velocity.y > 0.0f)
+				{
+					m_Velocity.y = -0.1f;
+				}*/
 			}
 			//If the enemy has collided with the kill zone
-			else if (hit.gameObject.name == "Kill Zone" )
+			else if (hitObject.name == "Kill Zone" )
 			{
 				m_Active = false;
-				GetComponent<Destructable>().instantKill();
+				GetComponentInParent<Destructable>().instantKill();
 			}
 			//If the enemy has hit the floor
-			else if (Vector3.Dot(hit.normal, Vector3.up) > 0.3f)
+			else if (Vector3.Dot(hit.normal, Vector3.up) > 0.4f)
 			{
+				if (m_Velocity.y > 0.0f)
+				{
+					return;
+				}
+
+				//Go up slightly to avoid falling through the floor
+				m_Controller.Move(Vector3.up);
+
 				//Inactivate the character controller for now
 				m_Controller.enabled = false;
 				m_Active = false;
+				m_Velocity = Vector3.zero;
 
 				//Set enemy to continue the chase
 				GetAgent ().enabled = true;
@@ -97,31 +136,63 @@ public class BaseKnockedBackBehavouir : BaseBehaviour
 	/// </summary>
 	public virtual void SetKnockBack (float knockBackForce, Vector3 direction)
 	{
-		if (m_Active)
+		//Do not get knocked back if already getting knocked back
+		if (isActive())
 		{
 			return;
 		}
+		
+		//Set us to the knocked back state
+		if (m_EnemyAI.getState() != EnemyAI.EnemyState.KnockedBack)
+		{
+			m_EnemyAI.SetState (EnemyAI.EnemyState.KnockedBack);
+		}
 
-		//Activate knockback in given direction
+		//Activate knockback
 		m_Active = true;
 
 		//Enable the character controller on the enemy
-		m_Controller = GetComponentInParent<CharacterController> ();
-		if (!m_Controller.enabled)
+		if (m_Controller != null && !m_Controller.enabled)
 		{
 			m_Controller.enabled = true;
 		}
 
 		//Turn off the nav mesh agent to avoid errors
-		GetAgent ().enabled = false;
-
-		//Move the character above the ground
-		m_Controller.Move (Vector3.up);
+		NavMeshAgent agent = GetAgent();
+		if (agent != null)
+		{
+			agent.enabled = false;
+		}
 
 		//Set the velocity of the enemy
+		m_Controller.Move(Vector3.up);
 		Vector2 newDirection = new Vector2 (direction.x, direction.z).normalized;
 		m_Velocity = new Vector3 (knockBackForce * m_LaunchAmount * newDirection.x,
 		                          knockBackForce * m_LaunchAmount * m_UpwardDirectionAmount,
 		                          knockBackForce * m_LaunchAmount * newDirection.y);
+	}
+
+	/// <summary>
+	/// Gets the velocity.
+	/// </summary>
+	public Vector3 getVelocity()
+	{
+		return m_Velocity;
+	}
+
+	/// <summary>
+	/// Returns if the enemy is already being knocked back
+	/// </summary>
+	public bool isActive()
+	{
+		return m_Active;
+	}
+
+	/// <summary>
+	/// Gets the amount that his enemy affects it's own launch speed.
+	/// </summary>
+	public float getLaunchMultiplier()
+	{
+		return m_LaunchAmount;
 	}
 }
