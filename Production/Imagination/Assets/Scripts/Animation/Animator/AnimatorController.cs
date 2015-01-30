f00f9ec4d5f2a2edd6792b8cd922c4e7b3843892
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-01-30 20:58:47 +0000

    test things for using animators

---
+++ Production/Imagination/Assets/Scripts/Animation/Animator/AnimatorController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorController : MonoBehaviour 
+{
+    public Animator i_Animator = null;
+
+    protected string[] m_States = new string[]{"test","test2"};
+
+    protected virtual void Start()
+    {
+        if (i_Animator == null)
+        {
+            i_Animator = gameObject.GetComponentInChildren<Animator>();
+#if UNITY_EDITOR || DEBUG
+            if (i_Animator == null)
+            {
+                Debug.LogError("You fucked up there should be an \"Animator\" component somewhere on this object or its children");
+            }
+#endif
+        }
+    }
+
+    //======================================================================================
+
+    public virtual void playAnimation(int animationNumber)
+    {
+        playAnimation(m_States[animationNumber]);
+    }
+
+    public virtual void playAnimation(string animationName)
+    {
+        i_Animator.Play(animationName);
+    }
+
+
+    //======================================================================================
+    //blend
+    /*
+    public virtual void addAnimation(int animationNumber, float targetWeight = DEFAULT_WEIGHT, float fadeLength = DEFAULT_FADE)
+    {
+        addAnimation(m_AnimationClips[animationNumber].name, targetWeight, fadeLength);
+    }
+
+    public virtual void addAnimation(string animationName, float targetWeight = DEFAULT_WEIGHT, float fadeLength = DEFAULT_FADE)
+    {
+        m_Animation.Blend(animationName, targetWeight, fadeLength);
+    }*/
+}
