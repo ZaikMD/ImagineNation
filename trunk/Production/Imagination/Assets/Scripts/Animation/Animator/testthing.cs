0493740524705018a7e8db8d399d600813c19f8c
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-01-30 20:58:47 +0000

    test things for using animators

--- trunk/Production/Imagination/Assets/Scripts/Animation/Animator/testthing.cs
+++ trunk/Production/Imagination/Assets/Scripts/Animation/Animator/testthing.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class test : MonoBehaviour 
+public class testthing : MonoBehaviour 
 {
 	public int animToPlay = 0;
+	public AnimatorController testy;
 	// Update is called once per frame
 	void Update () 
 	{
-		BaseAnimator test = gameObject.GetComponent<BaseAnimator> ();
-		test.playAnimation (animToPlay);
+		testy.playAnimation (animToPlay);
 	}
 }
