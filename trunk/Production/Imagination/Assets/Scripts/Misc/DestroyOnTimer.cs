f30dcdeb3a65838d0fa5550a125cdf28e9da19e3
tack0012 <tack0012@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-01-30 19:19:48 +0000

    Fixed bug where light pegs where in ground

---
+++ trunk/Production/Imagination/Assets/Scripts/Misc/DestroyOnTimer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnTimer : MonoBehaviour {
+
+	public float m_TimeToDestroy;
+
+	private float m_CurrentTime;
+
+	void Start()
+	{
+		m_CurrentTime = m_TimeToDestroy;
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		m_CurrentTime -= Time.deltaTime;
+
+		if(m_CurrentTime < 0)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
