e7a56755157b7dc77409a2e06882bd71938e958a
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-02-04 19:01:19 +0000

    started prioritizing animations

--- trunk/Production/Imagination/Assets/Scripts/Animation/AnimatorPlayers.cs
+++ trunk/Production/Imagination/Assets/Scripts/Animation/AnimatorPlayers.cs
@@ -46,8 +46,42 @@ public class AnimatorPlayers : AnimatorController
         };
     }
 
+    const string COMBO_ = "Combo_";
+
+    int m_LastAnimationPlayed = 0;
+
     public virtual void playAnimation(Animations animation)
     {
         playAnimation(m_States[(int)animation]);
+    }
+
+    public override void playAnimation(int animationNumber)
+    {
+        playAnimation(m_States[animationNumber]);
+    }
+
+    public override void playAnimation(string animationName)
+    {
+        i_Animator.Play(animationName);
+    }
+
+    void requestAnimation(string animation)
+    {
+        for (int i = 0; i < m_States.Length; i++)
+        {
+            if(m_States[i].CompareTo(animation) == 0)
+            {
+                requestAnimation(i);
+                return;
+            }
+        }
+    }
+
+    void requestAnimation(int animation)
+    {
+        if (!m_States[animation].Contains(COMBO_))
+        {
+            //if()
+        }
     }
 }
