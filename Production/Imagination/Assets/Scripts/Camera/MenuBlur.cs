a4c3eae88ba437b800e1fe1eefdc37db8305d22d
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-02-10 22:17:54 +0000

    Added menu blur.

---
+++ Production/Imagination/Assets/Scripts/Camera/MenuBlur.cs
@@ -0,0 +1,32 @@
+﻿//Created by Jason Hein on Feb 10th, 2015
+
+#region Changelog
+/*
+ * 
+ */ 
+#endregion
+
+
+using UnityEngine;
+using System.Collections;
+
+//This script calls OnRenderImage to set a custom shader to render the final image
+public class MenuBlur : MonoBehaviour
+{
+	//The material with our shader settings
+	public Material m_SettingsRenderMaterial;
+
+	//Material for bluring
+	public Material m_BlurMaterial;
+
+	//Render texture of the other camera
+	public RenderTexture m_RenderTexture;
+	
+	//When this camera is about to render
+	void OnRenderImage (RenderTexture src, RenderTexture dst)
+	{
+		//Render the new screen with a blurred background
+		Graphics.Blit (m_RenderTexture, dst, m_BlurMaterial);
+		Graphics.Blit (src, dst, m_SettingsRenderMaterial);
+	}
+}
