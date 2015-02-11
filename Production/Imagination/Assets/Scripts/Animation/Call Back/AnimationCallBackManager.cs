821be0b053a035dd8049fc6e3560f8ecbd9b842b
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-02-11 18:51:08 +0000

    lights added to section 2, call back stuff needs testing

---
+++ Production/Imagination/Assets/Scripts/Animation/Call Back/AnimationCallBackManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public enum CallBackEvents
+{
+    Player_ComboTimeStart,
+    Player_ComboTimeEnd,
+    Player_AttackOver,
+    Player_AttackBegin_Light,
+    Player_AttackBegin_Heavy,
+    Player_AttackBegin_Line,
+    Player_AttackBegin_Cone,
+    Player_AttackBegin_AOE,
+    Player_AttackBegin_HeavyAOE,
+    FootStep,
+    EnemyAttack
+};
+
+public interface CallBack
+{
+    void CallBack(CallBackEvents callBack);
+}
+
+public class AnimationCallBackManager : MonoBehaviour 
+{
+    string[] m_Events = new string[]
+    {
+        "Player_ComboTimeStart",
+        "Player_ComboTimeEnd",
+        "Player_AttackOver",
+        "Player_AttackBegin_Light",
+        "Player_AttackBegin_Heavy",
+        "Player_AttackBegin_Line",
+        "Player_AttackBegin_Cone",
+        "Player_AttackBegin_AOE",
+        "Player_AttackBegin_HeavyAOE",
+        "FootStep",
+        "EnemyAttack"
+    };
+
+    protected List<CallBack> m_Listeners = new List<CallBack>();
+
+    public void callBack(string callbackEVent)
+    {
+        for(int i = 0; i < m_Events.Length; i++)
+        {
+            if (m_Events[i].Equals(callbackEVent, System.StringComparison.OrdinalIgnoreCase))
+            {
+                sendcallBackEvent((CallBackEvents)i);
+                return;
+            }
+        }
+    }
+
+    void sendcallBackEvent(CallBackEvents callBackEvent)
+    {
+        for (int i = 0; i < m_Listeners.Count; i++)
+        {
+            m_Listeners[i].CallBack(callBackEvent);
+        }
+    }
+
+    public void registerCallBack(CallBack callbackToRegister)
+    {
+        if(!m_Listeners.Contains(callbackToRegister))
+        {
+            m_Listeners.Add(callbackToRegister);
+        }
+    }
+
+    public void removeCallBack(CallBack callbackToRemove)
+    {
+        if (m_Listeners.Contains(callbackToRemove))
+        {
+            m_Listeners.Remove(callbackToRemove);
+        }
+    }
+}
+
