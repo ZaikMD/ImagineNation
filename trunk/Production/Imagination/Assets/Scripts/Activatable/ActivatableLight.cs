8c44cce252e7c08ee5ac8a5e80861ab9021ae929
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-03-04 01:18:51 +0000

    Added light turning on and off script.

--- trunk/Production/Imagination/Assets/Scripts/Activatable/ActivatableLight.cs
+++ trunk/Production/Imagination/Assets/Scripts/Activatable/ActivatableLight.cs
@@ -82,4 +82,60 @@ public class ActivatableLight : Activatable
 	{
 		m_Light.intensity = m_OriginalIntensity;
 	}
+
+
+
+	/*public Light[] m_Lights;
+	float[] m_MaxIntensity;
+	int m_FinishedCount = 0;
+	public float LightUpSpeed = 1.0f;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		//Make sure this script has light to destroy
+		if (m_Lights.Length == 0)
+		{
+			//Remove this script
+			Destroy(this);
+			return;
+		}
+		
+		m_MaxIntensity = new float[m_Lights.Length];
+		for (int i = 0; i < m_Lights.Length; i++)
+		{
+			m_MaxIntensity[i] = m_Lights[i].intensity;
+			m_Lights[i].enabled = false;
+			m_Lights[i].intensity = 0.0f;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (m_Activated)
+		{
+			for (int i = 0; i < m_Lights.Length; i++)
+			{
+
+				if (m_Lights[i].intensity < m_MaxIntensity[i])
+				{
+					m_Lights[i].intensity += Time.deltaTime * (LightUpSpeed * m_MaxIntensity[i]);
+
+					//
+					if (m_Lights[i].intensity >= m_MaxIntensity[i])
+					{
+						m_Lights[i].intensity = m_Lights[i].intensity;
+						m_FinishedCount++;
+					}
+				}
+			}
+
+			if (m_FinishedCount == m_Lights.Length)
+			{
+				Deactivate ();
+			}
+		}
+	}*/
 }
