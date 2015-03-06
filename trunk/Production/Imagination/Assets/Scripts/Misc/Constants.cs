33f8d68b81f859cb21c25cba5e93fb58a369a8a9
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-03-06 17:37:02 +0000

    Fixed scene.
    Reduce light pegs to gain a life to 20.

--- trunk/Production/Imagination/Assets/Scripts/Misc/Constants.cs
+++ trunk/Production/Imagination/Assets/Scripts/Misc/Constants.cs
@@ -34,7 +34,7 @@ public static class Constants
 	}
 	public const int LIVE_INITIAL_COUNT = 3;
 	public const int LIVES_MAX = 99;
-	public const short LIGHT_PEGS_NEEDED_TO_GAIN_LIVES = 50;
+	public const short LIGHT_PEGS_NEEDED_TO_GAIN_LIVES = 20;
 	public const string KILLZONE_STRING = "Kill Zone";
 
 	public const string BRIGHTNESS_PROPERTY_NAME = "_Brightness";
