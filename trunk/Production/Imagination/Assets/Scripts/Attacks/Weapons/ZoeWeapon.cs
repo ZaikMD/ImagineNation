1328d943b2422891d033e674fc0dcc5871f8fbe2
elia0034 <elia0034@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-03-10 14:12:22 +0000

    Added the trail renderer to prefabs

--- trunk/Production/Imagination/Assets/Scripts/Attacks/Weapons/ZoeWeapon.cs
+++ trunk/Production/Imagination/Assets/Scripts/Attacks/Weapons/ZoeWeapon.cs
@@ -5,10 +5,17 @@ public class ZoeWeapon : BaseWeapon
 {
 	Transform m_Wand;
 
+	GameObject m_ChargingEffectObject;
+	GameObject m_ChargedEffectObject;
+
+	bool m_ChargeGlowOn = false;
+
 	// Use this for initialization
 	void Start () 
 	{
 		start ();
+
+		m_Wand = GameObject.Find ("Wand").transform;
 	}
 	
 	// Update is called once per frame
@@ -19,22 +26,35 @@ public class ZoeWeapon : BaseWeapon
 
 	protected override void ChargingEffect ()
 	{
-		
+		m_ChargingEffectObject = (GameObject) Instantiate (m_ChargingEffect, m_Wand.position, Quaternion.identity);
+		m_ChargingEffectObject.transform.SetParent (m_Wand);
 	}
 	
 	protected override void ChargedEffect ()
 	{
-		
+		if (!m_ChargeGlowOn)
+		{
+			m_ChargedEffectObject = (GameObject) Instantiate (m_ChargedEffect, m_Wand.position, Quaternion.identity);
+			m_ChargedEffectObject.transform.SetParent (m_Wand);
+			m_ChargeGlowOn = true;
+		}
 	}
-
+	
 	protected override void RemoveChargingEffects ()
 	{
+		if (m_ChargingEffectObject != null)
+			Destroy (m_ChargingEffectObject);
 		
+		if (m_ChargedEffectObject != null)
+			Destroy (m_ChargedEffectObject);
+		
+		m_ChargeGlowOn = false;
 	}
-
+	
 	protected override void AOEEffect ()
 	{
-
+		Instantiate (m_AOEEffectOne, m_Wand.position, Quaternion.identity);
+		Instantiate (m_AOEEffectTwo, m_Wand.position, Quaternion.identity);
 	}
 	
 
