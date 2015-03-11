114fa0404227392f05be059a8c87ae6491238856
elia0034 <elia0034@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-03-11 17:41:00 +0000

    Fixing combat

--- trunk/Production/Imagination/Assets/Scripts/Attacks/Weapons/DerekWeapon.cs
+++ trunk/Production/Imagination/Assets/Scripts/Attacks/Weapons/DerekWeapon.cs
@@ -6,17 +6,15 @@ public class DerekWeapon : BaseWeapon
 	Transform m_LeftGlove;
 	Transform m_RightGlove;
 
-	GameObject m_ChargingEffectObject;
-	GameObject m_ChargedEffectObject;
-
-	bool m_ChargeGlowOn = false;
-
 	// Use this for initialization
 	void Start () 
 	{
 		start ();
 		m_LeftGlove = GameObject.Find ("LeftGloveEffect").transform;
 		m_RightGlove = GameObject.Find ("RightGloveEffect").transform;
+
+		m_ChargingEffectObject = new GameObject[m_ChargingEffectPrefabs.Length];
+		m_ChargedEffectObject = new GameObject[m_ChargedEffectPrefabs.Length];
 	}
 	
 	// Update is called once per frame
@@ -27,34 +25,51 @@ public class DerekWeapon : BaseWeapon
 
 	protected override void ChargingEffect ()
 	{
-		m_ChargingEffectObject = (GameObject) Instantiate (m_ChargingEffect, m_RightGlove.position, Quaternion.identity);
-		m_ChargingEffectObject.transform.SetParent (m_RightGlove);
+		for (int i = 0; i < m_ChargingEffectObject.Length; i++)
+		{
+			m_ChargingEffectObject[i] = (GameObject) Instantiate (m_ChargingEffectPrefabs[i], m_RightGlove.position, Quaternion.identity);
+			m_ChargingEffectObject[i].transform.SetParent (m_RightGlove);
+		}
 	}
 	
 	protected override void ChargedEffect ()
 	{
 		if (!m_ChargeGlowOn)
 		{
-			m_ChargedEffectObject = (GameObject) Instantiate (m_ChargedEffect, m_RightGlove.position, Quaternion.identity);
-			m_ChargedEffectObject.transform.SetParent (m_RightGlove);
+			for (int i = 0; i < m_ChargedEffectObject.Length; i++)
+			{
+				m_ChargedEffectObject[i] = (GameObject) Instantiate (m_ChargedEffectPrefabs[i], m_RightGlove.position, Quaternion.identity);
+				m_ChargedEffectObject[i].transform.SetParent (m_RightGlove);
+			}
 			m_ChargeGlowOn = true;
 		}
 	}
 
 	protected override void RemoveChargingEffects ()
 	{
-		if (m_ChargingEffectObject != null)
-		Destroy (m_ChargingEffectObject);
+		for (int i = 0; i < m_ChargedEffectObject.Length; i++)
+		{
+			if (m_ChargedEffectObject[i] != null)
+				Destroy (m_ChargedEffectObject[i]);
+		}
 
-		if (m_ChargedEffectObject != null)
-			Destroy (m_ChargedEffectObject);
+		for (int i = 0; i < m_ChargedEffectObject.Length; i++)
+		{
+			if (m_ChargingEffectObject[i] != null)
+				Destroy (m_ChargingEffectObject[i]);
+		}
+
+
 
 		m_ChargeGlowOn = false;
 	}
 
 	protected override void AOEEffect ()
 	{
-		Instantiate (m_AOEEffectOne, m_RightGlove.position, Quaternion.identity);
-		Instantiate (m_AOEEffectTwo, m_RightGlove.position, Quaternion.identity);
+		for (int i = 0; i < m_AOEEffects.Length; i++)
+		{
+			Instantiate (m_AOEEffects[i], m_RightGlove.position, Quaternion.identity);
+		}
+		
 	}
 }
---
+++ trunk/Production/Imagination/Assets/Scripts/Misc/TimedDeleteSelf.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedDeleteSelf : MonoBehaviour 
+{
+	public float m_Time;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		m_Time -= Time.deltaTime;
+		if (m_Time <= 0.0f)
+			Destroy (this.gameObject);
+	}
+}
