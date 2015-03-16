0fd3039410790956e360a519e93d772d98bac699
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-03-16 23:46:15 +0000

    gnome mage animations unsuccessfully added

---
+++ Production/Imagination/Assets/Scripts/Animation/Enemies/AnimatorGnomeMage.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorGnomeMage : AnimatorEnemyBase
+{
+	public enum Animations
+	{
+		Hover,
+		Clone,
+		Attack
+	};
+
+	protected const string BLANK = "Blank";
+
+	protected override void Start ()
+	{
+		base.Start ();
+		m_States = new string[]{ "Hover",
+								 "Clone",
+								 "Attack"};
+	}
+
+	public virtual void playAnimation(Animations animation)
+	{
+		playAnimation (m_States [(int)animation]);
+	}
+
+	public override void playAnimation(string animationName)
+	{
+		if(animationName.Equals(m_States[(int)Animations.Attack],System.StringComparison.OrdinalIgnoreCase))
+		{
+
+			if(i_Animator.GetCurrentAnimatorStateInfo(1).IsTag(BLANK))
+			{
+				i_Animator.Play(animationName, 1);
+			}
+			else
+			{
+				if(i_Animator.GetCurrentAnimatorStateInfo(2).IsTag(BLANK))
+				{
+					i_Animator.Play(animationName, 2);
+				}
+			}
+			return;
+		}
+
+		i_Animator.Play(animationName);
+	}
+}
