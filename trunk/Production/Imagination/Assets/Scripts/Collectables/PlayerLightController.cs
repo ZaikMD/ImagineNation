8779a17bf4959e53f03a43339c70a529450d6d06
hein0040 <hein0040@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-03-16 20:15:19 +0000

    Added to light collect affect.
    Fixed zoe falling through platforms.

--- trunk/Production/Imagination/Assets/Scripts/Collectables/PlayerLightController.cs
+++ trunk/Production/Imagination/Assets/Scripts/Collectables/PlayerLightController.cs
@@ -6,31 +6,21 @@ public class PlayerLightController : MonoBehaviour
 	//The light to affect
 	public Light PlayerLight;
 
-	//Ranes
-	float m_OriginalRange = 10.0f;
-	const float MAX_RANGE_ADDED = 2.0f;
-	const float LIGHTPEG_RANGE_ADDED = 0.0f;
-	const float RANGE_CHANGE_SPEED = 1.5f;
-
 	//Intesities
 	float m_OriginalIntensity = 2.0f;
-	const float MAX_INTENSITY_ADDED = 1.25f;
-	const float LIGHTPEG_INTENSITY_ADDED = 1.25f;
-	const float INTENSITY_CHANGE_SPEED = 1.0f;
+	const float MAX_INTENSITY_ADDED = 1.5f;
+	const float LIGHTPEG_INTENSITY_ADDED = 1.5f;
+	const float INTENSITY_CHANGE_SPEED = 1.75f;
 
 	//Get the player lights original intesnity
 	void Start ()
 	{
-		m_OriginalRange = PlayerLight.range;
 		m_OriginalIntensity = PlayerLight.intensity;
 	}
 
+	//Fade light
 	void Update ()
 	{
-		if (PlayerLight.range > m_OriginalRange)
-		{
-			PlayerLight.range -= Time.deltaTime * RANGE_CHANGE_SPEED;
-		}
 		if (PlayerLight.intensity > m_OriginalIntensity)
 		{
 			PlayerLight.intensity -= Time.deltaTime * INTENSITY_CHANGE_SPEED;
@@ -42,12 +32,6 @@ public class PlayerLightController : MonoBehaviour
 	/// </summary>
 	public void AddToIntesnity ()
 	{
-		PlayerLight.range += LIGHTPEG_RANGE_ADDED;
-		if (PlayerLight.range - m_OriginalRange > MAX_RANGE_ADDED)
-		{
-			PlayerLight.range = m_OriginalRange + MAX_RANGE_ADDED;
-		}
-
 		PlayerLight.intensity += LIGHTPEG_INTENSITY_ADDED;
 		if (PlayerLight.intensity - m_OriginalIntensity > MAX_INTENSITY_ADDED)
 		{
@@ -60,7 +44,6 @@ public class PlayerLightController : MonoBehaviour
 	/// </summary>
 	public void RemoveAddedIntesnity ()
 	{
-		PlayerLight.range = m_OriginalRange;
 		PlayerLight.intensity = m_OriginalIntensity;
 	}
 }
