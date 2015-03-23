80e9202559bbe8eb988d4e76a0f63a5cb596c63d
tack0012 <tack0012@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-03-23 21:00:20 +0000

    Added some music stuff, not working yet

---
+++ Production/Imagination/Assets/Scripts/Sound/MusicPlayer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour 
+{
+
+	public int Section;
+	private SFXManager m_SFX;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_SFX = GameObject.FindGameObjectWithTag(Constants.SOUND_MANAGER).GetComponent<SFXManager>();
+	}
+
+	void Update()
+	{
+/*
+		switch(Section)
+		{
+		case 0:
+			m_SFX.playSound(this.transform, Sounds.SongMainMenu);
+			break;
+			
+		case 1:
+			m_SFX.playSound(this.transform, Sounds.SongSection1);
+			break;
+			
+		case 2:
+			m_SFX.playSound(this.transform, Sounds.SongSection2);
+			break;
+			
+		case 3:
+			m_SFX.playSound(this.transform, Sounds.SongSection3);
+			break;
+			
+		}
+*/
+	}
+}
+	
