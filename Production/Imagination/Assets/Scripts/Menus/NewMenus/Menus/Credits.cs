fc90a5ed33249e9eddbbe72cad3abc5830ce3ab4
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-03-27 21:43:28 +0000

    menu for Credits added

---
+++ Production/Imagination/Assets/Scripts/Menus/NewMenus/Menus/Credits.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MenuV2
+{
+	public Spin i_CreditsReel;
+
+	protected override void OnActivated()
+	{
+		i_CreditsReel.resetAngles ();
+	}
+}
