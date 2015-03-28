7d9199b7d4fa935cb0d70e4bbbdf6e9d78283a75
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-03-28 01:36:34 +0000

    smoothed out the cutscene camera

---
+++ trunk/Production/Imagination/Assets/Scripts/Camera/CutSceneCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutSceneCamera : MonoBehaviour 
+{
+	public Transform i_Follow;
+	public float i_LerpSpeed = 0.05f;
+	public float i_RotationalLerpSpeed = 0.05f;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = Vector3.Lerp (transform.position, i_Follow.position, i_LerpSpeed);
+		transform.rotation = Quaternion.Lerp(transform.rotation, i_Follow.transform.rotation, i_RotationalLerpSpeed);
+	}
+}
