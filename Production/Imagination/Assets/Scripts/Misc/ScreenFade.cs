d9ce790631f68670a9a45d3909c311a1c2da4a20
tack0012 <tack0012@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-03-28 00:15:58 +0000

    added screen fade

---
+++ Production/Imagination/Assets/Scripts/Misc/ScreenFade.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScreenFade : MonoBehaviour {
+
+	public float m_FadeInSpeed;
+	public float m_FadeOutSpeed;
+	public bool m_FadeIn = false;
+
+	public Image m_ImageForFade;
+
+	private bool m_FadeOut = false;
+
+	private float m_CurrentFadeTime;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_ImageForFade = GetComponent<Image>();
+
+		if(m_FadeIn)
+		{
+			m_ImageForFade.color = Color.black;
+			BeginFadeIn();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(m_FadeIn)
+		{
+			FadeIn();
+
+			if(m_ImageForFade.color.a <= 0.05f)
+			{
+				m_FadeIn = false;
+				m_ImageForFade.color = Color.clear;
+			}
+		}
+		else if(m_FadeOut)
+		{
+			FadeOut();
+			
+			if(m_ImageForFade.color.a >= 0.95f)
+			{
+				m_FadeOut = false;
+				m_ImageForFade.color = Color.black;
+			}
+		}
+	}
+
+	void FadeIn()
+	{
+		m_ImageForFade.color = Color.Lerp(m_ImageForFade.color, Color.clear, m_FadeInSpeed * Time.deltaTime);	
+	}
+
+	void FadeOut()
+	{
+		m_ImageForFade.color = Color.Lerp(m_ImageForFade.color, Color.black, m_FadeOutSpeed * Time.deltaTime);	
+	}
+
+	public void BeginFadeIn()
+	{
+		m_ImageForFade.color = new Vector4(1, 1, 1, 0.95f);
+		m_FadeIn = true;
+	}
+
+	public void BeginFadeOut()
+	{
+		m_ImageForFade.color = new Vector4(1, 1, 1, 0.05f);
+		m_FadeOut = true;
+	}
+}
