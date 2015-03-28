c4565851688b6d56b7c98bd9a3e60384abb4db6a
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-03-28 02:02:07 +0000

    sound added and teeth fixed

---
+++ trunk/Production/Imagination/Assets/Scripts/Animation/DelayedPositionFix.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedPositionFix : MonoBehaviour 
+{
+	public Transform i_Child;
+	const float DELAY = 0.3f;
+	// Use this for initialization
+	void Start () 
+	{
+	
+		StartCoroutine ("delayedParenting");
+	}
+	
+	IEnumerator delayedParenting()
+	{
+		yield return new WaitForSeconds (DELAY);
+
+		i_Child.position = transform.position;
+		i_Child.rotation = transform.rotation;
+		i_Child.parent = transform;
+	}
+}
