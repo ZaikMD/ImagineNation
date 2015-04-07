49c463316bcb60e06999107d975633d8753286fb
mati0029 <mati0029@609ee094-2344-44c4-a4fc-b89f93a7f5dc>
2015-04-07 14:20:40 +0000

    fixing bugs readding the jump

---
+++ Production/Imagination/Assets/Scripts/Misc/SceneJump.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneJump : MonoBehaviour 
+{
+	bool approved = false;
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.Minus) || Input.GetKey (KeyCode.KeypadMinus))
+						approved = true;
+				else
+						approved = false;
+
+		if (!approved)
+			return;
+
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			GameData.Instance.CurrentSection = Sections.Sections_1;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			GameData.Instance.CurrentSection = Sections.Sections_2;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			GameData.Instance.CurrentSection = Sections.Sections_3;
+		}
+
+		//==============================================================================
+
+		if(Input.GetKeyDown(KeyCode.F1))
+		{
+			Application.LoadLevel("Menusv2");
+		}
+
+		if(Input.GetKeyDown(KeyCode.F12))
+		{
+			Application.LoadLevel("Boss Animation");
+		}
+
+		//==============================================================================
+
+		if(Input.GetKeyDown(KeyCode.Keypad1))
+		{
+			GameData.Instance.CurrentSection = GameData.Instance.CurrentSection;
+			GameData.Instance.CurrentCheckPoint = CheckPoints.CheckPoint_1;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Keypad2))
+		{
+			GameData.Instance.CurrentSection = GameData.Instance.CurrentSection;
+			GameData.Instance.CurrentCheckPoint = CheckPoints.CheckPoint_2;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Keypad3))
+		{
+			if(GameData.Instance.CurrentSection != Sections.Sections_3)
+				return;
+
+			GameData.Instance.CurrentSection = GameData.Instance.CurrentSection;
+			GameData.Instance.CurrentCheckPoint = CheckPoints.CheckPoint_3;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+		{
+			Application.LoadLevel("LoadingScreen");
+		}
+	}
+}
